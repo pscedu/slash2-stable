308c89c3c01f62247144ebf54897697f0324c40c
Jared Yanovich <yanovich@psc.edu>
2007-12-05 22:32:30 +0000

    more transaction bits
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2320 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_util/journal.c
+++ psc_fsutil_libs/psc_util/journal.c
@@ -10,7 +10,7 @@ struct psc_journal {
 	daddr_t		pj_daddr;	/* disk offset of starting ent */
 	int		pj_nextwrite;	/* next entry slot to write to */
 	int		pj_genid;	/* current wrap generation */
-	int		pj_nextxid;	/* next transaction ID */
+	atomic_t	pj_nextxid;	/* next transaction ID */
 };
 
 struct psc_journal_walker {
@@ -27,26 +27,24 @@ struct psc_journal_enthdr {
 	/* record contents follow */
 };
 
-struct psc_journal_x {
-	struct plisthead		 pjx_head;
-	int				 pjx_nents;
-};
-
-struct psc_journal_xent {
-	struct plistentry		 pjxe_entry;
-	struct psc_journal_enthdr	*pjxe_pje;
-};
-
 #define PJ_LOCK(pj)	spinlock(&(pj)->pj_lock)
 #define PJ_ULOCK(pj)	freelock(&(pj)->pj_lock)
 
 #define PJE_MAGIC	0x45678912aabbccdd
+#define PJE_XID_NONE	0		/* invalid transaction ID */
 
-/* Entry types. */
-#define PJET_VOID	0
-#define PJET_XSTART	(-1)
-#define PJET_XEND	(-2)
+/* Journal entry types. */
+#define PJET_VOID	0		/* null journal record */
+#define PJET_XSTART	(-1)		/* transaction began */
+#define PJET_XEND	(-2)		/* transaction ended */
 
+/*
+ * pjournal_init - initialize the in-memory representation of a journal.
+ * @pj: the journal.
+ * @start: disk address of beginning of journal.
+ * @nents: number of entries journal can store before wrapping.
+ * @entsz: size of a journal entry.
+ */
 void
 pjournal_init(struct pjournal *pj, daddr_t start, int nents, int entsz)
 {
@@ -55,32 +53,69 @@ pjournal_init(struct pjournal *pj, daddr_t start, int nents, int entsz)
 	pj->pj_nents = nents;
 	pj->pj_entsz = entsz;
 	LOCK_INIT(&pj->pj_lock);
+	atomic_init(&pj->pj_nextxid, 0);
 }
 
+/*
+ * pjournal_nextxid - obtain an unused journal transaction ID.
+ * @pj: the journal.
+ * Returns: new, unused transaction ID.
+ */
 int
-pjournal_xstart(struct pjournal *pj)
+pjournal_nextxid(struct pjournal *pj)
+{
+	int xid;
+
+	do {
+		xid = atomic_inc_return(&pj->pj_nextxid);
+	} while (xid == PJE_XID_NONE);
+	return (xid);
+}
+
+/*
+ * pjournal_xend - write a "transaction began" record in a journal.
+ * @pj: the journal.
+ * @xid: ID of initiated transaction.
+ * Returns: 0 on success, -1 on error.
+ */
+int
+pjournal_xstart(struct pjournal *pj, int xid)
 {
 	struct psc_journal_enthdr *pje;
 	daddr_t addr;
 
 	pje = palloc(pj->pj_entsz);
+	pje->pje_xid = xid;
 	_pjournal_logwrite(pj, slot, PJET_XSTART, pje);
 	free(pje);
 	return (0);
 }
 
+/*
+ * pjournal_xend - write a "transaction finished" record in a journal.
+ * @pj: the journal.
+ * @xid: ID of finished transaction.
+ * Returns: 0 on success, -1 on error.
+ */
 int
-pjournal_xend(struct pjournal *pj, struct psc_journal_x *pjx)
+pjournal_xend(struct pjournal *pj, int xid)
 {
 	struct psc_journal_enthdr *pje;
 	daddr_t addr;
 
 	pje = palloc(pj->pj_entsz);
+	pje->pje_xid = xid;
 	_pjournal_logwrite(pj, slot, PJET_XEND, pje);
 	free(pje);
 	return (0);
 }
 
+/*
+ * pjournal_clearlog - invalidate a journal entry.
+ * @pj: the journal.
+ * @slot: position location in journal to invalidate.
+ * Returns: 0 on success, -1 on error.
+ */
 int
 pjournal_clearlog(struct psc_journal *pj, int slot)
 {
@@ -105,43 +140,16 @@ pjournal_alloclog(struct psc_journal *pj)
 }
 
 /*
- * pjournal_logwrite - store a new entry in a journal.
- * @pj: the journal.
- * @type: the application-specific log entry type.
- * @data: the journal entry contents to store.
- * Returns: 0 on success, -1 on error.
- */
-int
-pjournal_logwrite(struct psc_journal *pj, int type, const void *data)
-{
-	if (type == PJET_VOID ||
-	    type == PJET_XSTART ||
-	    type == PJET_XEND)
-		pfatal("invalid journal entry type");
-
-	rc = _pjournal_logwrite(pj, pj->pj_nextwrite, type, data);
-	if (rc)
-		return (rc);
-
-	PJ_LOCK(pj);
-	if (++pj->pj_nextwrite >= pj->pj_nents) {
-		pj->pj_nextwrite = 0;
-		pj->pj_xid = 0;
-		pj->pj_genid++;
-	}
-	PJ_ULOCK(pj);
-	return (rc);
-}
-
-/*
  * _pjournal_logwrite - store a new entry in a journal.
  * @pj: the journal.
+ * @slot: position location in journal to write.
  * @type: the application-specific log entry type.
+ * @xid: transaction ID of entry.
  * @data: the journal entry contents to store.
  * Returns: 0 on success, -1 on error.
  */
 __static int
-_pjournal_logwrite(struct psc_journal *pj, int slot, int type,
+_pjournal_logwrite(struct psc_journal *pj, int slot, int type, int xid,
     const void *data)
 {
 	struct psc_journal_enthdr *pje;
@@ -156,9 +164,55 @@ _pjournal_logwrite(struct psc_journal *pj, int slot, int type,
 	pje = data;
 	pje->pje_magic = PJE_MAGIC;
 	pje->pje_type = type;
+	pje->pje_xid = xid;
 	addr = pj->pj_daddr + slot * pj->pj_entsz;
 	ppio_write(addr, pje, pj->entsz);
 	return (0);
+}
+
+/*
+ * pjournal_logwritex - store a new entry in a journal transaction.
+ * @pj: the journal.
+ * @type: the application-specific log entry type.
+ * @xid: transaction ID.
+ * @data: the journal entry contents to store.
+ * Returns: 0 on success, -1 on error.
+ */
+int
+pjournal_logwritex(struct psc_journal *pj, int type, int xid,
+    const void *data)
+{
+	if (type == PJET_VOID ||
+	    type == PJET_XSTART ||
+	    type == PJET_XEND)
+		pfatal("invalid journal entry type");
+
+	rc = _pjournal_logwrite(pj, pj->pj_nextwrite, type, xid, data);
+	if (rc)
+		return (rc);
+
+	PJ_LOCK(pj);
+	if (++pj->pj_nextwrite >= pj->pj_nents) {
+		pj->pj_nextwrite = 0;
+		pj->pj_xid = 0;
+		pj->pj_genid++;
+	}
+	PJ_ULOCK(pj);
+	return (rc);
+}
+
+/*
+ * pjournal_logwrite - store a new entry in a journal.
+ * @pj: the journal.
+ * @type: the application-specific log entry type.
+ * @data: the journal entry contents to store.
+ * Returns: 0 on success, -1 on error.
+ */
+int
+pjournal_logwrite(struct psc_journal *pj, int type, const void *data)
+{
+	return (_pjournal_logwrite(pj, pj->pj_nextwrite, type, data,
+	    PJE_XID_NONE));
 }
 
 /*
@@ -193,54 +247,6 @@ pjournal_logread(struct psc_journal *pj, int slot, void *data)
  */
 int
 pjournal_walk(struct psc_journal *pj, struct psc_journal_walker *pjw,
-    struct psc_journal_entry *pje)
-{
-	daddr_t addr;
-
-	if (pjw->pjw_pos == pjw->pjw_stop) {
-		if (pjw->pjw_seen)
-			return (-2);
-		pjw->pjw_seen = 1;
-	}
-	pjournal_logread(pj, pjw->pjw_pos, pje);
-	if (++pjw->pjw_pos >= pj->pj_nents)
-		pjw->pjw_pos = 0;
-	return (0);
-}
-
-/*
- * pjournal_walk - traverse each entry in a journal.
- * @pj: the journal.
- * @pjw: a walker for the journal.
- * @pje: an entry to be filled in for the next journal entry.
- * Returns: 0 on success, -1 on error, -2 on end of journal.
- */
-int
-pjournal_walk(struct psc_journal *pj, struct psc_journal_walker *pjw,
-    struct psc_journal_entry *pje)
-{
-	daddr_t addr;
-
-	if (pjw->pjw_pos == pjw->pjw_stop) {
-		if (pjw->pjw_seen)
-			return (-2);
-		pjw->pjw_seen = 1;
-	}
-	pjournal_logread(pj, pjw->pjw_pos, pje);
-	if (++pjw->pjw_pos >= pj->pj_nents)
-		pjw->pjw_pos = 0;
-	return (0);
-}
-
-/*
- * pjournal_walkx - traverse each transaction in a journal.
- * @pj: the journal.
- * @pjw: a walker for the journal.
- * @pje: an entry to be filled in for the next journal entry.
- * Returns: 0 on success, -1 on error, -2 on end of journal.
- */
-int
-pjournal_walkx(struct psc_journal *pj, struct psc_journal_walker *pjw,
     struct psc_journal_entry *pje)
 {
 	daddr_t addr;
