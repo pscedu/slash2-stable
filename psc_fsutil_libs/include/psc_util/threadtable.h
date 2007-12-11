a3a229ed3829fc573832b005eb996a1b24dfcd6e
Jared Yanovich <yanovich@psc.edu>
2007-12-11 18:54:10 +0000

    more zest ref removal
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2363 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/threadtable.h
+++ psc_fsutil_libs/include/psc_util/threadtable.h
@@ -2,21 +2,18 @@
 
 #include <pthread.h>
 
+#include "psc_ds/hash.h"
+
 struct psc_thread;
 
 #define psc_threadtbl_get()         _psc_threadtbl_get(pthread_self(), 0)
 #define psc_threadtbl_getid(thrid)  _psc_threadtbl_get(thrid, 0)
 #define psc_threadtbl_get_canfail() _psc_threadtbl_get(pthread_self(), 1)
 
-void    
-psc_threadtbl_put(struct psc_thread *);
-
-struct psc_thread *
-_psc_threadtbl_get(pthread_t, int);
-
-void    zestion_threadtbl_init(void);
-
 void    prthrname(pthread_t);
+void    psc_threadtbl_put(struct psc_thread *);
+void    psc_threadtbl_init(void);
+struct psc_thread *
+	_psc_threadtbl_get(pthread_t, int);
 
 extern struct hash_table thrHtable;
-
