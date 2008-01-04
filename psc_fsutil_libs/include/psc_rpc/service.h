f279247593c88cfda7f6a33ffac410d2c2595c73
Jared Yanovich <yanovich@psc.edu>
2008-01-04 17:15:11 +0000

    decl of pscrpc_thread_spawn
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2423 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_rpc/service.h
+++ psc_fsutil_libs/include/psc_rpc/service.h
@@ -4,12 +4,13 @@
 #define _PFL_SERVICE_H_
 
 #include "psc_rpc/rpc.h"
-#include "psc_util/thread.h"
 #include "psc_ds/list.h"
+
+struct psc_thread;
 
 typedef int (*pscrpc_service_func_t)(struct pscrpc_request *);
 
-struct pscrpc_svc_handle { 
+struct pscrpc_svc_handle {
 	struct psclist_head    svh_chain;
 	struct psc_thread     *svh_threads;
 	struct pscrpc_service *svh_service;
@@ -27,5 +28,6 @@ struct pscrpc_svc_handle {
 
 typedef struct pscrpc_svc_handle pscrpc_svc_handle_t;
 
+void pscrpc_thread_spawn(pscrpc_svc_handle_t *);
 
 #endif /* _PFL_SERVICE_H_ */
