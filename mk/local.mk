5ace20106e7ae828b75bdf0bd4407f8d1f80e112
Jared Yanovich <yanovich@psc.edu>
2008-01-10 20:00:10 +0000

    add bits for easy kernel includes
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2458 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ mk/local.mk
@@ -0,0 +1,3 @@
+# $Id$
+
+KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-i686
--- mk/main.mk
+++ mk/main.mk
@@ -1,5 +1,7 @@
 # $Id$
 
+-include ${PROJ_BASE}/mk/local.mk
+
 OBJS=		$(patsubst %.c,%.o,$(filter %.c,${SRCS}))
 OBJS+=		$(patsubst %.y,%.o,$(filter %.y,${SRCS}))
 OBJS+=		$(patsubst %.l,%.o,$(filter %.l,${SRCS}))
