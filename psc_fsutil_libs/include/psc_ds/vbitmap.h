7e1853c138957232f5fb42b1b6d9f51dd8a90dec
Jared Yanovich <yanovich@psc.edu>
2008-02-07 21:33:33 +0000

    sync
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2644 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_ds/vbitmap.h
+++ psc_fsutil_libs/include/psc_ds/vbitmap.h
@@ -20,15 +20,13 @@ int		vbitmap_next(struct vbitmap *, size_t *);
 int		vbitmap_resize(struct vbitmap *, size_t);
 size_t		vbitmap_getsize(const struct vbitmap *);
 int		vbitmap_get(const struct vbitmap *, size_t);
-int             vbitmap_getncontig(const struct vbitmap *, int *);
-
+int		vbitmap_nfree(const struct vbitmap *);
+int		vbitmap_lcr(const struct vbitmap *);
+int		vbitmap_getncontig(struct vbitmap *, int *);
 
 void		vbitmap_printbin(const struct vbitmap *);
 void		vbitmap_printhex(const struct vbitmap *);
 void		vbitmap_printstats(const struct vbitmap *);
 void		vbitmap_getstats(const struct vbitmap *, int *, int *);
-
-int             vbitmap_nfree(const struct vbitmap *);
-int             vbitmap_lcr(const struct vbitmap *);
 
 #endif /* _PFL_VBITMAP_H_ */
