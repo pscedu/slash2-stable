001572282a1586366dd275a6052cefb05a75ee89
Jared Yanovich <yanovich@psc.edu>
2008-03-04 18:10:27 +0000

    workaround for path nastiness
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2757 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/main.mk
+++ mk/main.mk
@@ -29,7 +29,7 @@ recurse-all:
 	${YACC} ${YFLAGS} $<
 
 .c.o:
-	${CC} ${CFLAGS} ${$(subst .,_,$(subst /,_,$(subst ../,,$<)))_CFLAGS} -c $< -o $@
+	${CC} ${CFLAGS} ${$(subst .,_,$(subst /,_,$(subst ../,,$(subst //,/,$<))))_CFLAGS} -c $< -o $@
 
 ${PROG}: ${OBJS}
 	${CC} -o $@ ${OBJS} ${LDFLAGS}
