0cd71a2d8b354d898be35aae699698ad906c2790
Nathan Stone <nstone@psc.edu>
2008-03-06 22:36:41 +0000

    change this pointer to use the x86_64 kernel path on Lemon
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2795 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -1,4 +1,5 @@
 # $Id$
 
-KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-i686
+KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-x86_64
+#KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-i686
 psc_fsutil_libs_psc_util_crc_c_CFLAGS=	-O2
