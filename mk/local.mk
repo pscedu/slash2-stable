1143695320dc2aa390c62591975be748b095f8b9
Nathan Stone <nstone@psc.edu>
2008-03-10 21:14:25 +0000

    got the line to build on Muir (just switch comments)
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2808 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -1,5 +1,9 @@
 # $Id$
 
+# for ZESTIONs
 KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-x86_64
-#KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-i686
+
+# for MUIR
+#KERNEL_BASE=	/usr/src/kernels/2.6.18-53.1.4.el5-x86_64
+
 psc_fsutil_libs_psc_util_crc_c_CFLAGS=	-O2
