238b905e1ac0b7a705ee294535ed0b1dbf445be2
Jared Yanovich <yanovich@psc.edu>
2008-03-27 18:06:15 +0000

    since these routines shouldnt be called from the client, it wont matter if we error out during them (helps detects bad code paths in the client as well)
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@2959 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_util/waitq.c
+++ psc_fsutil_libs/psc_util/waitq.c
@@ -17,7 +17,8 @@
  * @q: the struct to be initialized.
  * Notes:  the initializer macros must be cast this way or the file will not compile.
  */
-void psc_waitq_init(psc_waitq_t *q)
+void
+psc_waitq_init(psc_waitq_t *q)
 {
 	int rc;
 
@@ -30,15 +31,16 @@ void psc_waitq_init(psc_waitq_t *q)
 
 /**
  * psc_waitq_wait - wait until resource managed by wq_cond is available.
- * @q: pointer to the queue struct which holds the necessary pthreads elements.
- * @k: pointer to spinlock needed to protect the list
+ * @q: wait queue.
+ * @k: optional lock needed to protect the list.
  * Notes: hopefully the freelock() call is not a source of deadlock.  It's here
  * to prevent a race between a process trying to wait and a process that free's
  * the resource also guarded by 'k'.  If this were to occur then the waiting
  * process would never wake up.  Using this method guarantees a free won't
  * happen before this process is put to sleep.
  */
-void psc_waitq_wait(psc_waitq_t *q, psc_spinlock_t *k)
+void
+psc_waitq_wait(psc_waitq_t *q, psc_spinlock_t *k)
 {
 	int rc;
 
@@ -48,6 +50,8 @@ void psc_waitq_wait(psc_waitq_t *q, psc_spinlock_t *k)
 		freelock(k);
 
 	rc |= pthread_cond_wait(&q->wq_cond, &q->wq_mut);
+//	if (k)
+//		spinlock(k);
 	rc |= pthread_mutex_unlock(&q->wq_mut);
 
 	psc_assert(rc == 0);
@@ -57,16 +61,17 @@ void psc_waitq_wait(psc_waitq_t *q, psc_spinlock_t *k)
  * psc_waitq_timedwait - wait a maximum amount of time for the resource managed by
  *	wq_cond to become available.
  * @q: the wait queue.
- * @k: lock needed to protect the list.
+ * @k: optional lock needed to protect the list.
  * @abstime: maximum amount of time to wait before giving up.
  * Notes: returns ETIMEDOUT if the resource has not become available.
  */
-int psc_waitq_timedwait(psc_waitq_t *q, psc_spinlock_t *k,
+int
+psc_waitq_timedwait(psc_waitq_t *q, psc_spinlock_t *k,
 		     const struct timespec *abstime)
 {
 	int rc;
 
-	//psc_trace("abstime.sec %lu abstime.usec %ld", 
+	//psc_trace("abstime.sec %lu abstime.usec %ld",
 	//       abstime->tv_sec, abstime->tv_nsec);
 
 	rc = pthread_mutex_lock(&q->wq_mut);
@@ -75,6 +80,8 @@ int psc_waitq_timedwait(psc_waitq_t *q, psc_spinlock_t *k,
 		freelock(k);
 
 	rc |= pthread_cond_timedwait(&q->wq_cond, &q->wq_mut, abstime);
+//	if (k)
+//		spinlock(k);
 	rc |= pthread_mutex_unlock(&q->wq_mut);
 
 	psc_assert(rc == 0 || rc == ETIMEDOUT);
@@ -85,7 +92,8 @@ int psc_waitq_timedwait(psc_waitq_t *q, psc_spinlock_t *k,
  * psc_waitq_wakeup - unblock one waiting thread
  * @q: pointer to the wait queue struct.
  */
-void psc_waitq_wakeup(psc_waitq_t *q)
+void
+psc_waitq_wakeup(psc_waitq_t *q)
 {
 	int rc;
 
@@ -100,9 +108,11 @@ void psc_waitq_wakeup(psc_waitq_t *q)
  * psc_waitq_wakeall - a method for implementing a psuedo-barrier.
  * @q: pointer to the wait queue struct.
  */
-void psc_waitq_wakeall(psc_waitq_t *q)
+void
+psc_waitq_wakeall(psc_waitq_t *q)
 {
-	int rc = 0;
+	int rc;
+
 	rc =  pthread_mutex_lock(&q->wq_mut);
 	rc |= pthread_cond_broadcast(&q->wq_cond);
 	rc |= pthread_mutex_unlock(&q->wq_mut);
@@ -115,37 +125,32 @@ void psc_waitq_wakeall(psc_waitq_t *q)
 void
 psc_waitq_init(__unusedx psc_waitq_t *q)
 {
-  //psc_fatalx("waitqs not supported");
-  return;
+	psc_fatalx("waitqs not supported");
 }
 
 void
 psc_waitq_wait(__unusedx psc_waitq_t *q, __unusedx psc_spinlock_t *k)
 {
-  //psc_fatalx("waitqs not supported");
-  return;
+	psc_fatalx("waitqs not supported");
 }
 
 int
 psc_waitq_timedwait(__unusedx psc_waitq_t *q, __unusedx psc_spinlock_t *k,
-__unusedx const struct timespec *abstime)
+    __unusedx const struct timespec *abstime)
 {
-  //psc_fatalx("waitqs not supported");
-  return;
+	psc_fatalx("waitqs not supported");
 }
 
 void
 psc_waitq_wakeup(__unusedx psc_waitq_t *q)
 {
-  //psc_fatalx("waitqs not supported");
-	return;
+	psc_fatalx("waitqs not supported");
 }
 
 void
 psc_waitq_wakeall(__unusedx psc_waitq_t *q)
 {
-  //psc_fatalx("waitqs not supported");
-	return;
+	psc_fatalx("waitqs not supported");
 }
 
 #endif /* HAVE_LIBPTHREAD */
