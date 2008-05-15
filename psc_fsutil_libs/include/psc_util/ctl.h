6db37d5bc4414fc9293079544cfbe4f4d49fc6db
Jared Yanovich <yanovich@psc.edu>
2008-05-15 18:41:08 +0000

    seperate into client/server sections and fill in, soon to be split into seperate files
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@3186 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/ctl.h
+++ psc_fsutil_libs/include/psc_util/ctl.h
@@ -5,6 +5,9 @@
  * parameters of a running daemon instance.
  */
 
+#ifndef __PFL_CTL_H__
+#define __PFL_CTL_H__
+
 #include <sys/types.h>
 
 #include "psc_ds/hash.h"
@@ -78,7 +81,7 @@ struct psc_ctlmsg_iostats {
 #define PCI_NAME_ALL		"all"
 
 /* Control message types. */
-#define PCMT_ERRMSG		0
+#define PCMT_ERROR		0
 #define PCMT_GETLOGLEVEL	1
 #define PCMT_GETLC		2
 #define PCMT_GETSTATS		3
@@ -88,17 +91,6 @@ struct psc_ctlmsg_iostats {
 #define PCMT_SETPARAM		7
 #define PCMT_GETIOSTATS		8
 #define NPCMT			9
-
-#define PSC_CTLDEFOPS								\
-/* 0 */	{ NULL,	0 },								\
-/* 1 */	{ psc_ctlrep_getloglevel,	sizeof(struct psc_ctlmsg_loglevel) },	\
-/* 2 */	{ psc_ctlrep_getlc,		sizeof(struct psc_ctlmsg_lc) },		\
-/* 3 */	{ psc_ctlrep_getstats,		sizeof(struct psc_ctlmsg_stats) },	\
-/* 4 */	{ psc_ctlrep_getsubsys,		0 },					\
-/* 5 */	{ psc_ctlrep_gethashtable,	sizeof(struct psc_ctlmsg_hashtable) },	\
-/* 6 */	{ psc_ctlrep_getparam,		sizeof(struct psc_ctlmsg_param) },	\
-/* 7 */	{ psc_ctlrep_setparam,		sizeof(struct psc_ctlmsg_param) },	\
-/* 8 */	{ psc_ctlrep_getiostats,	sizeof(struct psc_ctlmsg_iostats) }
 
 /*
  * Control message header.
@@ -111,16 +103,33 @@ struct psc_ctlmsghdr {
 	unsigned char		mh_data[0];
 };
 
-struct psc_ctlthr {
-	int pc_st_nclients;
-	int pc_st_nsent;
-	int pc_st_nrecv;
-};
 
-struct psc_ctlops {
-	void	(*pc_op)(int, struct psc_ctlmsghdr *, void *);
-	size_t	  pc_siz;
-};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #define PSC_CTL_FOREACH_THREAD(i, thr, thrname, threads, nthreads)	\
 	for ((i) = 0; ((thr) = (threads)[i]) && (i) < (nthreads); i++)	\
@@ -130,6 +139,123 @@ struct psc_ctlops {
 
 #define psc_ctlthr(thr)	((struct psc_ctlthr *)(thr)->pscthr_private)
 
+#define PSC_CTLDEFOPS								\
+/* 0 */	{ NULL,	0 },								\
+/* 1 */	{ psc_ctlrep_getloglevel,	sizeof(struct psc_ctlmsg_loglevel) },	\
+/* 2 */	{ psc_ctlrep_getlc,		sizeof(struct psc_ctlmsg_lc) },		\
+/* 3 */	{ psc_ctlrep_getstats,		sizeof(struct psc_ctlmsg_stats) },	\
+/* 4 */	{ psc_ctlrep_getsubsys,		0 },					\
+/* 5 */	{ psc_ctlrep_gethashtable,	sizeof(struct psc_ctlmsg_hashtable) },	\
+/* 6 */	{ psc_ctlrep_getparam,		sizeof(struct psc_ctlmsg_param) },	\
+/* 7 */	{ psc_ctlrep_setparam,		sizeof(struct psc_ctlmsg_param) },	\
+/* 8 */	{ psc_ctlrep_getiostats,	sizeof(struct psc_ctlmsg_iostats) }
+
+struct psc_ctlthr {
+	int	  pc_st_nclients;
+	int	  pc_st_nsent;
+	int	  pc_st_nrecv;
+};
+
+struct psc_ctlops {
+	void	(*pc_op)(int, struct psc_ctlmsghdr *, void *);
+	size_t	  pc_siz;
+};
+
 void psc_ctlthr_main(const char *, const struct psc_ctlops *, int);
 void psc_ctl_applythrop(int, struct psc_ctlmsghdr *, void *, const char *,
 	void (*)(int, struct psc_ctlmsghdr *, void *, struct psc_thread *));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define PSC_CTLMSG_PRFMT_DEFS															\
+/* 0 */	{ NULL,				psc_ctlmsg_error_prdat,		sizeof(struct psc_ctlmsg_error),	NULL },				\
+/* 1 */	{ psc_ctlmsg_loglevel_prhdr,	psc_ctlmsg_loglevel_prdat,	0,					psc_ctlmsg_loglevel_check },	\
+/* 2 */	{ psc_ctlmsg_lc_prhdr,		psc_ctlmsg_lc_prdat,		sizeof(struct psc_ctlmsg_lc),		NULL },				\
+/* 3 */	{ NULL,				psc_ctlmsg_stats_prdat,		sizeof(struct psc_ctlmsg_stats),	NULL },				\
+/* 4 */	{ NULL,				NULL,				0,					psc_ctlmsg_subsys_check },	\
+/* 5 */	{ psc_ctlmsg_hashtable_prhdr,	psc_ctlmsg_hashtable_prdat,	sizeof(struct psc_ctlmsg_hashtable),	NULL },				\
+/* 6 */	{ psc_ctlmsg_param_prhdr,	psc_ctlmsg_param_prdat,		sizeof(struct psc_ctlmsg_param),	NULL },				\
+/* 7 */	{ NULL,				NULL,				0,					NULL },				\
+/* 8 */	{ psc_ctlmsg_iostats_prhdr,	psc_ctlmsg_iostats_prdat,	sizeof(struct psc_ctlmsg_iostats),	NULL }
+
+struct psc_ctlshow_ent {
+	const char	 *pse_name;
+	int		  pse_value;
+};
+
+struct psc_ctl_thrstatfmt {
+	int		(*ptf_prhdr)(void);
+	void		(*ptf_prdat)(const struct psc_ctlmsg_stats *);
+};
+
+struct psc_ctlmsg_prfmt {
+	int		(*prf_prhdr)(struct psc_ctlmsghdr *);
+	void		(*prf_prdat)(const void *);
+	size_t		  prf_msgsiz;
+	int		(*prf_check)(const struct psc_ctlmsghdr *);
+};
+
+void psc_ctlparse_hashtable(const char *);
+void psc_ctlparse_show(char *);
+void psc_ctlparse_lc(char *);
+void psc_ctlparse_param(char *);
+void psc_ctlparse_iostat(char *);
+
+void psc_ctlcli_main(const char *);
+
+int  psc_ctlthr_prhdr(void);
+void psc_ctlthr_prdat(const struct psc_ctlmsg_stats *);
+
+int  psc_ctlmsg_hashtable_prhdr(struct psc_ctlmsghdr *);
+void psc_ctlmsg_hashtable_prdat(const void *);
+void psc_ctlmsg_error_prdat(const void *);
+int  psc_ctlmsg_subsys_check(const struct psc_ctlmsghdr *);
+int  psc_ctlmsg_iostats_prhdr(struct psc_ctlmsghdr *);
+void psc_ctlmsg_iostats_prdat(const void *);
+int  psc_ctlmsg_lc_prhdr(struct psc_ctlmsghdr *);
+void psc_ctlmsg_lc_prdat(const void *);
+int  psc_ctlmsg_param_prhdr(struct psc_ctlmsghdr *);
+void psc_ctlmsg_param_prdat(const void *);
+int  psc_ctlmsg_stats_prhdr(struct psc_ctlmsghdr *);
+void psc_ctlmsg_stats_prdat(const void *);
+int  psc_ctlmsg_loglevel_check(const struct psc_ctlmsghdr *);
+int  psc_ctlmsg_loglevel_prhdr(struct psc_ctlmsghdr *);
+void psc_ctlmsg_loglevel_prdat(const void *);
+
+extern int psc_ctl_noheader;
+extern int psc_ctl_inhuman;
+
+extern struct psc_ctl_thrstatfmt psc_ctl_thrstatfmts[];
+extern int			 psc_ctl_nthrstatfmts;
+extern struct psc_ctlshow_ent	 psc_ctlshow_tab[];
+extern int			 psc_ctlshow_ntabents;
+extern struct psc_ctlmsg_prfmt	 psc_ctlmsg_prfmts[];
+extern int			 psc_ctlmsg_nprfmts;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* __PFL_CTL_H__ */
