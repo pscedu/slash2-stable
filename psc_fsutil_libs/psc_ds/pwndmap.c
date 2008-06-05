623ac40d49ead7ad80234cd85ac244b529fd07b6
Jared Yanovich <yanovich@psc.edu>
2008-06-05 22:18:36 +0000

    sliding window bitmaps
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@3373 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/psc_ds/pwndmap.c
@@ -0,0 +1,95 @@
+/* $Id$ */
+
+struct psc_wndmap {
+	size_t			 pwm_min;	/* bottom edge of window */
+	psc_spinlock_t		 pwm_lock;
+	struct psclist_head	*pwm_wmbs;
+};
+
+/* windowmap block */
+struct psc_wndmap_block {
+	struct psclist_head	 pwmb_lentry;
+	char			 pwmb_buf[32];
+};
+
+struct psc_wndmap_block *
+pwndmap_addblock(struct psc_windowmap *p)
+{
+	struct psc_wndmap_block *pb;
+
+	pb = PSCALLOC(sizeof(*pb));
+	psclist_xadd_tail(&pb->pwmb_lentry, &p->pwm_wmbs);
+	return (pb);
+}
+
+int
+pwndmap_block_full(struct psc_wndmap_block *pb)
+{
+	int n;
+
+	for (n = 0; n < sizeof(*pb); n++)
+		if (pb->pwmb_buf[n] != 0xff)
+			return (0);
+	return (1);
+}
+
+void
+pwndmap_init(struct psc_windowmap *p, size_t min)
+{
+	p->pwm_min = min;
+	LOCK_INIT(&p->pwm_lock);
+	PSCLIST_HEAD_INIT(&p->pwm_wmbs);
+	pwndmap_addblock(p);
+}
+
+#define WMBSZ (sizeof(((struct psc_wndmap_block *)NULL)->pwmb_buf) * NBBY)
+
+int
+pwndmap_set(struct psc_wndmap *p, size_t pos)
+{
+	struct psc_wndmap_block *pb;
+	int rc;
+
+	rc = 0;
+	spinlock(&p->pmw_lock);
+	if (pos < p->pwm_min) {
+		rc = -1;
+		goto done;
+	}
+	pos -= p->pwm_min;
+	psclist_for_each_entry(pb, &p->pwm_wmbs, pwmb_lentry) {
+		if (pos < WMBSZ) {
+			if (pb->pwmb_buf[pos / NBBY] & (1 << (pos % NBBY)))
+				rc = 1;
+			else {
+				pb->pwmb_buf[pos / NBBY] |= 1 << (pos % NBBY);
+				if (pwndmap_block_full(pb)) {
+					p->pwm_min += WMBSZ;
+
+					psclist_del(&pb->pwmb_lentry)
+					memset(pb, 0, sizeof(*pb));
+					psclist_xadd_tail(&pb->pwmb_lentry, &p->pwm_wmbs);
+				}
+			}
+			goto done;
+		}
+	}
+	for (; pos >= WMBSZ; pos -= WMBSZ)
+		pb = pwndmap_addblock(p);
+	pb->pwmb_buf[pos / NBBY] |= 1 << (pos % NBBY);
+ done:
+	freelock(&p->pmw_lock);
+	return (rc);
+}
+
+void
+pwndmap_free(struct psc_windowmap *p)
+{
+	struct psc_wndmap_block *pb, *nextpb;
+
+	spinlock(&p->pwm_lock);
+	psclist_for_each_entry_safe(pb,
+	    nextpb, &p->pwm_wmbs, pwmb_lentry)
+		free(pb);
+	freelock(&p->pwm_lock);
+}
