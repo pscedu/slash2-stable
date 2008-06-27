32a57fb3d25b2e46949cbcae1720fd03f3f4731d
Jared Yanovich <yanovich@psc.edu>
2008-06-27 06:02:08 +0000

    use _P_U64 instead of the lnet ones (LPX64)
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@3547 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_rpc/rpclog.h
+++ psc_fsutil_libs/include/psc_rpc/rpclog.h
@@ -3,83 +3,81 @@
 #ifndef _PFL_RPCLOG_H_
 #define _PFL_RPCLOG_H_
 
-#include "psc_util/log.h"
 #include "psc_rpc/rpc.h"
+#include "psc_util/log.h"
 
 static inline const char *
 pscrpc_rqphase2str(struct pscrpc_request *req)
 {
-        switch (req->rq_phase) {
-        case ZRQ_PHASE_NEW:
-                return "New";
-        case ZRQ_PHASE_RPC:
-                return "Rpc";
-        case ZRQ_PHASE_BULK:
-                return "Bulk";
-        case ZRQ_PHASE_INTERPRET:
-                return "Interpret";
-        case ZRQ_PHASE_COMPLETE:
-                return "Complete";
-        default:
-                return "?Phase?";
-        }
+	switch (req->rq_phase) {
+	case ZRQ_PHASE_NEW:
+		return "New";
+	case ZRQ_PHASE_RPC:
+		return "Rpc";
+	case ZRQ_PHASE_BULK:
+		return "Bulk";
+	case ZRQ_PHASE_INTERPRET:
+		return "Interpret";
+	case ZRQ_PHASE_COMPLETE:
+		return "Complete";
+	default:
+		return "?Phase?";
+	}
 }
 
 /* Spare the preprocessor, spoil the bugs. */
 #define FLAG(field, str) (field ? str : "")
 
-#define DEBUG_REQ_FLAGS(req)                                                \
-        pscrpc_rqphase2str(req),                                            \
-		FLAG(req->rq_intr, "I"), FLAG(req->rq_replied, "R"),        \
-		FLAG(req->rq_err, "E"),                                     \
-		FLAG(req->rq_net_err, "e"),				    \
-		FLAG(req->rq_timedout, "X") /* eXpired */,                  \
-                FLAG(req->rq_resend, "S"),				    \
-		FLAG(req->rq_restart, "T"), FLAG(req->rq_replay, "P"),      \
-		FLAG(req->rq_no_resend, "N"),                               \
-		FLAG(req->rq_waiting, "W")
+#define DEBUG_REQ_FLAGS(req)						\
+	pscrpc_rqphase2str(req),					\
+		FLAG(req->rq_intr,	"I"),				\
+		FLAG(req->rq_replied,	"R"),				\
+		FLAG(req->rq_err,	"E"),				\
+		FLAG(req->rq_net_err,	"e"),				\
+		FLAG(req->rq_timedout,	"X") /* eXpired */,		\
+		FLAG(req->rq_resend,	"S"),				\
+		FLAG(req->rq_restart,	"T"),				\
+		FLAG(req->rq_replay,	"P"),				\
+		FLAG(req->rq_no_resend,	"N"),				\
+		FLAG(req->rq_waiting,	"W")
 
 #define REQ_FLAGS_FMT "%s:%s%s%s%s%s%s%s%s%s%s"
 
-#define DEBUG_REQ(level, req, fmt, ...)					\
-	do {								\
-		_psclog(__FILE__, __func__, __LINE__,			\
-		      PSS_RPC, level, 0,				\
-		      " req@%p x"LPD64"/t"LPD64" c"LPX64" o%d->@%s:%d "	\
-		      "lens %d/%d ref %d res %d ret %d fl "REQ_FLAGS_FMT \
-		      "/%x/%x replyc "LPX64" rc %d/%d to=%d :: "fmt,	\
-		      req, req->rq_xid, req->rq_transno,		\
-		      req->rq_reqmsg ? (u64)req->rq_reqmsg->handle.cookie : 0xfefefefe,	\
-		      req->rq_reqmsg ? (int)req->rq_reqmsg->opc : -1,	\
-		      req->rq_import ?					\
-		      libcfs_id2str(req->rq_import->imp_connection->c_peer) : \
-		      req->rq_conn ?					\
-		      libcfs_id2str(req->rq_conn->c_peer) : "<?>",	\
-		      (req->rq_import && req->rq_import->imp_client) ?	\
-		      (int)req->rq_import->imp_client->cli_request_portal : -1,	\
-		      req->rq_reqlen, req->rq_replen, atomic_read(&req->rq_refcount), \
-		      req->rq_resend,					\
-		      atomic_read(&req->rq_retries), DEBUG_REQ_FLAGS(req), \
-		      req->rq_reqmsg ? psc_msg_get_flags(req->rq_reqmsg) : 0, \
-		      req->rq_repmsg ? psc_msg_get_flags(req->rq_repmsg) : 0, \
-		      req->rq_repmsg ? (u64)req->rq_repmsg->handle.cookie : 0xfefefefe,	\
-		      req->rq_status,					\
-		      req->rq_repmsg ? req->rq_repmsg->status : 0, req->rq_timeout, \
-		      ## __VA_ARGS__);					\
-	} while(0)
+#define DEBUG_REQ(level, req, fmt, ...)						\
+	_psclog(__FILE__, __func__, __LINE__,					\
+	    PSS_RPC, level, 0,							\
+	    " req@%p x%"_P_U64"d/t%"_P_U64"d c%"_P_U64"x o%d->@%s:%d "		\
+	    "lens %d/%d ref %d res %d ret %d fl "REQ_FLAGS_FMT			\
+	    "/%x/%x replyc %"_P_U64"x rc %d/%d to=%d :: "fmt,			\
+	    req, req->rq_xid, req->rq_transno,					\
+	    req->rq_reqmsg ? (u64)req->rq_reqmsg->handle.cookie : 0xfefefefe,	\
+	    req->rq_reqmsg ? (int)req->rq_reqmsg->opc : -1,			\
+	    req->rq_import ?							\
+	    libcfs_id2str(req->rq_import->imp_connection->c_peer) :		\
+	    req->rq_conn ?							\
+	    libcfs_id2str(req->rq_conn->c_peer) : "<?>",			\
+	    (req->rq_import && req->rq_import->imp_client) ?			\
+	    (int)req->rq_import->imp_client->cli_request_portal : -1,		\
+	    req->rq_reqlen, req->rq_replen, atomic_read(&req->rq_refcount),	\
+	    req->rq_resend,							\
+	    atomic_read(&req->rq_retries), DEBUG_REQ_FLAGS(req),		\
+	    req->rq_reqmsg ? psc_msg_get_flags(req->rq_reqmsg) : 0,		\
+	    req->rq_repmsg ? psc_msg_get_flags(req->rq_repmsg) : 0,		\
+	    req->rq_repmsg ? (u64)req->rq_repmsg->handle.cookie : 0xfefefefe,	\
+	    req->rq_status,							\
+	    req->rq_repmsg ? req->rq_repmsg->status : 0, req->rq_timeout,	\
+	    ## __VA_ARGS__)
 
 
-#define DEBUG_EXP(level, exp, fmt, ...)                                      \
-do {                                                                         \
-        _psclog(__FILE__, __func__, __LINE__,                                  \
-	      PSS_RPC, level, 0,					     \
-	      " exp@%p h"LPX64" conn@%p p:%s ref %d cnt %d f%d :: "fmt,      \
-	      exp, exp->exp_handle.cookie, exp->exp_connection,              \
-	      exp->exp_connection ?                                          \
-	         libcfs_id2str(exp->exp_connection->c_peer) : "<?>",	     \
-	      atomic_read(&exp->exp_refcount),			 	     \
-	      atomic_read(&exp->exp_rpc_count), exp->exp_failed,  	     \
-	      ## __VA_ARGS__);                                               \
-} while(0)
+#define DEBUG_EXP(level, exp, fmt, ...)					\
+	_psclog(__FILE__, __func__, __LINE__,				\
+	    PSS_RPC, level, 0, " exp@%p h%"_P_U64"x "			\
+	    "conn@%p p:%s ref %d cnt %d f%d :: "fmt,			\
+	    exp, exp->exp_handle.cookie, exp->exp_connection,		\
+	    exp->exp_connection ?					\
+	       libcfs_id2str(exp->exp_connection->c_peer) : "<?>",	\
+	    atomic_read(&exp->exp_refcount),				\
+	    atomic_read(&exp->exp_rpc_count), exp->exp_failed,		\
+	    ## __VA_ARGS__)
 
 #endif /* _PFL_RPCLOG_H_ */
