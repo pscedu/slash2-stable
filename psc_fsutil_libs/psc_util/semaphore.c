eabbfd06c88a3489ee7b818cc5745a09b813d2bc
Jared Yanovich <yanovich@psc.edu>
2008-07-15 17:59:26 +0000

    fix some spacing and stuff in case this ever is used
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@3682 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_util/semaphore.c
+++ psc_fsutil_libs/psc_util/semaphore.c
@@ -1,3 +1,7 @@
+/* $Id$ */
+
+#include <sys/types.h>
+#include <sys/ipc.h>
 #include <sys/sem.h>
 
 #include <errno.h>
@@ -7,88 +11,83 @@
 #include "psc_util/semaphore.h"
 
 
-#define LOCK  1
-#define ULOCK 2
 /*
  * new locking interface, lckType needs to be
- *  something like (LOCK) or (ULOCK)
+ *	something like (LOCK) or (ULOCK)
  */
-void psc_sem_op(int sem, int lckType)
+void
+psc_sem_op(int sem, int type)
 {
-  struct sembuf sbuf;
+	struct sembuf sbuf;
 
-  if ( ( lckType & LOCK ) ) sbuf.sem_op = -1 ;
-  else sbuf.sem_op = 1;
+	if (type & PSEMS_LOCK)
+		sbuf.sem_op = -1;
+	else
+		sbuf.sem_op = 1;
 
-  sbuf.sem_flg = SEM_UNDO;
-  sbuf.sem_num = 0;
+	sbuf.sem_flg = SEM_UNDO;
+	sbuf.sem_num = 0;
 
-  psc_dbg("semop() t %d semid %d op %d",
-	    lckType, sem, sbuf.sem_op);
-
-  psc_assert(semop(sem, &sbuf, 1) >= 0);
-
-  psc_dbg("semop() t %d semid %d", lckType, sem);
-
-  return;
+	if (semop(sem, &sbuf, 1) == -1)
+		psc_fatal("semop");
 }
 
 /*
  * Init a semaphore based on the ftok of the handed
- *  string.  The string should be a file name or a FID
- *  This a cool way of performing this action.. supposedly
- *  Stevens was the person who came up with it.
+ *	string.	The string should be a file name or a FID
+ *	This a cool way of performing this action.. supposedly
+ *	Stevens was the person who came up with it.
  */
-int psc_sem_init(key_t key)
+int
+psc_sem_init(key_t key)
 {
-  int sem;
-  int semval;
-  union semun semvar;
-  struct sembuf op_lock[2] = {
-    {2,0,0},  /* wait for lock to equal 0 */
-    {2,1,0},  /* set sem 2 to 1 (locking it) */
-  };
+	int sem, semval;
+	union semun semvar;
+	struct sembuf op_lock[2] = {
+		{ 2, 0, 0 },	/* wait for lock to equal 0 */
+		{ 2, 1, 0 },	/* set sem 2 to 1 (locking it) */
+	};
 
-  struct sembuf op_unlock = {2,-1,0}; /* unlock sem 2 */
+	struct sembuf op_unlock = { 2, -1, 0 }; /* unlock sem 2 */
 
  try_again:
-  psc_dbg("try: semget() key %u", key);
+	psc_dbg("try: semget() key %u", key);
 
-  /* SEMGET needs permissions, preferably the mode of the file */
-  sem = semget(key, 3, IPC_CREAT|0700);
+	/* SEMGET needs permissions, preferably the mode of the file */
+	sem = semget(key, 3, IPC_CREAT | 0700);
 
-  psc_assert(sem >= 0);
+	psc_assert(sem >= 0);
 
-  psc_dbg("try: semop() sem %u", sem);
+	psc_dbg("try: semop() sem %u", sem);
 
-  /* need to get and init semaphore */
-  if ( semop(sem, op_lock, 2) < 0 ) {
-    psc_assert(errno == EINVAL );
-    goto try_again;
-  }
+	/* need to get and init semaphore */
+	if (semop(sem, op_lock, 2) == -1) {
+		psc_assert(errno == EINVAL);
+		goto try_again;
+	}
 
-  psc_dbg("try: semctl() sem 1");
+	psc_dbg("try: semctl() sem 1");
 
-  semval = semctl(sem, 1, GETVAL, 0);
+	semval = semctl(sem, 1, GETVAL, 0);
 
-  psc_assert(semval >= 0);
+	psc_assert(semval >= 0);
 
-  if ( !semval) {
-    psc_dbg("init the sems!");
+	if ( !semval) {
+		psc_dbg("init the sems!");
 
-    /* the semaphore has not yet been initialized */
-    semvar.val = 1;
+		/* the semaphore has not yet been initialized */
+		semvar.val = 1;
 
-    semval = semctl(sem, 0, SETVAL, semvar);
-    psc_assert(semval >= 0);
+		semval = semctl(sem, 0, SETVAL, semvar);
+		psc_assert(semval >= 0);
 
-    semval = semctl(sem, 1, SETVAL, semvar);
-    psc_assert(semval >= 0);
-  }
+		semval = semctl(sem, 1, SETVAL, semvar);
+		psc_assert(semval >= 0);
+	}
 
-  /* unlock the lock 2 */
-  psc_assert(!semop(sem, &op_unlock, 1));
+	/* unlock the lock 2 */
+	psc_assert(!semop(sem, &op_unlock, 1));
 
-  psc_dbg("semop() unlock completed.. returning %u", sem);
-  return sem;
+	psc_dbg("semop() unlock completed.. returning %u", sem);
+	return sem;
 }
