7b9852e570d3ec7b3332d6b68115832487c0710e
Jared Yanovich <yanovich@psc.edu>
2008-07-15 16:37:58 +0000

    add PSC_CALLOC which does an overflow check and clean up some other stuff here
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@3681 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/alloc.h
+++ psc_fsutil_libs/include/psc_util/alloc.h
@@ -1,41 +1,49 @@
 /* $Id$ */
 
-#ifndef _PFL_ALLOC_H_
-#define _PFL_ALLOC_H_
+#ifndef __PFL_ALLOC_H__
+#define __PFL_ALLOC_H__
 
+#include <errno.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "psc_util/log.h"
 
-#define PSCALLOC(s)	PSCALLOC_(s, 0)
-#define TRY_PSCALLOC(s)	PSCALLOC_(s, 1)
+#define PSCALLOC(s)	_PSCALLOC((s), 0)
+#define TRY_PSCALLOC(s)	_PSCALLOC((s), 1)
 
 static inline void *
-PSCALLOC_(size_t size, int can_fail)
+_PSCALLOC(size_t size, int can_fail)
 {
 	void *ptr = malloc(size);
 
 	if (ptr == NULL) {
 		if (!can_fail)
-			psc_fatal("malloc()");
+			psc_fatal("malloc");
 		else {
-			psc_error("malloc()");
-			return NULL;
+			psc_error("malloc");
+			return (NULL);
 		}
 	}
 	memset(ptr, 0, size);
-	return ptr;
+	return (ptr);
 }
 
-static inline void
-PSCFREE(void *p) 
+static inline void *
+PSC_CALLOC(size_t num, size_t size)
 {
-	return (free(p));
+	if (num && SIZE_MAX / num < size) {
+		errno = ENOMEM;
+		return (NULL);
+	}
+	return (PSCALLOC(size * num));
 }
+
+#define PSCFREE(p) free(p)
 
 void *palloc(size_t);
 
 extern long pscPageSize;
 
-#endif /* _PFL_ALLOC_H_ */
+#endif /* __PFL_ALLOC_H__ */
