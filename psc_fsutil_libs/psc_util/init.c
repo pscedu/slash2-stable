c60ade9f2102694ceae7cae0cae4025af8ae2994
Jared Yanovich <yanovich@psc.edu>
2008-07-17 22:49:37 +0000

    initialize pools.  i hate this file
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@3700 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_util/init.c
+++ psc_fsutil_libs/psc_util/init.c
@@ -7,12 +7,15 @@
 #include "psc_util/alloc.h"
 #include "psc_util/lock.h"
 #include "psc_util/log.h"
+#include "psc_util/pool.h"
 #include "psc_util/thread.h"
 #include "psc_util/threadtable.h"
 
 extern struct dynarray psc_subsystems;
 
 long pscPageSize;
+
+struct psc_lockedlist psc_pools;
 
 void
 pfl_init(int thrtabsz)
@@ -24,6 +27,8 @@ pfl_init(int thrtabsz)
 	psc_subsys_register(PSS_RPC, "rpc");
 	psc_subsys_register(PSS_LNET, "lnet");
 	psc_subsys_register(PSS_OTHER, "other");
+
+	pll_init(&psc_pools, struct psc_poolmgr, ppm_lentry);
 
 	psc_threadtbl_init(thrtabsz);
 
