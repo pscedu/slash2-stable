54aaefe9b178c228da02ae2b4f57259f2a98141f
Jared Yanovich <yanovich@psc.edu>
2008-08-29 17:23:01 +0000

    spacing
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4033 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/threadtable.h
+++ psc_fsutil_libs/include/psc_util/threadtable.h
@@ -6,11 +6,11 @@
 
 struct psc_thread;
 
-#define psc_threadtbl_init(sz)	init_hash_table(&thrHtable, sz, "threads")
+#define psc_threadtbl_init(sz)		init_hash_table(&thrHtable, sz, "threads")
 
-#define psc_threadtbl_get()	   _psc_threadtbl_get(pthread_self(), 0)
-#define psc_threadtbl_getid(thrid)  _psc_threadtbl_get(thrid, 0)
-#define psc_threadtbl_get_canfail() _psc_threadtbl_get(pthread_self(), 1)
+#define psc_threadtbl_get()		_psc_threadtbl_get(pthread_self(), 0)
+#define psc_threadtbl_getid(thrid)	_psc_threadtbl_get(thrid, 0)
+#define psc_threadtbl_get_canfail()	_psc_threadtbl_get(pthread_self(), 1)
 
 void    prthrname(pthread_t);
 void    psc_threadtbl_put(struct psc_thread *);
