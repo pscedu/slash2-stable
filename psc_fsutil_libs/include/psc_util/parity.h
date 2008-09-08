0fe289e6ab3ad5d0c51576dd56c6fe8f9b05751d
Paul Nowoczynski <pauln@psc.edu>
2008-09-08 17:39:22 +0000

    forgot this
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4099 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/include/psc_util/parity.h
@@ -0,0 +1,12 @@
+/* $Id$ */
+
+#ifndef __PFL_PARITY_H__
+#define __PFL_PARITY_H__
+
+#include "psc_types.h"
+#include <sys/types.h>
+
+extern void
+parity_calc(void *data, void *parity, u32 len);
+
+#endif
