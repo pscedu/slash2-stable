5dc4023036a8ee699cf10b239f1b3ace7ea7a165
Jared Yanovich <yanovich@psc.edu>
2008-09-24 22:24:35 +0000

    bitflag tester
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4255 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/tests/bitflag/bitflag.c
@@ -0,0 +1,73 @@
+/* $Id: random.c 4118 2008-09-09 17:59:03Z yanovich $ */
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "psc_util/bitflag.h"
+
+const char *progname;
+
+#define NENTS 5000
+int buf[NENTS];
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", progname);
+	exit(1);
+}
+
+#define CNT_ASSERT(code, xcode)				\
+	switch (fork()) {				\
+	case -1:					\
+		psc_fatal("fork");			\
+	case 0: /* child */				\
+		code;					\
+		exit(0);				\
+	default: /* parent */				\
+		if (wait(&st) == -1)			\
+			psc_fatal("wait");		\
+		if (!(xcode))				\
+			psc_fatalx("want %s, got %d",	\
+			    # xcode, st);		\
+		break;					\
+	}
+
+#define CNT_ASSERT0(code)	CNT_ASSERT(code, st == 0)
+#define CNT_ASSERTA(code)	CNT_ASSERT(code, WCOREDUMP(st))
+
+#define B0	(1 << 0)
+#define B1	(1 << 1)
+#define B2	(1 << 2)
+#define B3	(1 << 3)
+#define B4	(1 << 4)
+#define B5	(1 << 5)
+#define B6	(1 << 6)
+#define B7	(1 << 7)
+
+int
+main(int argc, char *argv[])
+{
+	int st, f, c;
+
+	progname = argv[0];
+	while ((c = getopt(argc, argv, "")) != -1)
+		switch (c) {
+		default:
+			usage();
+		}
+	argc -= optind;
+	if (argc)
+		usage();
+
+	f = 0;
+	CNT_ASSERT0(f = B1; psc_assert(bitflag_sorc(&f, NULL, B1, 0, 0, 0, BIT_CHK) == 0));
+	CNT_ASSERT0(f = B2; psc_assert(bitflag_sorc(&f, NULL, B2, B3, 0, 0, BIT_CHK) == 0));
+	CNT_ASSERT0(psc_assert(bitflag_sorc(&f, NULL, 0, 0, B6, 0, BIT_SET) == 0); psc_assert(f == B6));
+
+	exit(0);
+}
