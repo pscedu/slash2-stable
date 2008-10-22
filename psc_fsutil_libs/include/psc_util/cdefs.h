e70b75664157aaaefffe7b55ba8c962b2e8ef570
Jared Yanovich <yanovich@psc.edu>
2008-10-22 23:15:49 +0000

    try to be a little smarter about __attribute__ and friends
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4402 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/cdefs.h
+++ psc_fsutil_libs/include/psc_util/cdefs.h
@@ -1,34 +1,40 @@
 /* $Id$ */
 
+#include <sys/cdefs.h>
+
 #define NENTRIES(t) (int)(sizeof(t) / sizeof(t[0]))
 
 #ifndef offsetof
 #define offsetof(s, e) ((size_t)&((s *)0)->e)
 #endif
 
+/* Define __GNUC_PREREQ__(maj, min). */
+#ifndef __GNUC_PREREQ__
+# ifdef __GNUC__
+#  define __GNUC_PREREQ__(maj, min) \
+        ((__GNUC__ > (maj)) || (__GNUC__ == (maj) && __GNUC_MINOR__ >= (min)))
+# else
+#  define __GNUC_PREREQ__(maj, min) 0
+# endif
+#endif
+
+/* no-op __attribute if old/non gcc */
+#if !__GNUC_PREREQ__(2, 5)
+# define __attribute__(x)
+#endif
+
 #ifndef __dead
-#define __dead		__attribute__((__noreturn__))
+# define __dead		__attribute__((__noreturn__))
 #endif
 
 #ifndef __packed
-#define __packed	__attribute__((__packed__))
+# define __packed	__attribute__((__packed__))
 #endif
 
 #define __unusedx	__attribute__((__unused__))
 
-#ifdef __weak
 #undef __weak
-#endif
-
 #define __weak		__attribute__((__weak__))
-
-#ifdef __cplusplus
-# define __BEGIN_DECLS	extern "C" {
-# define __END_DECLS	}
-#else
-# define __BEGIN_DECLS
-# define __END_DECLS
-#endif
 
 /*
  * Keyword to mark something as file-scoped
