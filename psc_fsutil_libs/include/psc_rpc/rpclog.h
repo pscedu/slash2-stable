21adda57c966ecc8779b155393c575bc259f915e
Jared Yanovich <yanovich@psc.edu>
2008-11-07 18:31:51 +0000

    try to fix the crash in DEBUG_REQ, i think this will work, and tidy
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4551 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_rpc/rpclog.h
+++ psc_fsutil_libs/include/psc_rpc/rpclog.h
@@ -26,51 +26,52 @@ pscrpc_rqphase2str(struct pscrpc_request *req)
 }
 
 /* Spare the preprocessor, spoil the bugs. */
-#define FLAG(field, str) (field ? str : "")
-
-#define DEBUG_REQ_FLAGS(req)						\
-	pscrpc_rqphase2str(req),					\
-		FLAG(req->rq_intr,	"I"),				\
-		FLAG(req->rq_replied,	"R"),				\
-		FLAG(req->rq_err,	"E"),				\
-		FLAG(req->rq_net_err,	"e"),				\
-		FLAG(req->rq_timedout,	"X") /* eXpired */,		\
-		FLAG(req->rq_resend,	"S"),				\
-		FLAG(req->rq_restart,	"T"),				\
-		FLAG(req->rq_replay,	"P"),				\
-		FLAG(req->rq_no_resend,	"N"),				\
-		FLAG(req->rq_waiting,	"W")
+#define DEBUG_REQ_FLAGS(rq)						\
+	pscrpc_rqphase2str(rq),						\
+	(rq)->rq_intr		? "I" : "",				\
+	(rq)->rq_replied	? "R" : "",				\
+	(rq)->rq_err		? "E" : "",				\
+	(rq)->rq_net_err	? "e" : "",				\
+	(rq)->rq_timedout	? "X" : "", /* eXpired */		\
+	(rq)->rq_resend		? "S" : "",				\
+	(rq)->rq_restart	? "T" : "",				\
+	(rq)->rq_replay		? "P" : "",				\
+	(rq)->rq_no_resend	? "N" : "",				\
+	(rq)->rq_waiting	? "W" : ""
 
 #define REQ_FLAGS_FMT "%s:%s%s%s%s%s%s%s%s%s%s"
 
-#define DEBUG_REQ(level, req, fmt, ...)					\
-	{								\
-		char __nidstr[PSC_NIDSTR_SIZE];				\
-		psc_nid2str((req)->rq_conn->c_peer.nid, __nidstr);	\
-		_psclog(__FILE__, __func__, __LINE__,			\
-			PSS_RPC, level, 0,				\
-			" req@%p x%"_P_U64"d/t%"_P_U64"d c%"_P_U64"x o%d->@%s:%d " \
-			"lens %d/%d ref %d res %d ret %d fl "REQ_FLAGS_FMT \
-			"/%x/%x replyc %"_P_U64"x rc %d/%d to=%d :: "fmt, \
-			req, req->rq_xid, req->rq_transno,		\
-			req->rq_reqmsg ? (u64)req->rq_reqmsg->handle.cookie : 0xfefefefe, \
-			req->rq_reqmsg ? (int)req->rq_reqmsg->opc : -1,	\
-			req->rq_import ?				\
-			libcfs_id2str(req->rq_import->imp_connection->c_peer) :	\
-			req->rq_conn ?					\
-			__nidstr : "<?>",				\
-			(req->rq_import && req->rq_import->imp_client) ? \
-			(int)req->rq_import->imp_client->cli_request_portal : -1, \
-			req->rq_reqlen, req->rq_replen, atomic_read(&req->rq_refcount),	\
-			req->rq_resend,					\
-			atomic_read(&req->rq_retries), DEBUG_REQ_FLAGS(req), \
-			req->rq_reqmsg ? psc_msg_get_flags(req->rq_reqmsg) : 0,	\
-			req->rq_repmsg ? psc_msg_get_flags(req->rq_repmsg) : 0,	\
-			req->rq_repmsg ? (u64)req->rq_repmsg->handle.cookie : 0xfefefefe, \
-			req->rq_status,					\
-			req->rq_repmsg ? req->rq_repmsg->status : 0, req->rq_timeout, \
-			## __VA_ARGS__);				\
-			}
+#define DEBUG_REQ(level, rq, fmt, ...)					\
+    do {								\
+	struct pscrpc_import *__imp = (rq)->rq_import;			\
+	char __nidstr[PSC_NIDSTR_SIZE];					\
+									\
+	if ((rq)->rq_conn)						\
+		psc_nid2str((rq)->rq_conn->c_peer.nid, __nidstr);	\
+	_psclog(__FILE__, __func__, __LINE__,				\
+	    PSS_RPC, (level), 0,					\
+	    " req@%p x%"_P_U64"d/t%"_P_U64"d "				\
+	    "c%"_P_U64"x "						\
+	    "o%d->@%s:%d "						\
+	    "lens %d/%d ref %d res %d ret %d fl "REQ_FLAGS_FMT		\
+	    "/%x/%x replyc %"_P_U64"x rc %d/%d to=%d :: "fmt,		\
+	    (rq), (rq)->rq_xid, (rq)->rq_transno,			\
+	    (rq)->rq_reqmsg ? (rq)->rq_reqmsg->handle.cookie : 0xdeadbeef, \
+	    (rq)->rq_reqmsg ? (rq)->rq_reqmsg->opc : -1,		\
+	    __imp ? libcfs_id2str(__imp->imp_connection->c_peer) :	\
+	      (rq)->rq_conn ? __nidstr : "<?>",				\
+	    __imp && __imp->imp_client ?				\
+	      __imp->imp_client->cli_request_portal : -1,		\
+	    (rq)->rq_reqlen, (rq)->rq_replen,				\
+	    atomic_read(&(rq)->rq_refcount), (rq)->rq_resend,		\
+	    atomic_read(&(rq)->rq_retries), DEBUG_REQ_FLAGS(rq),	\
+	    (rq)->rq_reqmsg ? psc_msg_get_flags((rq)->rq_reqmsg) : -1,	\
+	    (rq)->rq_repmsg ? psc_msg_get_flags((rq)->rq_repmsg) : 0,	\
+	    (rq)->rq_repmsg ? (rq)->rq_repmsg->handle.cookie : 0xdeadbeef, \
+	    (rq)->rq_status,						\
+	    (rq)->rq_repmsg ? (rq)->rq_repmsg->status : 0,		\
+	    (rq)->rq_timeout, ## __VA_ARGS__);				\
+    } while (0)
 
 #define DEBUG_EXP(level, exp, fmt, ...)					\
 	_psclog(__FILE__, __func__, __LINE__,				\
