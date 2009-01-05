57ef559d48f6f9fe741975ba221db034aec1a8df
Jared Yanovich <yanovich@psc.edu>
2009-01-05 21:37:45 +0000

    old psig approach cant work, use systemtap instead
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4837 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- tools/psig/pathnames.h
+++
@@ -1,4 +0,0 @@
-/* $Id$ */
-
-#define _PATH_PROC "/proc"
-#define _RELPATH_FILE "/file"
--- tools/psig/putils.h
+++
@@ -1,8 +0,0 @@
-/* $Id$ */
-
-#include <sys/types.h>
-
-#define P_NODIE 1	/* Don't die in getpidpath(). */
-
-char	*getpidpath(char *, pid_t *, int);
-int	 parsepid(char *, pid_t *);
