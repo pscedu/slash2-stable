5824eae80b5fae33c1c73f0a6160cfed89a22c8c
Jared Yanovich <yanovich@psc.edu>
2009-01-06 00:11:36 +0000

    move to PFL
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4846 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/tests/acsvc/acsvc.c
@@ -0,0 +1,42 @@
+/* $Id$ */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "psc_util/acsvc.h"
+#include "psc_util/cdefs.h"
+#include "psc_util/thread.h"
+
+const char *progname;
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", progname);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct psc_thread thr;
+	int fd;
+
+	progname = argv[0];
+	while (getopt(argc, argv, "") != -1)
+		usage();
+	argc -= optind;
+	if (argc)
+		usage();
+
+	acsvc_init(&thr, 0, "test", argv);
+
+	fd = access_fsop(ACSOP_OPEN, geteuid(), getegid(),
+	    __FILE__, O_RDONLY);
+	if (fd == -1)
+		psc_fatal("access_fsop");
+	close(fd);
+	exit(0);
+}
