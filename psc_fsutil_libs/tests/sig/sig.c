39ef524b42c2b691841ba609255d0a4e36b295b2
Jared Yanovich <yanovich@psc.edu>
2009-01-06 00:13:59 +0000

    move to PFL
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@4848 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/tests/sig/sig.c
@@ -0,0 +1,129 @@
+/* $Id$ */
+
+
+#include <err.h>
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "psc_util/cdefs.h"
+
+const char *progname;
+pthread_t thr1;
+pthread_t thr2;
+pthread_t thr3;
+
+char *signames[] = {
+	"<zero>",
+	"SIGHUP",
+	"SIGINT",
+	"SIGQUIT",
+	"SIGILL",
+	"SIGTRAP",
+	"SIGABRT",
+	"SIGBUS",
+	"SIGFPE",
+	"SIGKILL",
+	"SIGUSR1",
+	"SIGSEGV",
+	"SIGUSR2",
+	"SIGPIPE",
+	"SIGALRM",
+	"SIGTERM",
+	"SIGSTKFLT",
+	"SIGCHLD",
+	"SIGCONT",
+	"SIGSTOP",
+	"SIGTSTP",
+	"SIGTTIN",
+	"SIGTTOU",
+	"SIGURG",
+	"SIGXCPU",
+	"SIGXFSZ",
+	"SIGVTALRM",
+	"SIGPROF",
+	"SIGWINCH",
+	"SIGIO",
+	"SIGPWR",
+	"SIGSYS"
+};
+#define NSIGS (sizeof(signames) / sizeof(signames[0]))
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", progname);
+	exit(1);
+}
+
+void
+report_sigs(void)
+{
+	struct sigaction sa;
+	const char *act;
+	int i;
+
+	for (i = 1; i < (int)NSIGS; i++) {
+		if (sigaction(i, NULL, &sa) == -1)
+			err(1, "sigaction");
+		if (sa.sa_handler == SIG_IGN)
+			act = "ign";
+		else if (sa.sa_handler == SIG_DFL)
+			act = "dfl";
+		printf("thr %ld: sig %02d [%s]: %s\n", pthread_self(),
+		    i, signames[i], act);
+	}
+}
+
+void *
+thr1_main(__unusedx void *arg)
+{
+	report_sigs();
+	return (NULL);
+}
+
+void *
+thr2_main(__unusedx void *arg)
+{
+	report_sigs();
+	return (NULL);
+}
+
+void *
+thr3_main(__unusedx void *arg)
+{
+
+	report_sigs();
+	return (NULL);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int rc, c;
+
+	while ((c = getopt(argc, argv, "")) != -1)
+		switch (c) {
+		default:
+			usage();
+		}
+
+	if ((rc = pthread_create(&thr1, NULL, thr1_main, NULL)) != 0)
+		errx(1, "pthread_join: %s", strerror(rc));
+	if ((rc = pthread_create(&thr2, NULL, thr2_main, NULL)) != 0)
+		errx(1, "pthread_join: %s", strerror(rc));
+	if ((rc = pthread_create(&thr3, NULL, thr3_main, NULL)) != 0)
+		errx(1, "pthread_join: %s", strerror(rc));
+
+	if ((rc = pthread_join(thr1, NULL)) != 0)
+		errx(1, "pthread_join: %s", strerror(rc));
+	if ((rc = pthread_join(thr2, NULL)) != 0)
+		errx(1, "pthread_join: %s", strerror(rc));
+	if ((rc = pthread_join(thr3, NULL)) != 0)
+		errx(1, "pthread_join: %s", strerror(rc));
+	exit(0);
+}
