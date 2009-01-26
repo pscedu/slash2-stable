0ccfea157ae848a2113d8bd5a513db99f3053f88
Jared Yanovich <yanovich@psc.edu>
2009-01-26 22:44:44 +0000

    add tester
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5062 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/tests/prsig/prsig.c
@@ -0,0 +1,31 @@
+/* $Id$ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "psc_util/cdefs.h"
+#include "psc_util/prsig.h"
+
+const char *progname;
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", progname);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	progname = argv[0];
+	if (getopt(argc, argv, "") != -1)
+		usage();
+	argc -= optind;
+	if (argc)
+		usage();
+
+	psc_prsig();
+	exit(0);
+}
