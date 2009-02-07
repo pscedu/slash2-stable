220882fc0eae2d838878c1cac37bad2381bd2cf7
Jared Yanovich <yanovich@psc.edu>
2009-02-07 00:35:04 +0000

    compilation fixes
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5159 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/tests/sock/sock.c
+++ psc_fsutil_libs/tests/sock/sock.c
@@ -1,24 +1,44 @@
 /* $Id: fmtstr.c 2344 2007-12-07 17:48:02Z yanovich $ */
 
-#include <limits.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
+#include "psc_util/alloc.h"
+#include "psc_util/atomic.h"
 #include "psc_util/cdefs.h"
+#include "psc_util/iostats.h"
+#include "psc_util/log.h"
+#include "psc_util/strlcpy.h"
+
+#include "sdp_inet.h"
 
 /* I/O operation flags */
-#define IOF_WR	(1 << 0)		/* write(2) instead of read(2) */
+#define IOF_WR	(1 << 0)			/* write(2) instead of read(2) */
+
+#define Q 15
 
 struct sockarg {
-	int s;				/* socket */
+	int s;					/* socket */
 };
 
-int		 forcesdp;		/* force sockets direct */
-const char	*progname;
-char		*buf;
-size_t		 bufsiz = 1024 * 1024;
-struct iostats	 rdst;			/* read stats */
-struct iostats	 wrst;			/* write stats */
+int			 forcesdp;		/* force sockets direct */
+const char		*progname;
+char			*buf;
+size_t			 bufsiz = 1024 * 1024;
+struct iostats	 	rdst;			/* read stats */
+struct iostats	 	wrst;			/* write stats */
 
 __dead void
 usage(void)
@@ -46,7 +66,7 @@ ioloop(int s, int ioflags)
 		if (rv == -1)
 			psc_fatal("select");
 		if (rv != 1)
-			psc_fatalx("select: unexpected value (%d)", rv);
+			psc_fatalx("select: unexpected value (%zd)", rv);
 
 		/* transfer a chunk of data */
 		if (ioflags & IOF_WR)
@@ -57,11 +77,11 @@ ioloop(int s, int ioflags)
 			psc_fatal("recv");
 		else if (rv == -1)
 			psc_fatalx("recv: unexpected EOF");
-		else if (rv != bufsiz)
+		else if (rv != (int)bufsiz)
 			psc_fatalx("recv: short write");
 
 		/* tally amount transferred */
-		atomic_inc(rv, &ist->ist_bytes_intv);
+		atomic_add(rv, &ist->ist_bytes_intv);
 	}
 }
 
@@ -70,7 +90,7 @@ worker_main(void *arg)
 {
 	struct sockarg *sarg = arg;
 
-	ioloop(sarg.s, 0);
+	ioloop(sarg->s, 0);
 }
 
 __dead void
@@ -80,10 +100,10 @@ dolisten(const char *listenif)
 	struct sockaddr_in *sin;
 	struct sockarg sarg;
 	struct ifreq ifr;
+	int rc, s, clisock;
 	char addrbuf[50];
 	pthread_t rdthr;
 	socklen_t salen;
-	int s;
 
 	memset(&ifr, 0, sizeof(ifr));
 	if (strlcpy(ifr.ifr_name, listenif,
@@ -94,24 +114,28 @@ dolisten(const char *listenif)
 		psc_fatal("ioctl GIFADDR %s", listenif);
 	close(s);
 
-	sin = &ifr.ifr_addr;
+	sin = (struct sockaddr_in *)&ifr.ifr_addr;
 	if (sin->sin_family != AF_INET)
 		psc_fatalx("non-ipv4 not supported");
 
 	if (forcesdp)
 		sin->sin_family = AF_INET_SDP;
 
-	s = socket(AF_INET, SOCK_DGRAM, 0);
-	if (bind(s, sin, sizeof(*sin)) == -1)
+	s = socket(AF_INET, SOCK_STREAM, 0);
+	if (bind(s, (struct sockaddr *)sin, sizeof(*sin)) == -1)
 		psc_fatal("bind");
 	if (listen(s, Q) == -1)
 		psc_fatal("bind");
 	psc_notice("listening on %s", inet_ntop(AF_INET,
 	    &sin->sin_addr.s_addr, addrbuf, sizeof(addrbuf)));
-	clisock = accept(s, (struct sockaddr *)&ss, sizeof(ss));
+	clisock = accept(s, (struct sockaddr *)&ss, &salen);
 	close(s);
 
-	sin = &ss;
+	if (ss.ss_family != AF_INET &&
+	    ss.ss_family != AF_INET_SDP)
+		psc_fatalx("accept: impossible address family");
+
+	sin = (struct sockaddr_in *)&ss;
 	psc_notice("accepted connection from %s", inet_ntop(AF_INET,
 	    &sin->sin_addr.s_addr, addrbuf, sizeof(addrbuf)));
 
@@ -125,15 +149,30 @@ dolisten(const char *listenif)
 __dead void
 doconnect(const char *addr)
 {
+	struct sockaddr_storage ss;
+	struct sockaddr_in *sin;
+	struct sockarg sarg;
 	pthread_t rdthr;
+	int rc, s;
 
-	AF_INET_SDP;
+	sin = (struct sockaddr_in *)&ss;
+	if (inet_pton(AF_INET, addr, &sin->sin_addr.s_addr) != 1)
+		psc_fatal("inet_pton");
+
+	if (forcesdp && sin->sin_family == AF_INET)
+		sin->sin_family = AF_INET_SDP;
+
+	s = socket(sin->sin_family, SOCK_STREAM, 0);
+	if (s == -1)
+		psc_fatal("socket");
+	if (connect(s, (struct sockaddr *)sin, sizeof(*sin)) == -1)
+		psc_fatal("connect");
 
 	memset(&sarg, 0, sizeof(sarg));
-	sarg.s = clisock;
+	sarg.s = s;
 	if ((rc = pthread_create(&rdthr, NULL, worker_main, &sarg)) != 0)
 		psc_fatalx("pthread_create: %s", strerror(rc));
-	ioloop(clisock, IOF_WR);
+	ioloop(s, IOF_WR);
 }
 
 int
