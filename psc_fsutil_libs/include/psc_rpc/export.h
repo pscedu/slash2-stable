2d1e419302269cb1bef6c5a4d60c813ffe02270d
Jared Yanovich <yanovich@psc.edu>
2009-02-18 20:00:09 +0000

    clang complains about the FMTSTR stuff but nothing else in zestland, except for some of the return values here, so change them from macros to static inlines
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5243 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_rpc/export.h
+++ psc_fsutil_libs/include/psc_rpc/export.h
@@ -3,46 +3,54 @@
 #ifndef _PFL_EXPORT_H_
 #define _PFL_EXPORT_H_
 
+#include "psc_util/assert.h"
 #include "psc_util/atomic.h"
 #include "psc_util/log.h"
 
-#define pscrpc_export_rpc_get(exp)                                      \
-	({								\
-		atomic_inc(&(exp)->exp_rpc_count);			\
-		psc_trace("RPC GETting export %p : new rpc_count %d\n",	\
-			(exp), atomic_read(&(exp)->exp_rpc_count));	\
-		pscrpc_export_get(exp);					\
-	})
-
-#define pscrpc_export_rpc_put(exp)                                      \
-	({								\
-		atomic_dec(&(exp)->exp_rpc_count);			\
-		psc_trace("RPC PUTting export %p : new rpc_count %d\n",	\
-			(exp), atomic_read(&(exp)->exp_rpc_count));	\
-		pscrpc_export_put(exp);					\
-	})
-
-#define pscrpc_export_get(exp)						\
-	({								\
-		struct pscrpc_export *exp_ = exp;			\
-		atomic_inc(&exp_->exp_refcount);			\
-		psc_trace("GETting export %p : new refcount %d\n", exp_,\
-			atomic_read(&exp_->exp_refcount));		\
-		exp_;							\
-	})
-
-#define pscrpc_export_put(exp)						\
-	do {								\
-		LASSERT((exp) != NULL);					\
-		psc_trace("PUTting export %p : new refcount %d\n", (exp),\
-			atomic_read(&(exp)->exp_refcount) - 1);		\
-		LASSERT(atomic_read(&(exp)->exp_refcount) > 0);		\
-		LASSERT(atomic_read(&(exp)->exp_refcount) < 0x5a5a5a);	\
-		__pscrpc_export_put(exp);				\
-	} while (0)
-
 struct pscrpc_export;
-extern void 
-__pscrpc_export_put(struct pscrpc_export *exp);
+
+void __pscrpc_export_put(struct pscrpc_export *);
+
+static inline struct pscrpc_export *
+pscrpc_export_get(struct pscrpc_export *exp)
+{
+	int rc;
+
+	rc = atomic_inc_return(&exp->exp_refcount);
+	psc_trace("GETting export %p : new refcount %d", exp, rc);
+	return (exp);
+}
+
+static inline struct pscrpc_export *
+pscrpc_export_rpc_get(struct pscrpc_export *exp)
+{
+	int rc;
+
+	refcnt = atomic_inc_return(&exp->exp_rpc_count);
+	psc_trace("RPC GETting export %p : new rpc_count %d", exp, rc);
+	return (pscrpc_export_get(exp));
+}
+
+static inline void
+pscrpc_export_put(struct pscrpc_export *exp)
+{
+	int rc;
+
+	rc = atomic_read(&exp->exp_refcount);
+	psc_trace("PUTting export %p : new refcount %d", exp, rc - 1);
+	psc_assert(rc > 0);
+	psc_assert(rc < 0x5a5a5a);
+	__pscrpc_export_put(exp);
+}
+
+static inline void
+pscrpc_export_rpc_put(struct pscrpc_export *exp)
+{
+	int rc;
+
+	rc = atomic_dec_return(&exp->exp_rpc_count);
+	psc_trace("RPC PUTting export %p : new rpc_count %d", exp, rc);
+	pscrpc_export_put(exp);
+}
 
 #endif /* _PFL_EXPORT_H_ */
