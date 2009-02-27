cf81a6f8ad9c78a502ddbf5a3cd4afc4fe2099b3
Jared Yanovich <yanovich@psc.edu>
2009-02-27 23:05:48 +0000

    fix off-by-one
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5410 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_util/humanscale.c
+++ psc_fsutil_libs/psc_util/humanscale.c
@@ -13,7 +13,7 @@ psc_humanscale(char buf[PSC_CTL_HUMANBUF_SZ], double num)
 	 * 1234567
 	 * 1000.3K
 	 */
-	for (mag = 0; num > 1024.0; mag++)
+	for (mag = 0; num >= 1024.0; mag++)
 		num /= 1024.0;
 	if (mag > 6)
 		snprintf(buf, sizeof(buf), "%.1e", num);
