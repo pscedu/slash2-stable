0037ebd3e2ef8f855514d63c2fea1627ed422bc4
Jared Yanovich <yanovich@psc.edu>
2009-03-03 22:59:38 +0000

    copy proper fuse pkgconfig invocations from mount_zest and add local conf for wolverine
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5446 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -13,8 +13,12 @@ PKG_CONFIG=	PKG_CONFIG_PATH=/usr/local/lib/pkgconfig pkg-config
 LKERNEL_BASE=	${ROOTDIR}/kernel/2.6.9-42.0.8.EL_lustre.1.4.9.1
 
 ifeq ($(wildcard /opt/xt-os),)
- # for ZESTIONs
- KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-x86_64
+ ifeq ($(wildcard /wolverine_pool),)
+   # for ZESTIONs
+   KERNEL_BASE=	/usr/src/kernels/2.6.22.14-72.fc6-x86_64
+ else
+   KERNEL_BASE=	/usr/src/kernels/2.6.18-92.el5-xen-x86_64/
+ endif
 else
  ifeq ($(wildcard /opt/sgi),)
   # on xt3
