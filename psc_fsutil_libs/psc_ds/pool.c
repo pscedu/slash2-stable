6a1061385335af7afc781239b9f59aa3dac93a16
Jared Yanovich <yanovich@psc.edu>
2009-03-07 01:35:28 +0000

    improve the pool return auto resize threshold calculation
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5475 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_ds/pool.c
+++ psc_fsutil_libs/psc_ds/pool.c
@@ -488,7 +488,8 @@ psc_pool_return(struct psc_poolmgr *m, void *p)
 	locked = POOL_RLOCK(m);
 	if ((m->ppm_flags & PPMF_AUTO) &&
 	    ((m->ppm_max && m->ppm_max < m->ppm_total) ||
-	     m->ppm_lg.plg_size * 100 < m->ppm_total * m->ppm_thres)) {
+	     m->ppm_lg.plg_size * 100 < m->ppm_thres * (m->ppm_max ?
+	      MIN(m->ppm_max, m->ppm_total) : m->ppm_total))) {
 		m->ppm_total--;
 		POOL_URLOCK(m, locked);
 		_psc_pool_destroy_obj(m, p);
