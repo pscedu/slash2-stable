c164a96a1c397bdf7ddc2dff115d6e23230b7bfc
Jared Yanovich <yanovich@psc.edu>
2009-03-09 23:11:59 +0000

    fix a thinko in the pool auto resize threshold return check
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5484 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_ds/pool.c
+++ psc_fsutil_libs/psc_ds/pool.c
@@ -489,7 +489,7 @@ psc_pool_return(struct psc_poolmgr *m, void *p)
 	if ((m->ppm_flags & PPMF_AUTO) &&
 	    ((m->ppm_max && m->ppm_max < m->ppm_total) ||
 	     m->ppm_lg.plg_size * 100 < m->ppm_thres * (m->ppm_max ?
-	      MIN(m->ppm_max, m->ppm_total) : m->ppm_total))) {
+	      MAX(m->ppm_max, m->ppm_total) : m->ppm_total))) {
 		m->ppm_total--;
 		POOL_URLOCK(m, locked);
 		_psc_pool_destroy_obj(m, p);
