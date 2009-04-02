6b4912be8624d05ce49cc0bd80af10bb305a99e5
Jared Yanovich <yanovich@psc.edu>
2009-04-02 00:11:50 +0000

    misc. fixes
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5730 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/lock2.h
+++ psc_fsutil_libs/include/psc_util/lock2.h
@@ -75,8 +75,8 @@ psc_spin_trylock(struct psc_spinlock *psl)
 	int16_t oldval;
 
 	PSL_MAGIC_CHECK();
-	oldval = psc_atomic16_setmask(&psl->psl_value, PSL_LOCKMASK);
-	if (oldval & PSL_LOCKMASK)
+	oldval = psc_atomic16_setmask_ret(&psl->psl_value, PSL_LOCKMASK);
+	if (oldval & PSL_LOCKMASK) {
 		if ((oldval & PSL_OWNERMASK) == pscthr_gettid())
 			psc_fatalx("already holding the lock");
 		return (0);				/* someone else has it */
@@ -86,8 +86,8 @@ psc_spin_trylock(struct psc_spinlock *psl)
 	return (1);
 }
 
-#define MAX_SPIN_CNT		100
-#define SPIN_SLEEP_DURATION	2000001
+#define PSL_SPIN_THRES		100
+#define PSL_SPIN_SLEEP		(2 * 1000 * 1000 + 1)	/* nsec */
 
 static inline void
 psc_spin_lock(struct psc_spinlock *psl)
@@ -96,13 +96,13 @@ psc_spin_lock(struct psc_spinlock *psl)
 	int i = 0;
 
 	while (!psc_spin_trylock(psl)) {
-		if (i < MAX_SPIN_CNT) {
+		if (i < PSL_SPIN_THRES) {
 			sched_yield();
 			i++;
 		} else {
 			tm.tv_sec = 0;
-			tm.tv_nsec = SPIN_SLEEP_DURATION;
-			nanosleep(&tm, 0);
+			tm.tv_nsec = PSL_SPIN_SLEEP;
+			nanosleep(&tm, NULL);
 			i = 0;
 		}
 	}
@@ -111,8 +111,11 @@ psc_spin_lock(struct psc_spinlock *psl)
 static inline int
 psc_spin_reqlock(struct psc_spinlock *psl)
 {
-	if ((psl->psl_value & PSL_LOCKMASK) &&
-	    (psl->psl_value & PSL_OWNERMASK) == pscthr_gettid())
+	int v;
+
+	v = psc_atomic16_read(&psl->psl_value);
+	if ((v & PSL_LOCKMASK) &&
+	    (v & PSL_OWNERMASK) == pscthr_gettid())
 		return (1);
 	psc_spin_lock(psl);
 	return (0);
@@ -121,8 +124,11 @@ psc_spin_reqlock(struct psc_spinlock *psl)
 static inline int
 psc_spin_tryreqlock(struct psc_spinlock *psl, int *locked)
 {
-	if ((psl->psl_value & PSL_LOCKMASK) &&
-	    (psl->psl_value & PSL_OWNERMASK) == pscthr_gettid()) {
+	int v;
+
+	v = psc_atomic16_read(&psl->psl_value);
+	if ((v & PSL_LOCKMASK) &&
+	    (v & PSL_OWNERMASK) == pscthr_gettid()) {
 		*locked = 1;
 		return (1);
 	}
@@ -130,11 +136,10 @@ psc_spin_tryreqlock(struct psc_spinlock *psl, int *locked)
 	return (psc_spin_trylock(psl));
 }
 
-static inline int
-psc_spin_ureqlock(struct psc_spinlock *psl, int locked)
-{
-	if (!locked)
-		psc_spin_unlock(psl);
-}
+#define psc_spin_ureqlock(psl, locked)					\
+	do {								\
+		if (!(locked))						\
+			psc_spin_unlock(psl);				\
+	} while (0)
 
 #endif /* _PFL_LOCK_H_ */
