952c5fad171b4563e94be3aa879e61f1bacf1246
Jared Yanovich <yanovich@psc.edu>
2009-04-02 22:22:28 +0000

    quiet this test and add tests for non-multiple-of-8 bitmaps
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5741 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/tests/vbitmap/vbitmap.c
+++ psc_fsutil_libs/tests/vbitmap/vbitmap.c
@@ -34,48 +34,43 @@ main(int argc, char *argv[])
 			usage();
 		}
 
-	if ((vb = vbitmap_new(NELEM)) == NULL)
+	argc -= optind;
+	if (argc)
+		usage();
+
+	if ((vb = vbitmap_new(213)) == NULL)
+		err(1, "vbitmap_new");
+	for (i = 0; i < 213; i++)
+		if (!vbitmap_next(vb, &elem))
+			errx(1, "out of elements at pos %d", i);
+
+	if (vbitmap_next(vb, &elem))
+		errx(1, "got another expected unused elem! %zu\n", elem);
+
+	if (vbitmap_resize(vb, NELEM) == -1)
 		err(1, "vbitmap_new");
 
 	/* fill up bitmap */
-	for (i = 0; i < NELEM; i++)
-		if (vbitmap_next(vb, &elem)) {
-			if ((i % 1000) == 0)
-				printf("got unused elem: %zu\n", elem);
-		} else
-			errx(1, "out of elements!");
+	for (; i < NELEM; i++)
+		if (!vbitmap_next(vb, &elem))
+			errx(1, "out of elements at pos %d", i);
 
 	/* try one past end of filled bitmap */
-	printf("expected to have filled up bitmap\n\n");
 	if (vbitmap_next(vb, &elem))
-		printf("got another expected unused elem! %zu\n", elem);
-	else
-		printf("rightfully out of elements\n");
+		errx(1, "got another expected unused elem! %zu\n", elem);
 
 	/* free some slots */
-	printf("\nfreeing some slots\n");
-	for (elem = 0; elem < NELEM; elem += NELEM / 10) {
-		printf("putting back elem: %zu\n", elem);
+	for (elem = 0; elem < NELEM; elem += NELEM / 10)
 		vbitmap_unset(vb, elem);
-	}
-
-	printf("\n");
-	//vbitmap_printhex(vb);
 
 	/* try to re-grab the freed slots */
-	printf("\nregrabbing %d freed slots\n", vbitmap_nfree(vb));
 	for (i = 0; i <= 10; i++)
-		if (vbitmap_next(vb, &elem))
-			printf("got unused elem: %zu\n", elem);
-		else
-			errx(1, "out of elements (%d/%d)!", i, 10);
+		if (!vbitmap_next(vb, &elem))
+			errx(1, "out of elements, request %d/%d", i, 10);
 
 	/* try one past end of filled bitmap */
-	printf("expected to have filled up bitmap\n\n");
 	if (vbitmap_next(vb, &elem))
-		printf("got another expected unused elem! %zu\n", elem);
-	else
-		printf("rightfully out of elements\n");
+		errx(1, "got another expected unused elem! %zu\n", elem);
 
 	vbitmap_free(vb);
 	exit(0);
