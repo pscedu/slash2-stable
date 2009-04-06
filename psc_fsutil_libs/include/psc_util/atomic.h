6eaeb20a06f7161cd837dc6395efa2484166845b
Jared Yanovich <yanovich@psc.edu>
2009-04-06 23:55:00 +0000

    introduce psc_atomic32 for congruency with atomic64 and atomic16
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@5768 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/atomic.h
+++ psc_fsutil_libs/include/psc_util/atomic.h
@@ -18,17 +18,21 @@
 #define ia64_cmpxchg_called_with_bad_pointer()	 ({ psc_fatalx("ia64_cmpxchg_called_with_bad_pointer"); 0; })
 #define ia64_xchg_called_with_bad_pointer()	 psc_fatalx("ia64_xchg_called_with_bad_pointer")
 
-typedef struct { volatile __s32 counter; } atomic_t;
-typedef struct { volatile __s64 counter; } psc_atomic64_t;
+typedef struct { volatile __s32 value; } atomic_t;
+typedef struct { volatile __s32 value; } psc_atomic32_t;
+typedef struct { volatile __s64 value; } psc_atomic64_t;
 
 #define ATOMIC_INIT(i)				{ (i) }
+#define PSC_ATOMIC32_INIT(i)			{ (i) }
 #define PSC_ATOMIC64_INIT(i)			{ (i) }
 
-#define atomic_read(v)				((v)->counter)
-#define psc_atomic64_read(v)			((v)->counter)
+#define atomic_read(v)				((v)->value)
+#define psc_atomic32_read(v)			((v)->value)
+#define psc_atomic64_read(v)			((v)->value)
 
-#define atomic_set(v, i)			(((v)->counter) = (i))
-#define psc_atomic64_set(v, i)			(((v)->counter) = (i))
+#define atomic_set(v, i)			(((v)->value) = (i))
+#define psc_atomic32_set(v, i)			(((v)->value) = (i))
+#define psc_atomic64_set(v, i)			(((v)->value) = (i))
 
 static __inline int
 ia64_atomic_add(int i, atomic_t *v)
@@ -95,7 +99,20 @@ ia64_atomic64_sub(__s64 i, psc_atomic64_t *v)
 	     || (__ia64_aar_i ==  8) || (__ia64_aar_i ==  16)		\
 	     || (__ia64_aar_i == -1) || (__ia64_aar_i ==  -4)		\
 	     || (__ia64_aar_i == -8) || (__ia64_aar_i == -16)))		\
-		? ia64_fetch_and_add(__ia64_aar_i, &(v)->counter)	\
+		? ia64_fetch_and_add(__ia64_aar_i, &(v)->value)	\
+		: ia64_atomic_add(__ia64_aar_i, (v));			\
+})
+
+#define psc_atomic32_add_return(v, i)					\
+({									\
+	int __ia64_aar_i = (i);						\
+									\
+	(__builtin_constant_p(i)					\
+	 && (   (__ia64_aar_i ==  1) || (__ia64_aar_i ==   4)		\
+	     || (__ia64_aar_i ==  8) || (__ia64_aar_i ==  16)		\
+	     || (__ia64_aar_i == -1) || (__ia64_aar_i ==  -4)		\
+	     || (__ia64_aar_i == -8) || (__ia64_aar_i == -16)))		\
+		? ia64_fetch_and_add(__ia64_aar_i, &(v)->value)		\
 		: ia64_atomic_add(__ia64_aar_i, (v));			\
 })
 
@@ -108,7 +125,7 @@ ia64_atomic64_sub(__s64 i, psc_atomic64_t *v)
 	     || (__ia64_aar_i ==  8) || (__ia64_aar_i ==  16)		\
 	     || (__ia64_aar_i == -1) || (__ia64_aar_i ==  -4)		\
 	     || (__ia64_aar_i == -8) || (__ia64_aar_i == -16)))		\
-		? ia64_fetch_and_add(__ia64_aar_i, &(v)->counter)	\
+		? ia64_fetch_and_add(__ia64_aar_i, &(v)->value)	\
 		: ia64_atomic64_add(__ia64_aar_i, (v));			\
 })
 
@@ -121,7 +138,20 @@ ia64_atomic64_sub(__s64 i, psc_atomic64_t *v)
 	     || (__ia64_asr_i ==   8) || (__ia64_asr_i ==  16)		\
 	     || (__ia64_asr_i ==  -1) || (__ia64_asr_i ==  -4)		\
 	     || (__ia64_asr_i ==  -8) || (__ia64_asr_i == -16)))	\
-		? ia64_fetch_and_add(-__ia64_asr_i, &(v)->counter)	\
+		? ia64_fetch_and_add(-__ia64_asr_i, &(v)->value)	\
+		: ia64_atomic_sub(__ia64_asr_i, (v));			\
+})
+
+#define psc_atomic32_sub_return(v, i)					\
+({									\
+	int __ia64_asr_i = (i);						\
+									\
+	(__builtin_constant_p(i)					\
+	 && (   (__ia64_asr_i ==   1) || (__ia64_asr_i ==   4)		\
+	     || (__ia64_asr_i ==   8) || (__ia64_asr_i ==  16)		\
+	     || (__ia64_asr_i ==  -1) || (__ia64_asr_i ==  -4)		\
+	     || (__ia64_asr_i ==  -8) || (__ia64_asr_i == -16)))	\
+		? ia64_fetch_and_add(-__ia64_asr_i, &(v)->value)	\
 		: ia64_atomic_sub(__ia64_asr_i, (v));			\
 })
 
@@ -134,29 +164,42 @@ ia64_atomic64_sub(__s64 i, psc_atomic64_t *v)
 	     || (__ia64_asr_i ==   8) || (__ia64_asr_i ==  16)		\
 	     || (__ia64_asr_i ==  -1) || (__ia64_asr_i ==  -4)		\
 	     || (__ia64_asr_i ==  -8) || (__ia64_asr_i == -16)))	\
-		? ia64_fetch_and_add(-__ia64_asr_i, &(v)->counter)	\
+		? ia64_fetch_and_add(-__ia64_asr_i, &(v)->value)	\
 		: ia64_atomic64_sub(__ia64_asr_i, (v));			\
 })
 
 #define atomic_dec_return(v)			atomic_sub_return(1, (v))
 #define atomic_inc_return(v)			atomic_add_return(1, (v))
+#define psc_atomic32_dec_return(v)		psc_atomic32_sub_return((v), 1)
+#define psc_atomic32_inc_return(v)		psc_atomic32_add_return((v), 1)
 #define psc_atomic64_dec_return(v)		psc_atomic64_sub_return(1, (v))
 #define psc_atomic64_inc_return(v)		psc_atomic64_add_return(1, (v))
 
 #define atomic_dec_and_test(v)			(atomic_sub_return(1, (v)) == 0)
+#define psc_atomic32_dec_test_zero(v)		(psc_atomic32_sub_return((v), 1) == 0)
+#define psc_atomic64_dec_test_zero(v)		(psc_atomic64_sub_return((v), 1) == 0)
 
 #define atomic_add(i, v)			atomic_add_return((i), (v))
 #define atomic_sub(i, v)			atomic_sub_return((i), (v))
 #define atomic_inc(v)				atomic_add(1, (v))
 #define atomic_dec(v)				atomic_sub(1, (v))
 
-#define psc_atomic64_add(i, v)			psc_atomic64_add_return((i), (v))
-#define psc_atomic64_sub(i, v)			psc_atomic64_sub_return((i), (v))
+#define psc_atomic32_add(v, i)			psc_atomic32_add_return((v), (i))
+#define psc_atomic32_sub(v, i)			psc_atomic32_sub_return((v), (i))
+#define psc_atomic32_inc(v)			psc_atomic32_add((v), 1)
+#define psc_atomic32_dec(v)			psc_atomic32_sub((v), 1)
+
+#define psc_atomic64_add(v, i)			psc_atomic64_add_return((i), (v))
+#define psc_atomic64_sub(v, i)			psc_atomic64_sub_return((i), (v))
 #define psc_atomic64_inc(v)			psc_atomic64_add(1, (v))
 #define psc_atomic64_dec(v)			psc_atomic64_sub(1, (v))
 
-#define atomic_cmpxchg(v, old, new)		((int)cmpxchg(&((v)->counter), (old), (new)))
-#define atomic_xchg(v, new)			(xchg(&((v)->counter), (new)))
+#define atomic_cmpxchg(v, old, new)		((int)cmpxchg(&((v)->value), (old), (new)))
+#define atomic_xchg(v, new)			(xchg(&((v)->value), (new)))
+#define psc_atomic32_cmpxchg(v, old, new)	((int)cmpxchg(&((v)->value), (old), (new)))
+#define psc_atomic32_xchg(v, new)		(xchg(&((v)->value), (new)))
+#define psc_atomic64_cmpxchg(v, old, new)	((int)cmpxchg(&((v)->value), (old), (new)))
+#define psc_atomic64_xchg(v, new)		(xchg(&((v)->value), (new)))
 
 static __inline int
 atomic_set_mask(unsigned int mask, atomic_t *v)
@@ -173,7 +216,21 @@ atomic_set_mask(unsigned int mask, atomic_t *v)
 }
 
 static __inline int
-atomic_clear_mask(unsigned int mask, atomic_t *v)
+psc_atomic32_set_mask(psc_atomic32_t *v, unsigned int mask)
+{
+	uint32_t old, new;
+	CMPXCHG_BUGCHECK_DECL
+
+	do {
+		CMPXCHG_BUGCHECK(v);
+		old = psc_atomic32_read(v);
+		new = old | mask;
+	} while (ia64_cmpxchg(acq, v, old, new, sizeof(atomic_t)) != old);
+	return (new);
+}
+
+static __inline int
+atomic_clear_mask(atomic_t *v, uint32_t mask)
 {
 	uint32_t old, new;
 	CMPXCHG_BUGCHECK_DECL
@@ -186,8 +243,22 @@ atomic_clear_mask(unsigned int mask, atomic_t *v)
 	return (new);
 }
 
+static __inline int
+atomic_clear_mask(atomic_t *v, uint32_t mask)
+{
+	uint32_t old, new;
+	CMPXCHG_BUGCHECK_DECL
+
+	do {
+		CMPXCHG_BUGCHECK(v);
+		old = psc_atomic32_read(v);
+		new = old & ~mask;
+	} while (ia64_cmpxchg(acq, v, old, new, sizeof(atomic_t)) != old);
+	return (new);
+}
+
 static __inline int64_t
-psc_atomic64_set_mask(uint64_t mask, psc_atomic64_t *v)
+psc_atomic64_set_mask(psc_atomic64_t *v, uint64_t mask)
 {
 	uint64_t new, old;
 	CMPXCHG_BUGCHECK_DECL
@@ -217,13 +288,15 @@ psc_atomic64_set_mask(uint64_t mask, psc_atomic64_t *v)
  * on us. We need to use _exactly_ the address the user gave us,
  * not some alias that contains the same information.
  */
-typedef struct { volatile int counter; } atomic_t;
-typedef struct { volatile int64_t counter; } psc_atomic64_t;
-typedef struct { volatile int32_t value; } psc_atomic16_t;
+typedef struct { volatile int value; } atomic_t;
+typedef struct { volatile int16_t value; } psc_atomic16_t;
+typedef struct { volatile int32_t value; } psc_atomic32_t;
+typedef struct { volatile int64_t value; } psc_atomic64_t;
 
 #define ATOMIC_INIT(i)			{ (i) }
-#define PSC_ATOMIC64_INIT(i)		{ (i) }
 #define PSC_ATOMIC16_INIT(i)		{ (i) }
+#define PSC_ATOMIC32_INIT(i)		{ (i) }
+#define PSC_ATOMIC64_INIT(i)		{ (i) }
 
 /**
  * atomic_read - read atomic variable
@@ -231,9 +304,10 @@ typedef struct { volatile int32_t value; } psc_atomic16_t;
  *
  * Atomically reads the value of @v.
  */
-#define atomic_read(v)			((v)->counter)
-#define psc_atomic64_read(v)		((v)->counter)
+#define atomic_read(v)			((v)->value)
 #define psc_atomic16_read(v)		((v)->value)
+#define psc_atomic32_read(v)		((v)->value)
+#define psc_atomic64_read(v)		((v)->value)
 
 /**
  * atomic_set - set atomic variable
@@ -242,9 +316,10 @@ typedef struct { volatile int32_t value; } psc_atomic16_t;
  *
  * Atomically sets the value of @v to @i.
  */
-#define atomic_set(v, i)		(((v)->counter) = (i))
-#define psc_atomic64_set(v, i)		(((v)->counter) = (i))
+#define atomic_set(v, i)		(((v)->value) = (i))
 #define psc_atomic16_set(v, i)		(((v)->value) = (i))
+#define psc_atomic32_set(v, i)		(((v)->value) = (i))
+#define psc_atomic64_set(v, i)		(((v)->value) = (i))
 
 /**
  * atomic_add - add integer to atomic variable
@@ -258,8 +333,17 @@ atomic_add(int i, atomic_t *v)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "addl %1,%0"
-		:"=m" (v->counter)
-		:"ir" (i), "m" (v->counter));
+		:"=m" (v->value)
+		:"ir" (i), "m" (v->value));
+}
+
+static __inline void
+psc_atomic32_add(psc_atomic32_t *v, int32_t i)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "addl %1,%0"
+		:"=m" (v->value)
+		:"ir" (i), "m" (v->value));
 }
 
 /**
@@ -270,12 +354,12 @@ atomic_add(int i, atomic_t *v)
  * Atomically adds @i to @v.
  */
 static __inline void
-psc_atomic64_add(int64_t i, psc_atomic64_t *v)
+psc_atomic64_add(psc_atomic64_t *v, int64_t i)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "addq %1,%0"
-		:"=m" (v->counter)
-		:"ir" (i), "m" (v->counter));
+		:"=m" (v->value)
+		:"ir" (i), "m" (v->value));
 }
 
 /**
@@ -290,8 +374,17 @@ atomic_sub(int i, atomic_t *v)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "subl %1,%0"
-		:"=m" (v->counter)
-		:"ir" (i), "m" (v->counter));
+		:"=m" (v->value)
+		:"ir" (i), "m" (v->value));
+}
+
+static __inline void
+psc_atomic32_sub(psc_atomic32_t *v, int32_t i)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "subl %1,%0"
+		:"=m" (v->value)
+		:"ir" (i), "m" (v->value));
 }
 
 /**
@@ -302,12 +395,12 @@ atomic_sub(int i, atomic_t *v)
  * Atomically subtracts @i from @v.
  */
 static __inline void
-psc_atomic64_sub(int64_t i, psc_atomic64_t *v)
+psc_atomic64_sub(psc_atomic64_t *v, int64_t i)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "subq %1,%0"
-		:"=m" (v->counter)
-		:"ir" (i), "m" (v->counter));
+		:"=m" (v->value)
+		:"ir" (i), "m" (v->value));
 }
 
 /**
@@ -326,8 +419,20 @@ atomic_sub_and_test(int i, atomic_t *v)
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "subl %2,%0; sete %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"ir" (i), "m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"ir" (i), "m" (v->value) : "memory");
+	return c;
+}
+
+static __inline int
+psc_atomic32_sub_test_zero(psc_atomic32_t *v, int32_t i)
+{
+	unsigned char c;
+
+	__asm__ __volatile__(
+		LOCK_PREFIX "subl %2,%0; sete %1"
+		:"=m" (v->value), "=qm" (c)
+		:"ir" (i), "m" (v->value) : "memory");
 	return c;
 }
 
@@ -341,14 +446,14 @@ atomic_sub_and_test(int i, atomic_t *v)
  * other cases.
  */
 static __inline int
-psc_atomic64_sub_and_test(int64_t i, psc_atomic64_t *v)
+psc_atomic64_sub_and_test(psc_atomic64_t *v, int64_t i)
 {
 	unsigned char c;
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "subq %2,%0; sete %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"ir" (i), "m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"ir" (i), "m" (v->value) : "memory");
 	return c;
 }
 
@@ -363,8 +468,17 @@ atomic_inc(atomic_t *v)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "incl %0"
-		:"=m" (v->counter)
-		:"m" (v->counter));
+		:"=m" (v->value)
+		:"m" (v->value));
+}
+
+static __inline void
+psc_atomic32_inc(psc_atomic32_t *v)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "incl %0"
+		:"=m" (v->value)
+		:"m" (v->value));
 }
 
 /**
@@ -378,8 +492,8 @@ psc_atomic64_inc(psc_atomic64_t *v)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "incq %0"
-		:"=m" (v->counter)
-		:"m" (v->counter));
+		:"=m" (v->value)
+		:"m" (v->value));
 }
 
 /**
@@ -393,8 +507,17 @@ atomic_dec(atomic_t *v)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "decl %0"
-		:"=m" (v->counter)
-		:"m" (v->counter));
+		:"=m" (v->value)
+		:"m" (v->value));
+}
+
+static __inline void
+psc_atomic32_dec(psc_atomic32_t *v)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "decl %0"
+		:"=m" (v->value)
+		:"m" (v->value));
 }
 
 /**
@@ -408,8 +531,8 @@ psc_atomic64_dec(psc_atomic64_t *v)
 {
 	__asm__ __volatile__(
 		LOCK_PREFIX "decq %0"
-		:"=m" (v->counter)
-		:"m" (v->counter));
+		:"=m" (v->value)
+		:"m" (v->value));
 }
 
 /**
@@ -427,8 +550,20 @@ atomic_dec_and_test(atomic_t *v)
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "decl %0; sete %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"m" (v->value) : "memory");
+	return c != 0;
+}
+
+static __inline int
+psc_atomic32_dec_test_zero(psc_atomic32_t *v)
+{
+	unsigned char c;
+
+	__asm__ __volatile__(
+		LOCK_PREFIX "decl %0; sete %1"
+		:"=m" (v->value), "=qm" (c)
+		:"m" (v->value) : "memory");
 	return c != 0;
 }
 
@@ -447,8 +582,8 @@ psc_atomic64_dec_and_test(psc_atomic64_t *v)
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "decq %0; sete %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"m" (v->value) : "memory");
 	return c != 0;
 }
 
@@ -467,8 +602,20 @@ atomic_inc_and_test(atomic_t *v)
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "incl %0; sete %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"m" (v->value) : "memory");
+	return c != 0;
+}
+
+static __inline int
+psc_atomic32_inc_test_zero(psc_atomic32_t *v)
+{
+	unsigned char c;
+
+	__asm__ __volatile__(
+		LOCK_PREFIX "incl %0; sete %1"
+		:"=m" (v->value), "=qm" (c)
+		:"m" (v->value) : "memory");
 	return c != 0;
 }
 
@@ -487,8 +634,8 @@ psc_atomic64_inc_and_test(psc_atomic64_t *v)
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "incq %0; sete %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"m" (v->value) : "memory");
 	return c != 0;
 }
 
@@ -508,8 +655,20 @@ atomic_add_negative(int i, atomic_t *v)
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "addl %2,%0; sets %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"ir" (i), "m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"ir" (i), "m" (v->value) : "memory");
+	return c;
+}
+
+static __inline int
+psc_atomic32_add_negative(psc_atomic32_t *v, int i)
+{
+	unsigned char c;
+
+	__asm__ __volatile__(
+		LOCK_PREFIX "addl %2,%0; sets %1"
+		:"=m" (v->value), "=qm" (c)
+		:"ir" (i), "m" (v->value) : "memory");
 	return c;
 }
 
@@ -523,14 +682,14 @@ atomic_add_negative(int i, atomic_t *v)
  * result is greater than or equal to zero.
  */
 static __inline int
-psc_atomic64_add_negative(int64_t i, psc_atomic64_t *v)
+psc_atomic64_add_negative(psc_atomic64_t *v, int64_t i)
 {
 	unsigned char c;
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "addq %2,%0; sets %1"
-		:"=m" (v->counter), "=qm" (c)
-		:"ir" (i), "m" (v->counter) : "memory");
+		:"=m" (v->value), "=qm" (c)
+		:"ir" (i), "m" (v->value) : "memory");
 	return c;
 }
 
@@ -548,7 +707,19 @@ atomic_add_return(int i, atomic_t *v)
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "xaddl %0, %1"
-		:"+r" (i), "+m" (v->counter)
+		:"+r" (i), "+m" (v->value)
+		: : "memory");
+	return i + __i;
+}
+
+static __inline int
+psc_atomic32_add_return(psc_atomic32_t *v, int32_t i)
+{
+	int __i = i;
+
+	__asm__ __volatile__(
+		LOCK_PREFIX "xaddl %0, %1"
+		:"+r" (i), "+m" (v->value)
 		: : "memory");
 	return i + __i;
 }
@@ -561,24 +732,30 @@ atomic_add_return(int i, atomic_t *v)
  * Atomically adds @i to @v and returns @i + @v
  */
 static __inline int64_t
-psc_atomic64_add_return(int64_t i, psc_atomic64_t *v)
+psc_atomic64_add_return(psc_atomic64_t *v, int64_t i)
 {
 	int64_t __i = i;
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "xaddq %0, %1;"
 		:"=r"(i)
-		:"m"(v->counter), "0"(i));
+		:"m"(v->value), "0"(i));
 	return i + __i;
 }
 
 #define atomic_sub_return(i, v)		atomic_add_return(-(i), (v))
-#define psc_atomic64_sub_return(i, v)	psc_atomic64_add_return(-(i), (v))
+#define psc_atomic16_sub_return(v, i)	psc_atomic16_add_return((v), -(i))
+#define psc_atomic32_sub_return(v, i)	psc_atomic32_add_return((v), -(i))
+#define psc_atomic64_sub_return(v, i)	psc_atomic64_add_return((v), -(i))
 
-#define atomic_cmpxchg(v, old, new)	((int)cmpxchg(&((v)->counter), (old), (new)))
-#define atomic_xchg(v, new)		(xchg(&((v)->counter), (new)))
-
+#define atomic_cmpxchg(v, old, new)		((int)cmpxchg(&((v)->value), (old), (new)))
+#define atomic_xchg(v, new)			(xchg(&((v)->value), (new)))
 #define psc_atomic16_cmpxchg(v, old, new)	((int16_t)cmpxchg(&((v)->value), (old), (new)))
+#define psc_atomic16_xchg(v, new)		(xchg(&((v)->value), (new)))
+#define psc_atomic32_cmpxchg(v, old, new)	((int32_t)cmpxchg(&((v)->value), (old), (new)))
+#define psc_atomic32_xchg(v, new)		(xchg(&((v)->value), (new)))
+#define psc_atomic64_cmpxchg(v, old, new)	((int64_t)cmpxchg(&((v)->value), (old), (new)))
+#define psc_atomic64_xchg(v, new)		(xchg(&((v)->value), (new)))
 
 /**
  * atomic_add_unless - add unless the number is a given value
@@ -590,7 +767,7 @@ psc_atomic64_add_return(int64_t i, psc_atomic64_t *v)
  * Returns non-zero if @v was not @u, and zero otherwise.
  */
 #define atomic_add_unless(v, a, u)				\
-({								\
+do {								\
 	int c, old;						\
 								\
 	c = atomic_read(v);					\
@@ -603,11 +780,15 @@ psc_atomic64_add_return(int64_t i, psc_atomic64_t *v)
 		c = old;					\
 	}							\
 	c != (u);						\
-})
+} while (0)
+
 #define atomic_inc_not_zero(v)	atomic_add_unless((v), 1, 0)
 
 #define atomic_inc_return(v)	atomic_add_return(1, (v))
 #define atomic_dec_return(v)	atomic_sub_return(1, (v))
+
+#define psc_atomic32_inc_return(v)	psc_atomic32_add_return((v), 1)
+#define psc_atomic32_dec_return(v)	psc_atomic32_sub_return((v), 1)
 
 #define atomic_clear_mask(mask, addr)				\
 	__asm__ __volatile__(					\
@@ -615,11 +796,29 @@ psc_atomic64_add_return(int64_t i, psc_atomic64_t *v)
 		: : "r" (~(mask)),				\
 		    "m" (*(addr)) : "memory")
 
+static __inline void
+psc_atomic32_clear_mask(psc_atomic32_t *v, int32_t mask)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "andl %0,%1"
+		: : "r" (~(mask)),
+		    "m" (v->value) : "memory");
+}
+
 #define atomic_set_mask(mask, addr)				\
 	__asm__ __volatile__(					\
 		LOCK_PREFIX "orl %0,%1"				\
 		: : "r" (mask),					\
 		    "m" (*(addr)) : "memory")
+
+static __inline void
+psc_atomic32_set_mask(psc_atomic32_t *v, int32_t mask)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "orl %0,%1"
+		: : "r" (mask),
+		    "m" (v->value) : "memory");
+}
 
 static inline int16_t
 psc_atomic16_setmask_ret(psc_atomic16_t *v, int16_t mask)
@@ -647,15 +846,15 @@ psc_atomic16_testmaskset(psc_atomic16_t *v, int16_t mask, int16_t newval)
 }
 
 static __inline int64_t
-psc_atomic64_set_mask(int64_t mask, psc_atomic64_t *v)
+psc_atomic64_set_mask(psc_atomic64_t *v, int64_t mask)
 {
 	int64_t newmask = mask;
 
 	__asm__ __volatile__(
 		LOCK_PREFIX "orq %0, %1;"
 		:"=r"(mask)
-		:"m"(v->counter), "0"(newmask));
-	return newmask | mask;
+		:"m"(v->value), "0"(newmask));
+	return (newmask | mask);
 }
 
 #endif /* __ia64 */
