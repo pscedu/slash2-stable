88e364c9e66d45e93f84ea950f82cb6b3785eff3
Paul Nowoczynski <pauln@psc.edu>
2009-06-24 21:04:30 +0000

    Initial bits for an on-disk table implementation.  Compiles but has not been tested.
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@6717 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/include/psc_util/odtable.h
@@ -0,0 +1,163 @@
+#ifndef _PFL_ODTABLE_H_ 
+#define _PFL_ODTABLE_H_
+
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include "psc_types.h"
+#include "psc_ds/vbitmap.h"
+#include "psc_util/crc.h"
+#include "psc_util/assert.h"
+#include "psc_util/lock.h"
+
+#define ODTBL_VERS  0x0000000000000001ULL
+#define ODTBL_MAGIC 0x001122335577bbddULL
+#define ODTBL_INUSE 0xf0f0f0f0f0f0f0f0ULL
+#define ODTBL_FREE  0xffffffffffffffffULL
+#define ODTBL_BAD   0x1010101010101010ULL
+#define ODTBL_ERR  ((size_t) -1)
+
+#define ODTBL_OPT_CRC  (1 << 0)
+#define ODTBL_OPT_SYNC (1 << 1)
+
+enum od_table_errors {
+	ODTBL_MAGIC_ERR = 1, 
+	ODTBL_SLOT_ERR  = 2,
+	ODTBL_INUSE_ERR = 3,
+	ODTBL_FREE_ERR  = 4,
+	ODTBL_CRC_ERR   = 5
+};
+
+/* Start data on a 4k boundary
+ */
+#define ODTBL_ALIGN 0x1000
+
+#define ODTABLE_ELEMSZ(odt)					\
+	((odt)->odt_elemsz + sizeof(struct odtable_hdr))
+
+#define ODTABLE_MAPSZ(odt)				\
+	(ODTABLE_ELEMSZ(odt) * (odt)->odt_nelems)
+
+struct odtable_hdr {
+	size_t          odth_nelems;
+	size_t          odth_elemsz;	
+	uint64_t        odth_magic;
+	uint32_t        odth_version;
+	uint32_t        odth_options;
+	off_t           odth_start;
+};
+
+struct odtable_entftr {
+	psc_crc_t       odtf_crc;
+	uint64_t        odtf_inuse;
+	uint64_t        odtf_slotno;
+	uint64_t        odtf_magic;
+};
+
+
+struct odtable {
+	size_t              odt_nelems;
+	size_t              odt_elemsz;
+	struct vbitmap     *odt_bitmap;
+	int                 odt_fd;
+	void               *odt_base;
+	struct odtable_hdr *odt_hdr;
+	psc_spinlock_t      odt_lock;
+};
+
+
+extern int
+odtable_create(const char *, size_t, size_t);
+
+extern int
+odtable_load(const char *, struct odtable **);
+
+extern void
+odtable_scan(struct odtable *, void (*odt_handler)(void *));
+
+extern int
+odtable_release(struct odtable *);
+
+extern size_t
+odtable_putitem(struct odtable *, void *);
+
+extern void *
+odtable_getitem(struct odtable *, size_t);
+
+extern void
+odtable_freeitem(struct odtable *, size_t);
+
+static inline off_t 
+odtable_getoffset(const struct odtable *odt, size_t elem)
+{
+	psc_assert(elem < odt->odt_nelems);
+
+	return ((off_t)((elem * (odt->odt_elemsz + sizeof(struct odtable_hdr)))
+			+ odt->odt_hdr->odth_start));
+}
+
+static inline void * 
+odtable_getitem_addr(const struct odtable *odt, size_t elem)
+{
+	psc_assert(elem < odt->odt_nelems);
+
+	return ((void *)((elem * (odt->odt_elemsz + 
+				  sizeof(struct odtable_hdr)))
+			 + odt->odt_hdr->odth_start));
+}
+
+static inline struct odtable_entftr *
+odtable_getfooter(const struct odtable *odt, size_t elem)
+{
+	psc_assert(elem < odt->odt_nelems);
+
+	return ((struct odtable_entftr *)
+		((void *)(elem * (odt->odt_elemsz + sizeof(struct odtable_hdr)))
+		 + odt->odt_hdr->odth_start + odt->odt_elemsz));
+}
+
+static inline int
+odtable_createmmap(struct odtable *odt)
+{
+	odt->odt_base = mmap(0, ODTABLE_MAPSZ(odt), (PROT_WRITE | PROT_READ), 
+			     0, odt->odt_fd, odt->odt_hdr->odth_start);
+
+	if (odt->odt_base == (void *)MAP_FAILED)
+		return (-errno);
+
+	return (0);
+}
+
+static inline int
+odtable_freemap(struct odtable *odt)
+{
+	return (munmap(odt->odt_base, ODTABLE_MAPSZ(odt)));
+}
+
+#define odtable_footercheck(odtf, elem, inuse, rc)      \
+        do {                                            \
+                rc = 0;                                 \
+                if ((odtf)->odtf_magic != ODTBL_MAGIC)  \
+                        rc = ODTBL_MAGIC_ERR;           \
+                                                        \
+                else if ((odtf)->odtf_slotno != elem)   \
+                        rc = ODTBL_SLOT_ERR;                    \
+                                                                \
+                else if ((odtf)->odtf_inuse == ODTBL_BAD)       \
+                        rc = ODTBL_BAD;                                 \
+                                                                        \
+                else if (inuse && (odtf)->odtf_inuse != ODTBL_INUSE)    \
+                        rc = ODTBL_INUSE_ERR;                           \
+                                                                        \
+                else if (!inuse && (odtf)->odtf_inuse != ODTBL_FREE)    \
+                        rc = ODTBL_FREE_ERR;                            \
+									\
+                if (rc)                                                 \
+                        psc_errorx("slot=%"_P_U64"d has error %"_P_U64"d", \
+				   elem, rc);				\
+        } while (0)
+ 
+#endif
---
+++ psc_fsutil_libs/psc_util/odtable.c
@@ -0,0 +1,268 @@
+#define _XOPEN_SOURCE 600
+#define _GNU_SOURCE
+
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include "psc_types.h"
+#include "psc_util/log.h"
+#include "psc_util/alloc.h"
+#include "psc_util/lock.h"
+#include "psc_util/odtable.h"
+
+
+static void
+odtable_sync(struct odtable *odt, __unusedx size_t elem)
+{
+	int rc, flags;
+	
+	psc_assert(odt->odt_hdr);
+
+	flags = (odt->odt_hdr->odth_options & ODTBL_OPT_SYNC) ?
+		 MS_SYNC : MS_ASYNC;
+
+	/* For now just sync the whole thing.
+	 */
+	rc = msync(odt->odt_base, ODTABLE_MAPSZ(odt), flags);
+	if (rc)
+		psc_error("msync error on table %p", odt);
+}
+
+size_t
+odtable_putitem(struct odtable *odt, void *data)
+{
+	size_t elem, rc = 0;
+	void *p;
+	struct odtable_entftr *odtf;
+
+	do { 
+		if (vbitmap_next(odt->odt_bitmap, &elem) <= 0)
+			return (ODTBL_ERR);
+		odtf = odtable_getfooter(odt, elem);
+		odtable_footercheck(odtf, elem, 0, rc);
+	} while (rc);
+	
+	p = odtable_getitem_addr(odt, elem);
+	odtf->odtf_inuse = ODTBL_INUSE;
+	
+	if (odt->odt_hdr->odth_options & ODTBL_OPT_CRC) {	
+		psc_crc_t crc;
+		
+		psc_crc_calc(&crc, p, odt->odt_elemsz);
+		odtf->odtf_crc = crc;
+		memcpy(p, data, odt->odt_elemsz);
+	}
+	odtable_sync(odt, elem);
+
+	return (elem);
+}
+
+void *
+odtable_getitem(struct odtable *odt, size_t elem)
+{
+	void *data = odtable_getitem_addr(odt, elem);
+	struct odtable_entftr *odtf = odtable_getfooter(odt, elem);
+	size_t rc;
+	
+	odtable_footercheck(odtf, elem, 1, rc);
+	if (rc)
+		return (NULL);
+
+	if (odt->odt_hdr->odth_options & ODTBL_OPT_CRC) {	
+		psc_crc_t crc;
+		
+		psc_crc_calc(&crc, data, odt->odt_elemsz);
+		if (crc != odtf->odtf_crc) {
+			odtf->odtf_inuse = ODTBL_BAD;
+			psc_warnx("slot=%"_P_U64"d crc fail", elem);
+		}
+		return (NULL);
+	}	
+	return (data);	
+}
+
+void 
+odtable_freeitem(struct odtable *odt, size_t elem)
+{
+	size_t rc;
+	struct odtable_entftr *odtf = odtable_getfooter(odt, elem);
+
+	odtable_footercheck(odtf, elem, 1, rc);
+	if (rc)
+		return;
+
+	odtf->odtf_inuse = ODTBL_FREE;
+
+	odtable_sync(odt, elem);
+	vbitmap_unset(odt->odt_bitmap, elem);
+
+	return;
+}
+
+
+int
+odtable_create(const char *f, size_t nelems, size_t elemsz)
+{
+	int rc = 0;
+	size_t z;
+	struct stat stb;
+	struct odtable odt;
+	struct odtable_entftr odtf = {0, ODTBL_FREE, 0, ODTBL_MAGIC};
+	struct odtable_hdr odth = {nelems, elemsz, ODTBL_MAGIC, ODTBL_VERS, 
+				   ODTBL_OPT_CRC, ODTBL_ALIGN};
+
+	if (!stat(f, &stb))
+		return (-EEXIST);
+
+	if (errno != ENOENT)
+		return (-errno);
+
+	if (!S_ISREG(stb.st_mode))
+		return (-EISDIR);
+
+	odt.odt_fd = open(f, O_CREAT|O_TRUNC|O_WRONLY, 0700);
+	if (odt.odt_fd < 0) {
+		rc = -errno;
+		goto out;
+	}
+	
+	if (pwrite(odt.odt_fd, &odth, sizeof(odth), 0) != sizeof(odth)) {
+		rc = -errno;
+                goto out;
+	}
+
+	for (z=0; z < nelems; z++) {
+		odtf.odtf_slotno = z;
+		if (pwrite(odt.odt_fd, &odtf, sizeof(odtf),
+			   (elemsz + odtable_getoffset(&odt, z))) != 
+		    sizeof(odtf)) {
+			rc = -errno;
+			goto out;
+		}			
+	}
+ out:
+	close(odt.odt_fd);
+	return (rc);
+}
+
+int
+odtable_load(const char *f, struct odtable **t)
+{
+	int rc = 0;	
+	size_t z, frc;
+	void *p;
+	struct odtable *odt = PSCALLOC(sizeof(struct odtable));
+	struct odtable_entftr *odtf;
+	struct odtable_hdr *odth;;
+
+	psc_assert(t);
+	*t = NULL;
+
+	odt->odt_fd = open(f, O_RDWR, 0700);
+	if (odt->odt_fd < 0)
+		return (-errno);
+
+	odth = odt->odt_hdr = PSCALLOC(sizeof(*odth));
+	
+	if (pread(odt->odt_fd, odth, sizeof(*odth), 0) != sizeof(*odth)) {
+		rc = -errno;
+		goto out;
+	}
+
+	if ((odth->odth_magic != ODTBL_MAGIC) || 
+	    (odth->odth_version != ODTBL_VERS)) {
+		rc = -EINVAL;
+		goto out;
+	}
+	
+	if ((rc = odtable_createmmap(odt)))
+		goto out;
+
+	odt->odt_bitmap = vbitmap_new(odt->odt_nelems);
+	if (!odt->odt_bitmap) {
+		rc = -ENOMEM;
+		goto out_unmap;
+	}
+
+	for (z=0; z < odt->odt_nelems; z++) {
+		p = odtable_getitem_addr(odt, z);
+		odtf = odtable_getfooter(odt, z);
+
+		odtable_footercheck(odtf, z, 0, frc);
+		/* Sanity checks for debugging.
+		 */
+		psc_assert(frc != ODTBL_MAGIC_ERR);	
+		psc_assert(frc != ODTBL_SLOT_ERR);
+
+		if (odtf->odtf_inuse == ODTBL_FREE)
+			vbitmap_unset(odt->odt_bitmap, z);
+
+		else if (odtf->odtf_inuse == ODTBL_INUSE) {
+			if (odth->odth_options & ODTBL_OPT_CRC) {
+				psc_crc_t crc;
+				
+				psc_crc_calc(&crc, p, odt->odt_elemsz);
+				if (crc != odtf->odtf_crc) {
+					odtf->odtf_inuse = ODTBL_BAD;
+					vbitmap_set(odt->odt_bitmap, z);
+					psc_warnx("slot=%"_P_U64"d crc fail", 
+						  z);
+				}				
+			} else
+				vbitmap_set(odt->odt_bitmap, z);	
+
+		} else {
+			vbitmap_set(odt->odt_bitmap, z);
+			psc_warnx("slot=%"_P_U64"d ignoring, bad inuse value", 
+				  z);
+		}			
+	}
+
+	psc_notify("odtable=%p has %d/%"_P_U64"d slots available",	   
+		   odt, vbitmap_nfree(odt->odt_bitmap), odth->odth_nelems);
+
+	*t = odt;
+ out:
+	close(odt->odt_fd);
+	return (rc);
+	
+ out_unmap:
+	odtable_freemap(odt);
+	goto out;
+}
+
+int 
+odtable_release(struct odtable *odt) {
+	PSCFREE(odt->odt_hdr);
+	vbitmap_free(odt->odt_bitmap);
+	
+	if (odtable_freemap(odt))
+		psc_fatal("odtable_freemap() failed on %p", odt);	
+
+	return (close(odt->odt_fd));
+}
+
+void
+odtable_scan(struct odtable *odt, void (*odt_handler)(void *))
+{
+	size_t z, rc;
+	
+	for (z=0; z < odt->odt_nelems; z++) {
+		if (!vbitmap_get(odt->odt_bitmap, z))
+			continue;
+		
+		odtable_footercheck(odtable_getfooter(odt, z), z, 1, rc);
+		psc_assert(rc != ODTBL_FREE_ERR);
+
+		if (rc) {
+			psc_warnx("slot=%"_P_U64"d marked bad, skipping", z);
+			continue;
+		}		
+		(odt_handler)(odtable_getitem(odt, z));		
+	}
+}
+
