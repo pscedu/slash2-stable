053b91acfde60af2b73ca02b2156da7039dda293
Jared Yanovich <yanovich@psc.edu>
2009-07-16 17:28:15 +0000

    standard header file layout
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@7099 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/rlimit.h
+++ psc_fsutil_libs/include/psc_util/rlimit.h
@@ -1,6 +1,13 @@
 /* $Id$ */
 
-extern psc_spinlock_t psc_rlimit_lock;
+#ifndef _PFL_RLIMIT_H_
+#define _PFL_RLIMIT_H_
+
+#include "sys/resource.h"
 
 int psc_setrlimit(int, rlim_t, rlim_t);
 int psc_getrlimit(int, rlim_t *, rlim_t *);
+
+extern psc_spinlock_t psc_rlimit_lock;
+
+#endif
