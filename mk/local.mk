05b79938742dd5c87f14117eee6840d4a6c559b4
Jared Yanovich <yanovich@psc.edu>
2009-09-02 16:18:48 +0000

    force ignore shadow warnings on LNET sources
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@7789 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -64,33 +64,33 @@ endif
 psc_fsutil_libs_psc_util_crc_c_CFLAGS=		-O2
 psc_fsutil_libs_psc_util_parity_c_CFLAGS=	-O2
 
-lnet_lite_socklnd_connection_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_socklnd_pqtimer_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_socklnd_procapi_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_socklnd_proclib_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_socklnd_select_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_socklnd_table_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_socklnd_tcplnd_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_socklnd_sendrecv_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
+lnet_lite_socklnd_connection_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_socklnd_pqtimer_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_socklnd_procapi_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_socklnd_proclib_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_socklnd_select_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_socklnd_table_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_socklnd_tcplnd_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_socklnd_sendrecv_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
 
-lnet_lite_libcfs_debug_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_libcfs_nidstrings_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_libcfs_user_lock_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_libcfs_user_prim_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
+lnet_lite_libcfs_debug_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_libcfs_nidstrings_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_libcfs_user_lock_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_libcfs_user_prim_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
 
-lnet_lite_lnet_acceptor_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_api_errno_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_api_ni_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_config_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_lib_eq_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_lib_md_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_lib_me_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_lib_move_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_lib_msg_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_lo_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_peer_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_router_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET
-lnet_lite_lnet_router_proc_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
+lnet_lite_lnet_acceptor_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_api_errno_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_api_ni_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_config_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_lib_eq_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_lib_md_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_lib_me_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_lib_move_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_lib_msg_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_lo_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_peer_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_router_c_CFLAGS=		-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_lnet_router_proc_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
 
-lnet_lite_ptllnd_ptllnd_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
-lnet_lite_ptllnd_ptllnd_cb_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET
+lnet_lite_ptllnd_ptllnd_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
+lnet_lite_ptllnd_ptllnd_cb_c_CFLAGS=	-DPSC_SUBSYS=PSS_LNET -Wno-shadow
