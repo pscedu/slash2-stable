3f2f9ffc7205f6283e94edba9301443ae61e9224
Jared Yanovich <yanovich@psc.edu>
2009-10-08 03:40:02 +0000

    data section needed for implementation
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@8263 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/psc_util/mspinlock.c
@@ -0,0 +1,13 @@
+/* $Id$ */
+
+#include <pthread.h>
+#include <stdint.h>
+
+#include "psc_ds/vbitmap.h"
+#include "psc_util/cdefs.h"
+#include "psc_util/mspinlock.h"
+#include "psc_util/spinlock.h"
+
+struct vbitmap	*_psc_mspin_unthridmap;
+psc_spinlock_t	 _psc_mspin_unthridmap_lock = LOCK_INITIALIZER;
+pthread_key_t	 _psc_mspin_thrkey;
