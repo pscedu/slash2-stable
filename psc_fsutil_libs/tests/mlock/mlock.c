f06e77f1d1d91f07974b73336809843cc2461687
Jared Yanovich <yanovich@psc.edu>
2009-10-08 03:39:42 +0000

    fixup test
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@8262 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/tests/mlock/mlock.c
+++ psc_fsutil_libs/tests/mlock/mlock.c
@@ -1,9 +1,9 @@
 /* $Id: lock2.c 8259 2009-10-08 03:15:33Z yanovich $ */
 
-#define TEST_LOCK_INCLUDE	"psc_util/spinlock.h"
-#define TEST_LOCK_TYPE		struct psc_spinlock
-#define TEST_LOCK_INITIALIZER	PSC_SPINLOCK_INIT
-#define TEST_LOCK_ACQUIRE(lk)	psc_spin_lock(lk)
-#define TEST_LOCK_RELEASE(lk)	psc_spin_unlock(lk)
+#define TEST_LOCK_INCLUDE	"psc_util/mspinlock.h"
+#define TEST_LOCK_TYPE		struct psc_mspinlock
+#define TEST_LOCK_INITIALIZER	PMSL_INIT
+#define TEST_LOCK_ACQUIRE(lk)	psc_mspin_lock(lk)
+#define TEST_LOCK_RELEASE(lk)	psc_mspin_unlock(lk)
 
 #include "../lock/lock_template.c"
