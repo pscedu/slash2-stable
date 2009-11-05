1370354e917136b044667cad9deaa32c358621a4
Jared Yanovich <yanovich@psc.edu>
2009-11-05 18:22:03 +0000

    make a real test out of this
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@8882 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/tests/list/list.c
+++ psc_fsutil_libs/tests/list/list.c
@@ -4,9 +4,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "pfl/cdefs.h"
+#include "pfl/pfl.h"
 #include "psc_ds/listcache.h"
 
 const char *progname;
+struct psclist_head hd = PSCLIST_HEAD_INIT(hd);
+int nitems;
 
 __dead void
 usage(void)
@@ -15,14 +19,71 @@ usage(void)
 	exit(1);
 }
 
+struct m {
+	struct psclist_head lentry;
+	int v;
+};
+
+void
+addelem(int v)
+{
+	struct m *m;
+
+	m = PSCALLOC(sizeof(*m));
+	m->v = v;
+	psclist_xadd(&m->lentry, &hd);
+	nitems++;
+}
+
+int
+shift(void)
+{
+	struct m *m;
+
+	m = psclist_first_entry(&hd, struct m, lentry);
+	if (m == NULL)
+		return (0);
+	psclist_del(&m->lentry);
+	printf("v: %d\n", m->v);
+	PSCFREE(m);
+	return (1);
+}
+
+int
+m_cmp(const void *a, const void *b)
+{
+	const struct m *x = a, *y = b;
+
+	return (CMP(x->v, y->v));
+}
+
 int
 main(int argc, char *argv[])
 {
+	void *p;
+
+	pfl_init();
 	progname = argv[0];
-	if (getopt(argc, argv, "") == -1)
+	if (getopt(argc, argv, "") != -1)
 		usage();
 	argc -= optind;
 	if (argc)
 		usage();
+
+	addelem(4);
+	addelem(2);
+	addelem(9);
+	addelem(23);
+	addelem(47);
+	addelem(5);
+	addelem(8);
+
+	p = PSCALLOC(sizeof(void *) * nitems);
+	psclist_sort(p, &hd, nitems, offsetof(struct m, lentry),
+	    qsort, m_cmp);
+	PSCFREE(p);
+
+	while (shift())
+		;
 	exit(0);
 }
