74e700ffeccdc2f5d5f4df1f31602f66077a7111
Jared Yanovich <yanovich@psc.edu>
2009-11-06 21:24:02 +0000

    get rid of more unused types
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@8908 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/pfl/types.h
+++ psc_fsutil_libs/include/pfl/types.h
@@ -9,11 +9,6 @@ typedef uint32_t	u32;
 typedef uint16_t	u16;
 typedef uint8_t		u8;
 
-typedef int64_t		s64;
-typedef int32_t		s32;
-typedef int16_t		s16;
-typedef int8_t		s8;
-
 /* printf(3) specifier modifiers for custom types. */
 #define PRIxLNID	PRIx64
 #define PRIxOFF		PRIx64
