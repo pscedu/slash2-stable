884bda74ef8f32212cccec60990359eda9758a50
Jared Yanovich <yanovich@psc.edu>
2009-11-11 19:50:09 +0000

    fixup
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9001 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/tests/setprocesstitle/setprocesstitle_test.c
+++ psc_fsutil_libs/tests/setprocesstitle/setprocesstitle_test.c
@@ -1,16 +1,34 @@
 /* $Id$ */
 
+#include <sys/param.h>
+
+#include <err.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "pfl/cdefs.h"
+#include "psc_util/log.h"
 #include "psc_util/setprocesstitle.h"
+
+#define _PATH_CMDLINE "/proc/self/cmdline"
 
 int
 main(__unusedx int argc, char *argv[])
 {
+	char buf[BUFSIZ];
+	int fd;
+
 	setprocesstitle(argv, "foobar %d", 13);
-	sleep(10);
+
+	fd = open(_PATH_CMDLINE, O_RDONLY);
+	if (fd == -1)
+		err(1, _PATH_CMDLINE);
+	read(fd, buf, sizeof(buf));
+	close(fd);
+
+	psc_assert(strcmp(buf, "foobar 13") == 0);
 	exit(0);
 }
