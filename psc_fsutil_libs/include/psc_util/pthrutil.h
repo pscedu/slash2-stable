61c8ab86a2bc78b5e7ecc0fdfc118cbc6e9954e2
Jared Yanovich <yanovich@psc.edu>
2009-12-09 00:25:09 +0000

    add reqlock API for pthread mutexes
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9387 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/pthrutil.h
+++ psc_fsutil_libs/include/psc_util/pthrutil.h
@@ -1,6 +1,14 @@
 /* $Id$ */
 
+#ifndef _PFL_PTHRUTIL_H_
+#define _PFL_PTHRUTIL_H_
+
 #include <pthread.h>
 
-void psc_pthread_mutex_init(pthread_mutex_t *);
-void psc_pthread_mutex_lock(pthread_mutex_t *);
+void	psc_pthread_mutex_init(pthread_mutex_t *);
+void	psc_pthread_mutex_lock(pthread_mutex_t *);
+void	psc_pthread_mutex_unlock(pthread_mutex_t *);
+int	psc_pthread_mutex_reqlock(pthread_mutex_t *);
+void	psc_pthread_mutex_ureqlock(pthread_mutex_t *, int);
+
+#endif /* _PFL_PTHRUTIL_H_ */
--- psc_fsutil_libs/psc_util/pthrutil.c
+++ psc_fsutil_libs/psc_util/pthrutil.c
@@ -1,5 +1,6 @@
 /* $Id$ */
 
+#include <errno.h>
 #include <pthread.h>
 #include <string.h>
 
@@ -33,4 +34,34 @@ psc_pthread_mutex_lock(pthread_mutex_t *mut)
 	rc = pthread_mutex_lock(mut);
 	if (rc)
 		psc_fatalx("pthread_mutex_lock: %s", strerror(rc));
+}
+
+void
+psc_pthread_mutex_unlock(pthread_mutex_t *mut)
+{
+	int rc;
+
+	rc = pthread_mutex_unlock(mut);
+	if (rc)
+		psc_fatalx("pthread_mutex_unlock: %s", strerror(rc));
+}
+
+int
+psc_pthread_mutex_reqlock(pthread_mutex_t *mut)
+{
+	int rc;
+
+	rc = pthread_mutex_lock(mut);
+	if (rc == EDEADLK)
+		rc = 1;
+	else if (rc)
+		psc_fatalx("pthread_mutex_unlock: %s", strerror(rc));
+	return (rc);
+}
+
+void
+psc_pthread_mutex_ureqlock(pthread_mutex_t *mut, int waslocked)
+{
+	if (!waslocked)
+		psc_pthread_mutex_unlock(mut);
 }
