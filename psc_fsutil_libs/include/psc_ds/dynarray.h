b1166c9ff806f6274fcd8ad1357727703fd9cb8a
Jared Yanovich <yanovich@psc.edu>
2009-12-10 23:13:57 +0000

    remove compat aliases
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9445 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_ds/dynarray.h
+++ psc_fsutil_libs/include/psc_ds/dynarray.h
@@ -16,8 +16,6 @@ struct psc_dynarray {
 #define da_items	pda_items
 };
 
-#define dynarray	psc_dynarray
-
 #define DYNARRAY_INIT	{ 0, 0, NULL }
 
 /**
@@ -25,26 +23,26 @@ struct psc_dynarray {
  * @p: item iterator.
  * @n: integer iterator variable.
  * @pda: dynamic array.
- * Notes: do not invoke dynarray_add/remove() in the body of this loop.
+ * Notes: do not invoke psc_dynarray_add/remove() in the body of this loop.
  */
 #define DYNARRAY_FOREACH(p, n, pda)					\
-	for ((n) = 0; (n) < dynarray_len(pda) &&			\
-	    (((p) = dynarray_getpos((pda), (n))) || 1); (n)++)
+	for ((n) = 0; (n) < psc_dynarray_len(pda) &&			\
+	    (((p) = psc_dynarray_getpos((pda), (n))) || 1); (n)++)
 
 /**
- * dynarray_len - Obtain the number of elements stored in a dynamic array.
+ * psc_dynarray_len - Obtain the number of elements stored in a dynamic array.
  * @pda: dynamic array to access.
  */
 #define psc_dynarray_len(pda)	(pda)->pda_pos
 
 /**
- * dynarray_get - Access a dynamic array's item list.
+ * psc_dynarray_get - Access a dynamic array's item list.
  * @pda: dynamic array to access.
  */
 #define psc_dynarray_get(pda)	((void *)(pda)->pda_items)
 
 /**
- * dynarray_sort - Reorder elements of a dynarray.
+ * psc_dynarray_sort - Reorder elements of a dynarray.
  * @pda: dynamic array whose elements to sort.
  * @sortf: which sorting algorithm to use.
  * @cmpf: criteria for reordering items.
@@ -64,18 +62,5 @@ void	*psc_dynarray_getpos(const struct psc_dynarray *, int);
 void	 psc_dynarray_init(struct psc_dynarray *);
 void	 psc_dynarray_remove(struct psc_dynarray *, const void *);
 void	 psc_dynarray_reset(struct psc_dynarray *);
-
-#define dynarray_add		psc_dynarray_add
-#define dynarray_add_ifdne	psc_dynarray_add_ifdne
-#define dynarray_ensurelen	psc_dynarray_ensurelen
-#define dynarray_exists		psc_dynarray_exists
-#define dynarray_free		psc_dynarray_free
-#define dynarray_freeslack	psc_dynarray_freeslack
-#define dynarray_get		psc_dynarray_get
-#define dynarray_getpos		psc_dynarray_getpos
-#define dynarray_init		psc_dynarray_init
-#define dynarray_len		psc_dynarray_len
-#define dynarray_remove		psc_dynarray_remove
-#define dynarray_reset		psc_dynarray_reset
 
 #endif /* _PFL_DYNARRAY_H_ */
