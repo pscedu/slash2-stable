75ad5169aa797c8dbba8d2a1689211128cb85cc0
Jared Yanovich <yanovich@psc.edu>
2009-12-11 17:42:31 +0000

    add psc_pthread_mutex_ensure_locked()
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9449 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/pthrutil.h
+++ psc_fsutil_libs/include/psc_util/pthrutil.h
@@ -11,4 +11,7 @@ void	psc_pthread_mutex_unlock(pthread_mutex_t *);
 int	psc_pthread_mutex_reqlock(pthread_mutex_t *);
 void	psc_pthread_mutex_ureqlock(pthread_mutex_t *, int);
 
+#define psc_pthread_mutex_ensure_locked(mutex)				\
+	psc_assert(pthread_mutex_trylock(mutex) == EDEADLK)
+
 #endif /* _PFL_PTHRUTIL_H_ */
