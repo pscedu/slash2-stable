86d3fd838c5d66956f8b8eb424564857f12eb85a
Jared Yanovich <yanovich@psc.edu>
2009-12-14 16:14:31 +0000

    remove some unneeded newlines
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9474 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/tests/vbitmap/vbitmap_test.c
+++ psc_fsutil_libs/tests/vbitmap/vbitmap_test.c
@@ -53,7 +53,7 @@ main(int argc, char *argv[])
 			errx(1, "out of elements at pos %d", i);
 
 	if (psc_vbitmap_next(vb, &elem))
-		errx(1, "got another expected unused elem! %zu\n", elem);
+		errx(1, "got another expected unused elem! %zu", elem);
 
 	psc_vbitmap_getstats(vb, &u, &t);
 	if (u != 213 || t != 213)
@@ -69,7 +69,7 @@ main(int argc, char *argv[])
 
 	/* try one past end of filled bitmap */
 	if (psc_vbitmap_next(vb, &elem))
-		errx(1, "got another expected unused elem! %zu\n", elem);
+		errx(1, "got another expected unused elem! %zu", elem);
 
 	/* free some slots */
 	for (i = 0, elem = 0; elem < NELEM; i++, elem += NELEM / 10)
@@ -95,7 +95,7 @@ main(int argc, char *argv[])
 
 	/* try one past end of filled bitmap */
 	if (psc_vbitmap_next(vb, &elem))
-		errx(1, "got another expected unused elem! %zu\n", elem);
+		errx(1, "got another expected unused elem! %zu", elem);
 
 	psc_vbitmap_free(vb);
 	exit(0);
