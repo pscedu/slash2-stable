dc35814a8e9ace40495a33a3a905ddad26fd0fc2
Jared Yanovich <yanovich@psc.edu>
2009-12-16 17:44:28 +0000

    why is this here
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9532 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_ds/list.h
+++ psc_fsutil_libs/include/psc_ds/list.h
@@ -388,34 +388,8 @@ psclist_add_sorted(struct psclist_head *hd, struct psclist_head *elem,
 	psclist_xadd_tail(elem, hd);
 }
 
-static __inline void
-psclist_sort(void **p, struct psclist_head *hd, int n, ptrdiff_t off,
-    void (*sortf)(void *, size_t, size_t, int (*)(const void *, const void *)),
-    int (*cmpf)(const void *, const void *))
-{
-	struct psclist_head *e;
-	void *next, *prev;
-	int j = 0;
-
-	psc_assert(n >= 0);
-	if (n < 2)
-		return;
-
-	psclist_for_each(e, hd)
-		p[j++] = ((char *)e) - off;
-	sortf(p, n, sizeof(*p), cmpf);
-	prev = hd;
-	for (j = 0; j < n; j++, prev = e) {
-		e = (void *)((char *)p[j] + off);
-		if (j + 1 == n)
-			next = hd;
-		else
-			next = (char *)p[j + 1] + off;
-		e->zprev = prev;
-		e->znext = next;
-	}
-	hd->znext = (void *)((char *)p[0] + off);
-	hd->zprev = prev;
-}
+void psclist_sort(void **, struct psclist_head *, int, ptrdiff_t,
+	void (*)(void *, size_t, size_t, int (*)(const void *, const void *)),
+	int (*)(const void *, const void *));
 
 #endif /* _PFL_LIST_H_ */
--- psc_fsutil_libs/psc_util/list.c
+++ psc_fsutil_libs/psc_util/list.c
@@ -5,3 +5,33 @@
 
 struct psc_lockedlist pscListCaches =
     PLL_INITIALIZER(&pscListCaches, struct psc_listcache, lc_index_lentry);
+
+void
+psclist_sort(void **p, struct psclist_head *hd, int n, ptrdiff_t off,
+    void (*sortf)(void *, size_t, size_t, int (*)(const void *, const void *)),
+    int (*cmpf)(const void *, const void *))
+{
+	struct psclist_head *e;
+	void *next, *prev;
+	int j = 0;
+
+	psc_assert(n >= 0);
+	if (n < 2)
+		return;
+
+	psclist_for_each(e, hd)
+		p[j++] = ((char *)e) - off;
+	sortf(p, n, sizeof(*p), cmpf);
+	prev = hd;
+	for (j = 0; j < n; j++, prev = e) {
+		e = (void *)((char *)p[j] + off);
+		if (j + 1 == n)
+			next = hd;
+		else
+			next = (char *)p[j + 1] + off;
+		e->zprev = prev;
+		e->znext = next;
+	}
+	hd->znext = (void *)((char *)p[0] + off);
+	hd->zprev = prev;
+}
