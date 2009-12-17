3d295c57b4bbd249a73453cfa8d9a74b315e7b34
Jared Yanovich <yanovich@psc.edu>
2009-12-17 22:46:03 +0000

    move tsuite tools to a shared location
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9586 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ tools/doresults.pl
@@ -0,0 +1,117 @@
+#!/usr/bin/perl -W
+# $Id$
+
+use Getopt::Std;
+use Net::SMTP;
+use POSIX qw(mktime strftime);
+use warnings;
+use strict;
+
+my %opts;
+getopts("mN", \%opts) or die "usage: $0 [-mN] testname logbase\n";
+
+my ($testname, $logbase) = @ARGV;
+
+sub avg {
+	my ($r) = @_;
+	my $sum = 0;
+	my $i;
+	foreach $i (@$r) {
+		$sum += $i;
+	}
+	return $sum / (@$r || 1);
+}
+
+sub getstats {
+	my ($dat, $date, $t_tm, $ndays) = @_;
+	my @tms;
+	open F, "<", $dat or die "$dat: $!\n";
+	while (<F>) {
+		# 2009-03-12t16:52
+		if (/^(\d\d\d\d)-(\d\d)-(\d\d)t\d\d:\d\d(?=\s)/ and
+		    $& ne $date and $t_tm - mktime(0, 0, 0, $3, $2, $1) <=
+		    $ndays * 24 * 60 * 60) {
+			my $t = $';
+			$t =~ s/^\s+|\s+$//g;
+			push @tms, $t;
+		}
+	}
+	close F;
+	return \@tms;
+}
+
+sub fmtres {
+	my ($tm, $r_stats) = @_;
+
+	if (@$r_stats) {
+		my $avg = avg $r_stats;
+		return sprintf "%3d %5d %6d %4d%%", scalar(@$r_stats),
+		    $avg, $avg - $tm, ($avg-$tm)/$avg * 100,
+	}
+	return sprintf "%3d %5s %6s %5s", 0, "-", "-", "-";
+}
+
+sub show_results {
+	my ($dat, $date, $tm) = @_;
+
+	my ($t_yr, $t_mon, $t_day) =
+	    ($date =~ /^(\d\d\d\d)-(\d\d)-(\d\d)t\d\d:\d\d$/) or
+	    die "date $date: bad format\n";
+	my $t_tm = mktime(0, 0, 0, $3, $2, $1);
+
+	# average last 2, 8, and 30 days' stats and compare
+	my $d_st = getstats $dat, $date, $t_tm, 2;
+	my $w_st = getstats $dat, $date, $t_tm, 8;
+	my $m_st = getstats $dat, $date, $t_tm, 30;
+
+	my $tlabel = $dat;
+	$tlabel =~ s!.*/!!;
+
+	return sprintf " %-15s %5d|%s|%s|%s\n", $tlabel, $tm,
+	    fmtres($tm, $d_st), fmtres($tm, $w_st), fmtres($tm, $m_st);
+}
+
+my $report = "timing stats (negative means longer, positive means improvement)\n";
+$report .= sprintf " %-15s %5s+%22s+%22s+%22s\n", "", "",
+    "-------- day ---------",
+    "-------- week --------",
+    "------- month --------";
+
+my $fm="%3s %5s %6s %5s";
+$report .= sprintf " %-15s %5s|$fm|$fm|$fm\n", "test", "time",
+    '#t', qw(avgtm tmdiff %impr),
+    '#t', qw(avgtm tmdiff %impr),
+    '#t', qw(avgtm tmdiff %impr);
+$report .= " " .
+    "---------------------" .
+    "+----------------------" .
+    "+----------------------" .
+    "+----------------------\n";
+
+my $date = strftime("%Ft%R", localtime);
+while (<STDIN>) {
+	my ($name, $tm) = split;
+	my $dat = "$logbase/$testname.$name";
+	unless ($opts{N}) {
+		open DAT, ">>", $dat or die "$dat: $!\n";
+		print DAT "$date $tm\n";
+		close DAT;
+	}
+	$report .= show_results $dat, $date, $tm;
+}
+
+print "\n\n", $report;
+
+if ($opts{m}) {
+	my $smtp = Net::SMTP->new('mailer.psc.edu');
+	$smtp->mail('zest-devel@psc.edu');
+	$smtp->to('zest-devel@psc.edu');
+	$smtp->data();
+	$smtp->datasend("To: zest-devel\@psc.edu\n");
+	$smtp->datasend("Subject: tsuite report\n\n");
+	$smtp->datasend("$report");
+	$smtp->dataend();
+	$smtp->quit;
+}
+
+exit 0;
---
+++ tools/gdbtry.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/perl -W
+# $Id$
+
+use POSIX qw(:errno_h);
+use IPC::Open3;
+use Symbol;
+use strict;
+use warnings;
+
+sub fatal {
+	# Prepend a newline to leave the "(notgdb)" prompt.
+	die "\n", $@, ": $!\n";
+}
+
+sub slurp {
+	my ($fh) = @_;
+	my $rd_set = 0;
+	my $off = 0;
+	my ($data, $rv);
+
+	vec($rd_set, fileno($fh), 1) = 1;
+	while (select $rd_set, undef, undef, 0) {
+		$rv = sysread $fh, $data, 4096, $off;
+		fatal "sysread" if $rv == -1;
+		last if $rv == 0;
+		$off += $rv;
+	}
+	return undef unless $data and $rv;
+	return ($data);
+}
+
+die "usage: $0 program [arg ...]\n" unless @ARGV;
+
+my $prog = shift;
+
+$| = 1;
+
+my $sigint = 0;
+my $rc = 1;
+my $infh;
+my $outfh;
+my $pid = open3($infh, $outfh, 0, "gdb -q -f $prog");
+open TTY, "<", "/dev/tty" or die "/dev/tty: $!\n";
+
+my @cmds = (
+	"set prompt (gdb)\\n",
+	"set annotate 3",
+	"set height 0",
+	"set confirm off",
+	"run @ARGV",
+);
+
+$SIG{INT} = sub { $sigint = 1; };
+
+for (;;) {
+	my $rd_set = 0;
+	vec($rd_set, fileno(TTY), 1) = 1;
+	vec($rd_set, fileno($outfh), 1) = 1;
+	my $er_set = $rd_set;
+	my $nfd = select($rd_set, undef, $er_set, undef);
+
+	if ($sigint) {
+		$sigint = 0;
+		kill "SIGINT", $pid or warn "kill $pid: $!\n";
+	}
+
+	if ($nfd == -1) {
+		# Probably from a signal
+		fatal "select" unless $! == EINTR;
+		next;
+	}
+
+	my ($data, $ln, @lns);
+	if (vec($rd_set, fileno(TTY), 1)) {
+		$data = slurp(*TTY);
+		unless (defined $data) {
+			print "^D\n";
+			last;
+		}
+		print $infh $data or last;
+	} elsif (vec($rd_set, fileno($outfh), 1)) {
+		$data = slurp($outfh);
+		defined $data or last;
+
+		my $do_cmd = 0;
+		@lns = split /\n/, $data;
+		foreach $ln (@lns) {
+			if (($ln =~ /^\032\032exited (\d+)/ ||
+			    $ln =~ /Program exited with code (\d+)/) &&
+			    ($rc = $1) == 0) {
+				push @cmds, "quit";
+				$do_cmd = 1;
+				next;
+			}
+			if ($ln =~ m!^\(gdb\)!) {
+				$do_cmd = 1;
+				next;
+			}
+			next if $ln =~ /^\s*$/;
+			print "$ln\n" unless $ln =~ /^\032\032/;
+		}
+
+		if ($do_cmd) {
+			print "(notgdb) ";
+			if (@cmds) {
+				my $cmd = shift @cmds;
+				print "$cmd\n";
+				print $infh "$cmd\n";
+			}
+		}
+	} elsif ($er_set) {
+		last;
+	}
+}
+close $infh;
+close $outfh;
+close TTY;
+
+wait;
+
+exit $rc;
