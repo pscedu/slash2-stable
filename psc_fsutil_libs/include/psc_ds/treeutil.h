ed7b0e9691270efc876e2cbed290f3f6a754bdda
Jared Yanovich <yanovich@psc.edu>
2009-12-21 21:13:28 +0000

    add PSC_SPLAY_XINSERT()
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9621 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_ds/treeutil.h
+++ psc_fsutil_libs/include/psc_ds/treeutil.h
@@ -1,7 +1,19 @@
 /* $Id$ */
 
+#ifndef _PFL_TREEUTIL_H_
+#define _PFL_TREEUTIL_H_
+
 #define PSC_SPLAY_XREMOVE(name, head, elm)				\
 	do {								\
 		if (name ## _SPLAY_REMOVE((head), (elm)) != (elm))	\
-			psc_fatalx("splay inconsistency");		\
+			psc_fatalx("SPLAY_REMOVE: item not found");	\
 	} while (0)
+
+#define PSC_SPLAY_XINSERT(name, head, elm)				\
+	do {								\
+		if (name ## _SPLAY_INSERT((head), (elm)))		\
+			psc_fatalx("SPLAY_INSERT: item with key "	\
+			    "already exists");				\
+	} while (0)
+
+#endif /* _PFL_TREEUTIL_H_ */
