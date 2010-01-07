9c4980d928c368becd6a744e69549f413f7ee9fc
Jared Yanovich <yanovich@psc.edu>
2010-01-07 03:36:14 +0000

    spacing, no code changes
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9683 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_rpc/import.c
+++ psc_fsutil_libs/psc_rpc/import.c
@@ -7,39 +7,39 @@
 static inline char *
 pscrpc_import_state_name(enum psc_imp_state state)
 {
-        static char* import_state_names[] = {
-                "<UNKNOWN>", "CLOSED",  "NEW", "DISCONN",
-                "CONNECTING", "REPLAY", "REPLAY_LOCKS", "REPLAY_WAIT",
-                "RECOVER", "FULL", "EVICTED",
-        };
+	static char* import_state_names[] = {
+		"<UNKNOWN>", "CLOSED",  "NEW", "DISCONN",
+		"CONNECTING", "REPLAY", "REPLAY_LOCKS", "REPLAY_WAIT",
+		"RECOVER", "FULL", "EVICTED",
+	};
 
-        LASSERT (state <= PSC_IMP_EVICTED);
-        return import_state_names[state];
+	LASSERT (state <= PSC_IMP_EVICTED);
+	return import_state_names[state];
 }
 
 /* A CLOSED import should remain so. */
-#define PSCIMPORT_SET_STATE_NOLOCK(imp, state)				\
-	do {								\
-		if (imp->imp_state != PSC_IMP_CLOSED) {		\
-			psc_warnx("%p %s: changing import state from %s to %s", \
-			       imp, libcfs_id2str(imp->imp_connection->c_peer), \
-			       pscrpc_import_state_name(imp->imp_state), \
-			       pscrpc_import_state_name(state));	\
-			imp->imp_state = state;				\
-		}							\
+#define PSCIMPORT_SET_STATE_NOLOCK(imp, state)					\
+	do {									\
+		if (imp->imp_state != PSC_IMP_CLOSED) {				\
+			psc_warnx("%p %s: changing import state from %s to %s",	\
+			       imp, libcfs_id2str(imp->imp_connection->c_peer),	\
+			       pscrpc_import_state_name(imp->imp_state),	\
+			       pscrpc_import_state_name(state));		\
+			imp->imp_state = state;					\
+		}								\
 	} while(0)
 
 
 int pscrpc_init_import(struct pscrpc_import *imp)
 {
-        spinlock(&imp->imp_lock);
+	spinlock(&imp->imp_lock);
 
-        imp->imp_generation++;
-        imp->imp_state =  PSC_IMP_NEW;
+	imp->imp_generation++;
+	imp->imp_state =  PSC_IMP_NEW;
 
-        freelock(&imp->imp_lock);
+	freelock(&imp->imp_lock);
 
-        return 0;
+	return 0;
 }
 
 /**
@@ -55,15 +55,15 @@ int pscrpc_init_import(struct pscrpc_import *imp)
  */
 int pscrpc_set_import_discon(struct pscrpc_import *imp, uint32_t conn_cnt)
 {
-        int rc = 0;
+	int rc = 0;
 
-        spinlock(&imp->imp_lock);
+	spinlock(&imp->imp_lock);
 
 	psc_warnx("inhere conn_cnt %u imp_conn_cnt %u, imp->imp_state = %d",
 		  conn_cnt, imp->imp_conn_cnt, imp->imp_state);
 
-        if (imp->imp_state == PSC_IMP_FULL &&
-            (conn_cnt == 0 || conn_cnt == (uint32_t)imp->imp_conn_cnt)) {
+	if (imp->imp_state == PSC_IMP_FULL &&
+	    (conn_cnt == 0 || conn_cnt == (uint32_t)imp->imp_conn_cnt)) {
 
 		psc_errorx("Connection to service via nid %s was "
 			   "lost; in progress operations using this "
@@ -72,28 +72,28 @@ int pscrpc_set_import_discon(struct pscrpc_import *imp, uint32_t conn_cnt)
 			   imp->imp_replayable ?
 			   "wait for recovery to complete" : "fail");
 
-                PSCIMPORT_SET_STATE_NOLOCK(imp, PSC_IMP_DISCON);
-                freelock(&imp->imp_lock);
+		PSCIMPORT_SET_STATE_NOLOCK(imp, PSC_IMP_DISCON);
+		freelock(&imp->imp_lock);
 
-                //if (obd_dump_on_timeout)
-                //        libcfs_debug_dumplog();
+		//if (obd_dump_on_timeout)
+		//        libcfs_debug_dumplog();
 
 #if PAULS_TODO
 		/* inform the client to take action in response to the event */
-                pscobd_import_event(imp->imp_obd, imp, IMP_EVENT_DISCON);
+		pscobd_import_event(imp->imp_obd, imp, IMP_EVENT_DISCON);
 #endif
-                rc = 1;
-        } else {
-                freelock(&imp->imp_lock);
-                psc_warnx("%s: import %p already %s (conn %u, was %u): %s",
+		rc = 1;
+	} else {
+		freelock(&imp->imp_lock);
+		psc_warnx("%s: import %p already %s (conn %u, was %u): %s",
 			imp->imp_client->cli_name, imp,
 			(imp->imp_state == PSC_IMP_FULL &&
 			 (uint32_t)imp->imp_conn_cnt > conn_cnt) ?
 			"reconnected" : "not connected", imp->imp_conn_cnt,
 			conn_cnt, pscrpc_import_state_name(imp->imp_state));
-        }
+	}
 
-        return rc;
+	return rc;
 }
 
 /*
@@ -102,17 +102,17 @@ int pscrpc_set_import_discon(struct pscrpc_import *imp, uint32_t conn_cnt)
  */
 void pscrpc_deactivate_import(struct pscrpc_import *imp)
 {
-        ENTRY;
+	ENTRY;
 
-        spinlock(&imp->imp_lock);
-        psc_warnx("setting import %p INVALID", imp);
-        imp->imp_invalid = 1;
-        imp->imp_generation++;
-        freelock(&imp->imp_lock);
+	spinlock(&imp->imp_lock);
+	psc_warnx("setting import %p INVALID", imp);
+	imp->imp_invalid = 1;
+	imp->imp_generation++;
+	freelock(&imp->imp_lock);
 
 	psc_errorx("Here's where failover is supposed to happen!!!");
-        pscrpc_abort_inflight(imp);
-        //pscobd_import_event(imp->imp_obd, imp, IMP_EVENT_INACTIVE);
+	pscrpc_abort_inflight(imp);
+	//pscobd_import_event(imp->imp_obd, imp, IMP_EVENT_INACTIVE);
 }
 
 /*
@@ -123,41 +123,41 @@ void pscrpc_deactivate_import(struct pscrpc_import *imp)
  */
 void pscrpc_invalidate_import(struct pscrpc_import *imp)
 {
-        struct l_wait_info lwi;
-        int rc;
+	struct l_wait_info lwi;
+	int rc;
 
-        if (!imp->imp_invalid)
-                pscrpc_deactivate_import(imp);
+	if (!imp->imp_invalid)
+		pscrpc_deactivate_import(imp);
 
-        LASSERT(imp->imp_invalid);
+	LASSERT(imp->imp_invalid);
 
-        /* wait for all requests to error out and call completion callbacks */
-        lwi = LWI_TIMEOUT_INTERVAL(MAX(PSCRPC_OBD_TIMEOUT, 1), HZ, NULL, NULL);
-        rc = psc_cli_wait_event(&imp->imp_recovery_waitq,
+	/* wait for all requests to error out and call completion callbacks */
+	lwi = LWI_TIMEOUT_INTERVAL(MAX(PSCRPC_OBD_TIMEOUT, 1), HZ, NULL, NULL);
+	rc = psc_cli_wait_event(&imp->imp_recovery_waitq,
 			    (atomic_read(&imp->imp_inflight) == 0),
 			    &lwi);
 
-        if (rc)
-                CERROR("rc = %d waiting for callback (%d != 0)\n",
-                       rc, atomic_read(&imp->imp_inflight));
+	if (rc)
+		CERROR("rc = %d waiting for callback (%d != 0)\n",
+		       rc, atomic_read(&imp->imp_inflight));
 
-        //pscobd_import_event(imp->imp_obd, imp, IMP_EVENT_INVALIDATE);
+	//pscobd_import_event(imp->imp_obd, imp, IMP_EVENT_INVALIDATE);
 }
 
 void pscrpc_activate_import(struct pscrpc_import *imp)
 {
-        //struct obd_device *obd = imp->imp_obd;
+	//struct obd_device *obd = imp->imp_obd;
 
-        spinlock(&imp->imp_lock);
-        imp->imp_invalid = 0;
-        freelock(&imp->imp_lock);
+	spinlock(&imp->imp_lock);
+	imp->imp_invalid = 0;
+	freelock(&imp->imp_lock);
 
-        //pscobd_import_event(obd, imp, IMP_EVENT_ACTIVE);
+	//pscobd_import_event(obd, imp, IMP_EVENT_ACTIVE);
 }
 
 void pscrpc_fail_import(struct pscrpc_import *imp, uint32_t conn_cnt)
 {
-        ENTRY;
+	ENTRY;
 
 	if (imp->imp_state == PSC_IMP_NEW) {
 		psc_info("Failing new import %p", imp);
@@ -165,22 +165,22 @@ void pscrpc_fail_import(struct pscrpc_import *imp, uint32_t conn_cnt)
 		return;
 	}
 
-        if (pscrpc_set_import_discon(imp, conn_cnt)) {
-                if (!imp->imp_replayable) {
-                        psc_warnx("import for %s not replayable, "
+	if (pscrpc_set_import_discon(imp, conn_cnt)) {
+		if (!imp->imp_replayable) {
+			psc_warnx("import for %s not replayable, "
 			       "auto-deactivating",
 			       libcfs_id2str(imp->imp_connection->c_peer));
 
-                        pscrpc_deactivate_import(imp);
-                }
+			pscrpc_deactivate_import(imp);
+		}
 		//CDEBUG(D_HA, "%s: waking up pinger\n",
-                //       obd2cli_tgt(imp->imp_obd));
+		//       obd2cli_tgt(imp->imp_obd));
 
 		psc_notify("failing import %p", imp);
-                spinlock(&imp->imp_lock);
-                imp->imp_force_verify = 1;
+		spinlock(&imp->imp_lock);
+		imp->imp_force_verify = 1;
 		imp->imp_failed = 1;
-                freelock(&imp->imp_lock);
-        }
+		freelock(&imp->imp_lock);
+	}
 	EXIT;
 }
