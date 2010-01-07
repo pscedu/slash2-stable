cb2411c8512a001814d02f3286e321e051a94ad2
Jared Yanovich <yanovich@psc.edu>
2010-01-07 18:54:43 +0000

    add copyrights on headers
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9700 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/daemon.h
+++ psc_fsutil_libs/include/psc_util/daemon.h
@@ -1,9 +1,21 @@
 /* $Id$ */
-
-/* a daemon startup routine, by Richard Stevens
- * sever all ties to the launching process
+/*
+ * %PSC_START_COPYRIGHT%
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2006-2009, Pittsburgh Supercomputing Center (PSC).
+ *
+ * Permission to use, copy, and modify this software and its documentation
+ * without fee for personal use or non-commercial use within your organization
+ * is hereby granted, provided that the above copyright notice is preserved in
+ * all copies and that the copyright and this permission notice appear in
+ * supporting documentation.  Permission to redistribute this software to other
+ * organizations or individuals is not permitted without the written permission
+ * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+ * the suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ * -----------------------------------------------------------------------------
+ * %PSC_END_COPYRIGHT%
  */
-void daemon_start(void);
 
 #define PIDFILE_DIR "/var/run"
 
