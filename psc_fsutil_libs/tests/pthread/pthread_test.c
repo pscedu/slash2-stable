d82035e5c63b23262a6011f692bf08bcd9503500
Jared Yanovich <yanovich@psc.edu>
2010-01-14 15:51:15 +0000

    add a pthread tester which reveals a problem on castor
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9832 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/tests/pthread/pthread_test.c
@@ -0,0 +1,58 @@
+/* $Id$ */
+/*
+ * %PSC_START_COPYRIGHT%
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2006-2009, Pittsburgh Supercomputing Center (PSC).
+ *
+ * Permission to use, copy, and modify this software and its documentation
+ * without fee for personal use or non-commercial use within your organization
+ * is hereby granted, provided that the above copyright notice is preserved in
+ * all copies and that the copyright and this permission notice appear in
+ * supporting documentation.  Permission to redistribute this software to other
+ * organizations or individuals is not permitted without the written permission
+ * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+ * the suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ * -----------------------------------------------------------------------------
+ * %PSC_END_COPYRIGHT%
+ */
+
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "pfl/cdefs.h"
+#include "pfl/pfl.h"
+#include "psc_util/pthrutil.h"
+
+const char *progname;
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", progname);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	pthread_mutex_t mut;
+	int rc;
+
+	pfl_init();
+	progname = argv[0];
+	if (getopt(argc, argv, "") != -1)
+		usage();
+	argc -= optind;
+	if (argc)
+		usage();
+
+	pthread_mutex_init(&mut, NULL);
+	psc_pthread_mutex_init(&mut);
+	psc_pthread_mutex_lock(&mut);
+	psc_pthread_mutex_ensure_locked(&mut);
+	exit(0);
+}
