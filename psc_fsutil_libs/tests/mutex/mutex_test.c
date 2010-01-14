a439d7515e7f0b5773241dea5893ed9cfb8a43cc
Jared Yanovich <yanovich@psc.edu>
2010-01-14 16:17:13 +0000

    oops, integrate into the mutex tester
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9835 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/tests/mutex/mutex_test.c
+++ psc_fsutil_libs/tests/mutex/mutex_test.c
@@ -17,15 +17,32 @@
  * %PSC_END_COPYRIGHT%
  */
 
+#include <errno.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <pthread.h>
 
 #include "pfl/cdefs.h"
+#include "pfl/pfl.h"
 #include "psc_util/pthrutil.h"
+#include "psc_util/completion.h"
 
-const char *progname;
+const char		*progname;
+pthread_mutex_t		 m;
+struct psc_completion	 compl = PSC_COMPLETION_INIT;
+struct psc_waitq	 wq = PSC_WAITQ_INIT;
+
+void *
+spawn(__unusedx void *arg)
+{
+	psc_pthread_mutex_lock(&m);
+	psc_completion_done(&compl, 0);
+	psc_waitq_wait(&wq, NULL);
+	psc_pthread_mutex_unlock(&m);
+	return (NULL);
+}
 
 __dead void
 usage(void)
@@ -37,9 +54,10 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
-	pthread_mutex_t m;
-	int lk;
+	pthread_t pt;
+	int rc, lk;
 
+	pfl_init();
 	progname = argv[0];
 	if (getopt(argc, argv, "") != -1)
 		usage();
@@ -48,8 +66,22 @@ main(int argc, char *argv[])
 		usage();
 
 	psc_pthread_mutex_init(&m);
+
+	rc = pthread_create(&pt, NULL, spawn, NULL);
+	psc_completion_wait(&compl);
+
+	rc = pthread_mutex_trylock(&m);
+	psc_assert(rc == EBUSY);
+
+	psc_waitq_wakeall(&wq);
+
 	psc_pthread_mutex_lock(&m);
+	rc = pthread_mutex_lock(&m);
+	psc_assert(rc == EDEADLK);
+
 	lk = psc_pthread_mutex_reqlock(&m);
+
+	psc_pthread_mutex_ensure_locked(&m);
 	psc_pthread_mutex_ureqlock(&m, lk);
 	psc_pthread_mutex_unlock(&m);
 	exit(0);
