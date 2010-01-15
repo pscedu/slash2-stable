925f459a72edafdf0b3da3ddd844999518848997
Jared Yanovich <yanovich@psc.edu>
2010-01-15 00:54:11 +0000

    compilation improvements
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9853 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/rlimit.h
+++ psc_fsutil_libs/include/psc_util/rlimit.h
@@ -20,7 +20,9 @@
 #ifndef _PFL_RLIMIT_H_
 #define _PFL_RLIMIT_H_
 
-#include "sys/resource.h"
+#include <sys/resource.h>
+
+#include "psc_util/lock.h"
 
 int psc_setrlimit(int, rlim_t, rlim_t);
 int psc_getrlimit(int, rlim_t *, rlim_t *);
