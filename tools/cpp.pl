994b6a93c40a461f0c4fa80c0cdd73825ab5c767
Jared Yanovich <yanovich@psc.edu>
2010-01-19 06:36:59 +0000

    beef up
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@9891 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- tools/cpp.pl
+++ tools/cpp.pl
@@ -12,15 +12,18 @@ close F;
 # debugging file ID
 print qq{# 1 "$ARGV[0]"\n};
 
-if ($c =~ m!psc_util/log\.h!) {
+if ($c =~ m!psc_util/log\.h! && $ARGV[0] !~ m!/log\.c$!) {
 	# add missing return statements
 	$c =~ s/^}$/return;}/g;
 
-	# translate 'return;' into 'PSC_RETURNX();'
-	$c =~ s/\breturn;/PSC_RETURNX();/g;
+	# catch 'return' without an arg
+	$c =~ s/\breturn;/PFL_RETURNX();/g;
 
-	# translate 'return ...;' into 'PSC_RETURN(...);'
-	$c =~ s/\breturn\s*\(?(.*?)\)?\s*;/PSC_RETURN($1);/g;
+	# catch 'return' with a string literal arg
+	$c =~ s/\breturn\s*\(?\s*(".*?")\s*\)?\s*;/PFL_RETURN_STRLIT($1);/g;
+
+	# catch 'return' with an args
+	$c =~ s/\breturn\s*(.*?)\s*;/PFL_RETURN($1);/g;
 }
 
 print $c;
