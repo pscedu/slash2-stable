15acb7d2ed4617fedc4eae7d2e15f271ad5d5174
Jared Yanovich <yanovich@psc.edu>
2010-02-05 08:12:08 +0000

    more progress
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@10173 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/refmgr.h
+++ psc_fsutil_libs/include/psc_util/refmgr.h
@@ -24,11 +24,13 @@ struct psc_refmgr {
 	psc_spinlock_t			 prm_trlock;
 
 	/* offsets into various object properties */
+	int				 prm_lock_offset;
+	int				 prm_wait_offset;
 	int				 prm_list_offset;
 	int				 prm_lru_offset;
 	int				 prm_hash_offset;
 	int				 prm_tree_offset;
-	int				 prm_refmgr_offset;
+	int				 prm_refmgrtree_offset;
 	int				 prm_private_offset;
 };
 
@@ -48,21 +50,15 @@ struct psc_refmgr {
 #define PRMF_ANY			(PRMF_LIST | PRMF_LRU | PRMF_HASH | PRMF_TREE)
 
 struct psc_objref {
+#ifdef DEBUG
+	uint64_t			 pobj_magic;
+#endif
 	int				 pobj_refcnt;
+	int				 pobj_flags;
 	struct psclist_head		 pobj_pool_lentry;
-	union {
-		psc_spinlock_t		 pobj_locku_lock;
-		pthread_mutex_t		 pobj_locku_mutex;
-	}				 pobj_locku;
-	union {
-		struct psc_waitq	 pobj_waitu_waitq;
-		struct psc_multiwaitcond pobj_waitu_mwcond;
-	}				 pobj_waitu;
 
-#define pobj_lock	pobj_locku.pobj_locku_lock
-#define pobj_mutex	pobj_locku.pobj_locku_mutex
-#define pobj_waitq	pobj_waitu.pobj_waitu_waitq
-#define pobj_mwcond	pobj_waitu.pobj_waitu_mwcond
+	/* this is a ghost field, it will only be there if TREE is set in refmgr */
+	SPLAY_ENTRY(psc_objref)		 pobj_tree_entry;
 };
 
 /* pobj_flags */
@@ -70,24 +66,31 @@ struct psc_objref {
 #define POBJF_BUSY			(1 << 1)
 #define POBJF_MULTIWAIT			(1 << 2)
 
-#define PSC_GETOBJREF(p)		((struct psc_obj *)(p))
-#define PSC_GETOBJLOCK(p)		(&PSC_GETOBJREF(p)->pobj_lock)
-#define PSC_OBJREF_LOCK(p)		spinlock(PSC_GETOBJLOCK(p))
-#define PSC_OBJREF_RLOCK(p)		reqlock(PSC_GETOBJLOCK(p))
-#define PSC_OBJREF_ULOCK(p)		freelock(PSC_GETOBJLOCK(p))
-#define PSC_OBJREF_URLOCK(p, lk)	ureqlock(PSC_GETOBJLOCK(p), (lk))
+#define _PSC_OBJCAST(prm, pr, field, type)				\
+	((type *)((prm)->field + (char *)(pr)))
 
-#define PSC_OBJ_LOCK(p)			psc_objref_lock(psc_obj_getref(p))
-#define PSC_OBJ_ULOCK(p)		psc_objref_unlock(psc_obj_getref(p))
-#define PSC_OBJ_RLOCK(p)		psc_objref_reqlock(psc_obj_getref(p))
-#define PSC_OBJ_URLOCK(p, lk)		psc_objref_ureqlock(psc_obj_getref(p), (lk))
+#define PSC_OBJREF_GETLOCK(m, r)	_PSC_OBJCAST(m, r, prm_lock_offset, psc_spinlock_t)
+#define PSC_OBJREF_GETMUTEX(m, r)	_PSC_OBJCAST(m, r, prm_lock_offset, pthread_mutex_t)
+#define PSC_OBJREF_GETWAITQ(m, r)	_PSC_OBJCAST(m, r, prm_wait_offset, struct psc_waitq)
+#define PSC_OBJREF_GETMWCOND(m, r)	_PSC_OBJCAST(m, r, prm_wait_offset, struct psc_multiwaitcond)
+#define PSC_OBJREF_GETLISTENTRY(m, r)	_PSC_OBJCAST(m, r, prm_list_offset, struct psclist_head)
+#define PSC_OBJREF_GETLRUENTRY(m, r)	_PSC_OBJCAST(m, r, prm_lru_offset, struct psclist_head)
+#define PSC_OBJREF_GETHASHENTRY(m, r)	_PSC_OBJCAST(m, r, prm_hash_offset, struct psclist_head)
+#define PSC_OBJREF_GETREFMGRTREE(m, r)	_PSC_OBJCAST(m, r, prm_refmgrtree_offset, struct psc_refmgr)
+#define PSC_OBJREF_GETPRIVATE(m, r)	_PSC_OBJCAST(m, r, prm_private_offset, void)
 
 void	 psc_refmgr_init(struct psc_refmgr *, int, int, int, int, int,
 	    int (*)(struct psc_poolmgr *, void *), void (*)(void *),
 	    const char *, ...);
 
-void	*psc_refmgr_findobj(struct psc_refmgr *, int, void *);
+void	*_psc_refmgr_findobj(struct psc_refmgr *, int, void *, int);
 void	*psc_refmgr_getobj(struct psc_refmgr *, int, void *);
+
+#define psc_refmgr_findremoveobj(prm, pref, key)			\
+	psc_refmgr_findobj((prm), (pref), (key), 1)
+
+#define psc_refmgr_findobj(prm, pref, key)				\
+	psc_refmgr_findobj((prm), (pref), (key), 0)
 
 int	 psc_objref_cmp(const void *, const void *);
 
@@ -95,30 +98,13 @@ void	 psc_obj_share(struct psc_refmgr *prm, void *);
 void	 psc_obj_incref(struct psc_refmgr *prm, void *);
 void	 psc_obj_decref(struct psc_refmgr *prm, void *);
 
+void	 psc_obj_dump(const struct psc_refmgr *prm, const void *);
+
 static __inline struct psc_objref *
 psc_obj_getref(const struct psc_refmgr *prm, void *p)
 {
 	psc_assert(p);
-	return ((char *)p - prm->prm_private_offset);
-}
-
-static __inline void
-psc_objref_wait(struct psc_objref *pobj)
-{
-	psc_objref_rlock(pobj);
-	if (pobj->pobj_flags & POBJF_MULTIWAIT)
-		psc_multiwaitcond_wait(&pobj->pobj_mwcond, &pobj->pobj_mutex);
-	else
-		psc_waitq_wait(&pobj->pobj_waitq, &pobj->pobj_lock);
-}
-
-static __inline void
-psc_objref_wake(struct psc_objref *pobj)
-{
-	if (pobj->pobj_flags & POBJF_MULTIWAIT)
-		psc_multiwaitcond_wakeup(&pobj->pobj_mwcond);
-	else
-		psc_waitq_wakeall(&pobj->pobj_waitq);
+	return ((void *)((char *)p - prm->prm_private_offset));
 }
 
 static __inline void
@@ -131,7 +117,7 @@ psc_objref_lock(struct psc_objref *pobj)
 }
 
 static __inline void
-psc_objref_unlock(struct psc_objref *pboj)
+psc_objref_unlock(struct psc_objref *pobj)
 {
 	if (pobj->pobj_flags & POBJF_MULTIWAIT)
 		psc_pthread_mutex_unlock(&pobj->pobj_mutex);
@@ -156,6 +142,25 @@ psc_objref_ureqlock(struct psc_objref *pobj, int locked)
 		ureqlock(&pobj->pobj_lock, locked);
 }
 
-SPLAY_GENERATE(psc_objref_tree, psc_objref, pobjf_tree_tentry, psc_objref_cmp);
+static __inline void
+psc_objref_wait(struct psc_objref *pobj)
+{
+	psc_objref_reqlock(pobj);
+	if (pobj->pobj_flags & POBJF_MULTIWAIT)
+		psc_multiwaitcond_wait(&pobj->pobj_mwcond, &pobj->pobj_mutex);
+	else
+		psc_waitq_wait(&pobj->pobj_waitq, &pobj->pobj_lock);
+}
+
+static __inline void
+psc_objref_wake(struct psc_objref *pobj)
+{
+	if (pobj->pobj_flags & POBJF_MULTIWAIT)
+		psc_multiwaitcond_wakeup(&pobj->pobj_mwcond);
+	else
+		psc_waitq_wakeall(&pobj->pobj_waitq);
+}
+
+SPLAY_GENERATE(psc_objref_tree, psc_objref, pobj_tree_entry, psc_objref_cmp);
 
 #endif /* _PFL_REFMGR_H_ */
