cee719c8939038e3f5bd98ba66398747696be0d1
Jared Yanovich <yanovich@psc.edu>
2010-02-05 08:11:58 +0000

    accept multiple args to options in perl getopts().  totally ridiculous that it has to be done manually
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@10172 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- tools/gentypes.pl
+++ tools/gentypes.pl
@@ -40,21 +40,60 @@ sub filter {
 	return @a;
 }
 
+sub getoptv {
+	my ($optstr, $ropts) = @_;
+	my @av = @ARGV;
+
+	getopts($optstr, $ropts) or return 0;
+
+	for (my $j = 0; $j < length($optstr); $j++) {
+		delete $ropts->{$1}, $j++ if substr($optstr, $j, 2) =~ /^([a-zA-Z0-9]):/;
+	}
+
+	ARG: for (my $i = 0; $i < @av; $i++) {
+		my $s = $av[$i];
+		if ($s =~ /^-/) {
+			for (my $k = 1; $k < length($s); $k++) {
+				for (my $j = 0; $j < length($optstr); $j++) {
+					my $ch = substr($optstr, $j, 1);
+					my $wantarg = $j + 1 < length($optstr) &&
+					    substr($optstr, $j + 1, 1) eq ":";
+					next unless $wantarg;
+					$j++, next unless substr($s, $k, 1) eq $ch;
+
+					my $arg;
+					if ($k + 1 == length($s)) {
+						$arg = $av[$i + 1];
+						$s++;
+					} else {
+						$arg = substr($s, $k + 1);
+					}
+					$ropts->{$ch} = [] unless exists $ropts->{$ch};
+					push @{ $ropts->{$ch} }, $arg;
+					next ARG;
+				}
+			}
+		}
+	}
+	return 1;
+}
+
 my %opts;
-getopts("g:h:x:", \%opts) or usage();
+getoptv("g:h:x:", \%opts) or usage();
 usage() unless @ARGV == 1;
 
 my $outfn = $ARGV[0];
 
 my @vals;
 my @types;
-my @hdrs = uniq sort <$opts{h}>;
+my @hdrs;
+@hdrs = uniq sort map { glob } @{ $opts{h} } if $opts{h};
 if ($opts{x}) {
-	@hdrs = filter [<$opts{x}>], @hdrs;
+	@hdrs = filter [ map { glob } @{ $opts{x} } ], @hdrs;
 }
 my $lvl = 0;
 foreach my $hdr (@hdrs) {
-	open HDR, "<", $hdr;
+	open HDR, "<", $hdr or die "$hdr: $!\n";
 	while (<HDR>) {
 		if ($lvl) {
 			$lvl++ if /^\s*#if/;
