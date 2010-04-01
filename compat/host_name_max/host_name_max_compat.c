cab9fd4ded41b41e62e7cbadd5249fa24459771b
Jared Yanovich <yanovich@psc.edu>
2010-04-01 14:14:29 +0000

    expand compatibility tests a bit
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@11028 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ compat/host_name_max/host_name_max_compat.c
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+#include <sys/param.h>
+
+#include <stdlib.h>
+
+#include "pfl/cdefs.h"
+
+int
+main(__unusedx int argc, __unusedx char *argv[])
+{
+	int n;
+
+	n = HOST_NAME_MAX;
+	exit(0);
+}
