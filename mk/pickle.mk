74b172c71582025de29b3a4e1aba39af87e3d904
Jared Yanovich <yanovich@psc.edu>
2010-04-02 03:57:46 +0000

    add pthread_barrier pickle test hooks
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@11058 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/pickle.mk
+++ mk/pickle.mk
@@ -6,7 +6,7 @@ PICKLELOCALMK=${ROOTDIR}/mk/gen-localdefs-pickle.mk
 
 -include ${PICKLELOCALMK}
 
-PICKLE_NEED_VERSION=		1
+PICKLE_NEED_VERSION=					2
 
 ifneq (${PICKLE_NEED_VERSION},${PICKLE_HAS_VERSION})
  $(shell ${PICKLEGEN} "${ROOTDIR}" "${PICKLE_NEED_VERSION}" "${MAKE}" "${PICKLELOCALMK}" >&2)
@@ -21,12 +21,22 @@ ifdef PICKLE_HAVE_PTHREAD_MUTEX_TIMEDLOCK
   DEFINES+=						-DHAVE_PTHREAD_MUTEX_TIMEDLOCK
 endif
 
-ifndef PICKLE_HAVE_CLOCK_GETTIME
+ifdef PICKLE_HAVE_CLOCK_GETTIME
+  DEFINES+=						-DHAVE_CLOCK_GETTIME
+else
   SRCS+=						${ROOTDIR}/psc_fsutil_libs/compat/clock_gettime.c
 endif
 
 ifndef PICKLE_HAVE_HOST_NAME_MAX
   DEFINES+=						-DHOST_NAME_MAX=MAXHOSTNAMELEN
+endif
+
+ifneq ($(filter pthread,${MODULES}),)
+ ifndef PICKLE_HAVE_PTHREAD_BARRIER
+  SRCS+=						${ROOTDIR}/psc_fsutil_libs/compat/pthread_barrier.c
+ else
+  DEFINES+=						-DHAVE_PTHREAD_BARRIER
+ endif
 endif
 
 endif
