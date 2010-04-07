186d51939fc5443571e76e7364f662c07314078b
Jared Yanovich <yanovich@psc.edu>
2010-04-07 15:26:43 +0000

    more compat junk
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@11220 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/include/pfl/fcntl.h
@@ -0,0 +1,44 @@
+/* $Id$ */
+
+#ifndef _PFL_FCNTL_H_
+#define _PFL_FCNTL_H_
+
+#include <fcntl.h>
+
+#ifndef O_DSYNC
+#define O_DSYNC		0
+#endif
+
+#ifndef O_RSYNC
+#define O_RSYNC		0
+#endif
+
+#ifndef O_SHLOCK
+#define O_SHLOCK	0
+#endif
+
+#ifndef O_EXLOCK
+#define O_EXLOCK	0
+#endif
+
+#ifndef O_DIRECT
+#define O_DIRECT	0
+#endif
+
+#ifndef O_SYMLINK
+#define O_SYMLINK	0
+#endif
+
+#ifndef O_NOATIME
+#define O_NOATIME	0
+#endif
+
+#ifndef O_LARGEFILE
+#define O_LARGEFILE	0
+#endif
+
+#ifndef O_CLOEXEC
+#define O_CLOEXEC	0
+#endif
+
+#endif /* _PFL_FCNTL_H_ */
