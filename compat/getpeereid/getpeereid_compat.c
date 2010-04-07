c53084858e664465cba3d9882401d2625bd56c9c
Jared Yanovich <yanovich@psc.edu>
2010-04-07 05:32:36 +0000

    add getpeereid() compat test
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@11208 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- compat/getpeereid/getpeereid_compat.c
+++ compat/getpeereid/getpeereid_compat.c
@@ -1,12 +1,17 @@
 /* $Id$ */
 
-#include <rtnetlink.h>
+#include <sys/types.h>
+
 #include <stdlib.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
+	void *p;
+
 	(void)argc;
 	(void)argv;
+	p = getpeereid;
 	exit(0);
 }
