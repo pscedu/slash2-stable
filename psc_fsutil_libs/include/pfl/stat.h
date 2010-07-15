820ae6d5f15e4715c6399f12a9a1d422784f4571
Jared Yanovich <yanovich@psc.edu>
2010-07-15 17:33:26 +0000

    use stb_[acm]timespec when its available
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@12370 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/pfl/stat.h
+++ psc_fsutil_libs/include/pfl/stat.h
@@ -20,22 +20,33 @@
 #ifndef _PFL_STAT_H_
 #define _PFL_STAT_H_
 
-#ifdef HAVE_STB_TIM
-# define PFL_STB_ATIME_SET(s, ns, stb)	do { (stb)->st_atim.tv_sec = (s); (stb)->st_atim.tv_nsec = (ns); } while (0)
-# define PFL_STB_MTIME_SET(s, ns, stb)	do { (stb)->st_mtim.tv_sec = (s); (stb)->st_mtim.tv_nsec = (ns); } while (0)
-# define PFL_STB_CTIME_SET(s, ns, stb)	do { (stb)->st_ctim.tv_sec = (s); (stb)->st_ctim.tv_nsec = (ns); } while (0)
+#if defined(HAVE_STB_TIM) || defined(HAVE_STB_TIMESPEC)
 
-# define PFL_STB_ATIME_GET(stb, s, ns)	do { *(s) = (stb)->st_atim.tv_sec; *(ns) = (stb)->st_atim.tv_nsec; } while (0)
-# define PFL_STB_MTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_mtim.tv_sec; *(ns) = (stb)->st_mtim.tv_nsec; } while (0)
-# define PFL_STB_CTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_ctim.tv_sec; *(ns) = (stb)->st_ctim.tv_nsec; } while (0)
+# ifdef HAVE_STB_TIM
+#  define st_pfl_atim st_atim
+#  define st_pfl_ctim st_ctim
+#  define st_pfl_mtim st_mtim
+# else
+#  define st_pfl_atim st_atimespec
+#  define st_pfl_ctim st_ctimespec
+#  define st_pfl_mtim st_mtimespec
+# endif
+
+# define PFL_STB_ATIME_SET(s, ns, stb)	do { (stb)->st_pfl_atim.tv_sec = (s); (stb)->st_pfl_atim.tv_nsec = (ns); } while (0)
+# define PFL_STB_MTIME_SET(s, ns, stb)	do { (stb)->st_pfl_mtim.tv_sec = (s); (stb)->st_pfl_mtim.tv_nsec = (ns); } while (0)
+# define PFL_STB_CTIME_SET(s, ns, stb)	do { (stb)->st_pfl_ctim.tv_sec = (s); (stb)->st_pfl_ctim.tv_nsec = (ns); } while (0)
+
+# define PFL_STB_ATIME_GET(stb, s, ns)	do { *(s) = (stb)->st_pfl_atim.tv_sec; *(ns) = (stb)->st_pfl_atim.tv_nsec; } while (0)
+# define PFL_STB_MTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_pfl_mtim.tv_sec; *(ns) = (stb)->st_pfl_mtim.tv_nsec; } while (0)
+# define PFL_STB_CTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_pfl_ctim.tv_sec; *(ns) = (stb)->st_pfl_ctim.tv_nsec; } while (0)
 #else
 # define PFL_STB_ATIME_SET(s, ns, stb)	do { (stb)->st_atime = (s); } while (0)
 # define PFL_STB_MTIME_SET(s, ns, stb)	do { (stb)->st_mtime = (s); } while (0)
 # define PFL_STB_CTIME_SET(s, ns, stb)	do { (stb)->st_ctime = (s); } while (0)
 
-# define PFL_STB_ATIME_GET(stb, s, ns)	do { *(s) = (stb)->st_atim.tv_sec; *(ns) = 0; } while (0)
-# define PFL_STB_MTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_mtim.tv_sec; *(ns) = 0; } while (0)
-# define PFL_STB_CTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_ctim.tv_sec; *(ns) = 0; } while (0)
+# define PFL_STB_ATIME_GET(stb, s, ns)	do { *(s) = (stb)->st_atime; *(ns) = 0; } while (0)
+# define PFL_STB_MTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_mtime; *(ns) = 0; } while (0)
+# define PFL_STB_CTIME_GET(stb, s, ns)	do { *(s) = (stb)->st_ctime; *(ns) = 0; } while (0)
 #endif
 
 #endif /* _PFL_STAT_H_ */
