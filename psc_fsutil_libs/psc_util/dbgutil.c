bbb63e2887368cb307a4a956c4420ab5dce0d167
Jared Yanovich <yanovich@psc.edu>
2010-08-12 19:07:53 +0000

    add pfl_dump_mode()
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@12943 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_util/dbgutil.c
+++ psc_fsutil_libs/psc_util/dbgutil.c
@@ -66,6 +66,59 @@ pfl_dump_fflags(int fflags)
 	printf("\n");
 }
 
+void
+pfl_dump_mode(mode_t m)
+{
+	char ch, buf[10];
+
+	if (S_ISDIR(m))
+		ch = 'd';
+	else if (S_ISCHR(m))
+		ch = 'c';
+	else if (S_ISBLK(m))
+		ch = 'b';
+	else if (S_ISSOCK(m))
+		ch = 's';
+	else if (S_ISDIR(m))
+		ch = 'd';
+	else if (S_ISLNK(m))
+		ch = 'l';
+	else if (S_ISFIFO(m))
+		ch = 'f';
+	else if (S_ISREG(m))
+		ch = '-';
+	else
+		ch = '?';
+
+	buf[0] = m & S_IRUSR ? 'r' : '-';
+	buf[1] = m & S_IWUSR ? 'w' : '-';
+	if (m & S_ISUID)
+		buf[2] = m & S_IXUSR ? 's' : 'S';
+	else
+		buf[2] = m & S_IXUSR ? 'x' : '-';
+
+	buf[3] = m & S_IRGRP ? 'r' : '-';
+	buf[4] = m & S_IWGRP ? 'w' : '-';
+	if (m & S_ISGID)
+		buf[5] = m & S_IXGRP ? 's' : 'S';
+	else
+		buf[5] = m & S_IXGRP ? 'x' : '-';
+
+	buf[6] = m & S_IROTH ? 'r' : '-';
+	buf[7] = m & S_IWOTH ? 'w' : '-';
+	if (m & S_ISVTX)
+		buf[8] = m & S_IXOTH ? 't' : 'T';
+	else
+		buf[8] = m & S_IXOTH ? 'x' : '-';
+	buf[9] = '\0';
+	m &= ~(ALLPERMS | S_IFMT);
+
+	printf("%c%s", ch, buf);
+	if (m)
+		printf(" unknown bits: %#o\n", m);
+	printf("\n");
+}
+
 __weak int
 psc_log_getlevel(__unusedx int subsys)
 {
