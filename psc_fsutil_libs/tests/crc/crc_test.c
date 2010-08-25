4d0726e42767e518ff600b6e37de276a68336a21
Jared Yanovich <yanovich@psc.edu>
2010-08-25 18:39:58 +0000

    add tester for crc
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@13188 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/tests/crc/crc_test.c
@@ -0,0 +1,37 @@
+/* $Id$ */
+/* %PSC_COPYRIGHT% */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "pfl/cdefs.h"
+#include "pfl/pfl.h"
+#include "psc_util/crc.h"
+
+const char *progname;
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", progname);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	uint64_t crc;
+	char n = 0;
+
+	pfl_init();
+	progname = argv[0];
+	if (getopt(argc, argv, "") != -1)
+		usage();
+	argc -= optind;
+	if (argc)
+		usage();
+
+	psc_crc64_calc(&crc, &n, 1);
+	exit(0);
+}
