74ccca5522ed6ec968a48db270434eb9010950cf
Jared Yanovich <yanovich@psc.edu>
2010-09-16 14:48:38 +0000

    add simple script for checking double free()
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@13692 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ tools/allocmatch.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -W
+# $Id$
+
+use strict;
+use warnings;
+
+my $s;
+{
+	local $/;
+	$s = <>;
+}
+
+while ($s =~ /free\((0x[0-9a-f]+)\)/gc) {
+	my $pos = $+[0];
+	my $addr = $1;
+
+	print $addr, "\n" if
+	    $' =~ /free\($addr\)/ and
+	    $` =~ /alloc\(0x[0-9a-f]+\)=$addr/;
+}
