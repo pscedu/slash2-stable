64c4ad608a7684c7161c4ce3a61b749200441d21
Jared Yanovich <yanovich@psc.edu>
2010-10-06 19:27:35 +0000

    introduce use-after-free operation when PFL_DEBUG > 1 (PFL_DEBUG is set to 1 by default).
    so, if you want to use it, you have to do:
    
      $ DEBUG=2 make build
    
    "linger" mode is implemented with a LRU with entries placed on one end
    on free(3) and eventually pulled off on alloc(3) when the configured
    list size is full.
    
    also, move handling of realloc(p, 0) to the macros; its much easier.
    
    
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@14072 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/alloc.h
+++ psc_fsutil_libs/include/psc_util/alloc.h
@@ -33,6 +33,21 @@
 # include "pfl/compat/posix_memalign.h"
 #endif
 
+/*
+ * The -DPFL_DEBUG define operates the following way:
+ *
+ *	+-----------+-------------------------------------------------+
+ *	|           |                      Feature                    |
+ *	| PFL_DEBUG +--------+-------------+-------------+------------+
+ *	|           | checks |   linger    | prot-before | prot-after |
+ *	+-----------+--------+-------------+-------------+------------+
+ *	| 0	    |        |             |             |            |
+ *	| 1	    |   X    |             |             |            |
+ *	| 2	    |   X    | if LINGERSZ |      X      |            |
+ *	| 3	    |   X    | if LINGERSZ |             |      X     |
+ *	+-----------+--------+-------------+-------------+------------+
+ */
+
 /* aliases for common usage */
 #define PSCALLOC(sz)		psc_alloc((sz), 0)
 #define TRY_PSCALLOC(sz)	psc_alloc((sz), PAF_CANFAIL)
@@ -40,58 +55,22 @@
 #define PSC_REALLOC(p, sz)	psc_realloc((p), (sz), 0)
 #define PSC_TRY_REALLOC(p, sz)	psc_realloc((p), (sz), PAF_CANFAIL)
 
-struct psc_memalloc {
-	void			*pma_allocbase;		/* guarded alloc region */
-	void			*pma_userbase;		/* user alloc region */
-#ifndef __LP64__
-	long			_pma_pad;
-#endif
-	void			*pma_guardbase;		/* user alloc region */
-	size_t			 pma_userlen;
-	struct psc_hashent	 pma_hentry;
-};
+#define PFL_MEMGUARD_LINGERSZ	10000	/* # freed allocs to keep around */
 
-#if PFL_DEBUG > 1
-
-/*
- * Memory guard legend:
- *	X - no access (PROT_NONE)
- *	U - user region
- *	G - guard byte, value checked in free()
- *	< - left fill
- *	> - right fill
- *
- * If PFL_DEBUG > 2, the layout looks like this ("guard after"):
- *
- *	 <----- PAGE -----> <----- PAGE ----->
- *	+-----------+------+------------------+
- *	| GGGGGGGG> | UUUU | XXXXXXXXXXXXXXXX |
- *	+-----------+------+------------------+
- *
- * Otherwise, the layout looks like this ("guard before"):
- *
- *	 <----- PAGE -----> <----- PAGE ----->
- *	+------------------+------+-----------+
- *	| XXXXXXXXXXXXXXXX | UUUU | <GGGGGGGG |
- *	+------------------+------+-----------+
- *
- * If PAF_ALIGN was specified, case #2 must always be used.  The guard
- * checks should hopefully be enough to catch out-of-range memory
- * access.
- *
- * When a region of memory is freed, the page(s) constituting the area
- * are mapped PROT_NONE.
- */
-
-#define PFL_MEMGUARD_MAGIC	0x7a
+#if PFL_DEBUG > 1 && PFL_MEMGUARD_LINGERSZ
+#  define _PFL_FREE		_psc_lru_userfree
+#else
+#  define _PFL_FREE		_psc_free
 #endif
 
 #define psc_free(p, flags, ...)						\
 	do {								\
 		if (((flags) & PAF_NOLOG) == 0)				\
-			psc_debugs(PSS_MEM, "free(%p)", (p));		\
-		_psc_free((p), (flags), ##__VA_ARGS__);			\
-		(p) = NULL;						\
+			psclogs_debug(PSS_MEM, "free(%p)", (p));	\
+		if (p) {						\
+			_PFL_FREE((p), (flags), ##__VA_ARGS__);		\
+			(p) = NULL;					\
+		}							\
 	} while (0)
 
 #define PSCFREE(p)		psc_free((p), 0)
@@ -100,16 +79,27 @@ struct psc_memalloc {
 	{								\
 		void *_p;						\
 									\
-		_p = _psc_realloc((oldp), (sz), (fl));			\
-		if (((fl) & PAF_NOLOG) == 0) {				\
-			if (oldp)					\
-				psc_debugs(PSS_MEM, "realloc(%p)=%p "	\
-				    "sz=%zu fl=%d", (void *)(oldp), _p,	\
-				    (size_t)(sz), (fl));		\
-			else						\
-				psc_debugs(PSS_MEM, "alloc()=%p "	\
-				    "sz=%zu fl=%d", _p, (size_t)(sz),	\
-				    (fl));				\
+		if ((oldp) && (sz) == 0) {				\
+			psc_assert(((fl) & PAF_LOCK) == 0);		\
+			if (((fl) & PAF_NOLOG) == 0)			\
+				psclogs_debug(PSS_MEM,			\
+				    "realloc(%p) to zero (free)",	\
+				    (oldp));				\
+			_PFL_FREE((oldp), (fl));			\
+			_p = NULL;					\
+		} else {						\
+			_p = _psc_realloc((oldp), (sz), (fl));		\
+			if (((fl) & PAF_NOLOG) == 0) {			\
+				if (oldp)				\
+					psclogs_debug(PSS_MEM,		\
+					    "realloc(%p)=%p sz=%zu "	\
+					    "fl=%d", (void *)(oldp),	\
+					    _p, (size_t)(sz), (fl));	\
+				else					\
+					psclogs_debug(PSS_MEM,		\
+					    "alloc()=%p sz=%zu fl=%d",	\
+					    _p, (size_t)(sz), (fl));	\
+			}						\
 		}							\
 		_p;							\
 	}
@@ -123,13 +113,14 @@ struct psc_memalloc {
 #define PAF_CANFAIL		(1 << 0)	/* return NULL instead of fatal */
 #define PAF_PAGEALIGN		(1 << 1)	/* align to physmem page size */
 #define PAF_NOREAP		(1 << 2)	/* don't reap pools if mem unavail */
-#define PAF_LOCK		(1 << 3)	/* lock mem regions as unswappable */
+#define PAF_LOCK		(1 << 3)	/* mlock(2) mem regions as unswappable */
 #define PAF_NOZERO		(1 << 4)	/* don't force memory zeroing */
 #define PAF_NOLOG		(1 << 5)	/* don't psclog this allocation */
 #define PAF_NOGUARD		(1 << 6)	/* do not use memory guards */
 
 void	 *psc_calloc(size_t, size_t, int);
 void	 _psc_free(void *, int, ...);
+void	 _psc_lru_userfree(void *, int, ...);
 void	*_psc_realloc(void *, size_t, int);
 char	 *psc_strdup(const char *);
 
--- psc_fsutil_libs/psc_util/alloc.c
+++ psc_fsutil_libs/psc_util/alloc.c
@@ -33,10 +33,65 @@
 #include "psc_util/log.h"
 
 #if PFL_DEBUG > 1
-#  include "psc_util/mem.h"
+
+struct psc_memalloc {
+	void			*pma_allocbase;		/* guarded alloc region */
+	void			*pma_userbase;		/* user alloc region */
+#ifndef __LP64__
+	long			_pma_pad;
+#endif
+	void			*pma_guardbase;		/* user alloc region */
+	size_t			 pma_userlen;
+	union {
+		struct psc_hashent	 pmau_hentry;
+		struct psc_listentry	 pmau_lru_lentry;
+	} pma_u;
+#define pma_hentry	pma_u.pmau_hentry
+#define pma_lru_lentry	pma_u.pmau_lru_lentry
+};
+
+#define PMA_LEN(pma)		(psc_pagesize +				\
+				 PSC_ALIGN((pma)->pma_userlen, psc_pagesize))
+
+/*
+ * Memory guard diagram legend:
+ *	X - no access (PROT_NONE)
+ *	U - user region
+ *	G - inner guard byte, checked in free() and realloc()
+ *	< - left fill
+ *	> - right fill
+ *
+ * If PFL_DEBUG > 2, the layout looks like this ("guard after"):
+ *
+ *	 <----- PAGE -----> <----- PAGE ----->
+ *	+-----------+------+------------------+
+ *	| GGGGGGGG> | UUUU | XXXXXXXXXXXXXXXX |
+ *	+-----------+------+------------------+
+ *
+ * Otherwise, the layout looks like this ("guard before"):
+ *
+ *	 <----- PAGE -----> <----- PAGE ----->
+ *	+------------------+------+-----------+
+ *	| XXXXXXXXXXXXXXXX | UUUU | <GGGGGGGG |
+ *	+------------------+------+-----------+
+ *
+ * If PAF_ALIGN was specified, case #2 must always be used.  The guard
+ * checks should hopefully be enough to catch out-of-range memory
+ * access.
+ *
+ * When a region of memory is freed, the page(s) constituting the area
+ * are mapped PROT_NONE.
+ */
+
+#  define PFL_MEMGUARD_MAGIC	0x7a	/* guard byte val written within page */
 #endif
 
-#define GUARD_AFTER (PFL_DEBUG > 2)
+#define GUARD_AFTER	(PFL_DEBUG > 2)
+
+#if PFL_DEBUG > 1
+#  include "psc_ds/list.h"
+#  include "psc_util/mem.h"
+#endif
 
 struct psc_memalloc_key {
 	void *p;
@@ -53,6 +108,8 @@ int			psc_pagesize;
 
 #if PFL_DEBUG > 1
 struct psc_hashtbl	psc_memallocs;
+struct psc_lockedlist	psc_memallocs_lru =
+    PLL_INIT_NOLOG(&psc_memallocs_lru, struct psc_memalloc, pma_lru_lentry);
 #endif
 
 /**
@@ -79,20 +136,20 @@ _psc_getpma(void *p)
 }
 
 void
-_psc_checkpma(struct psc_memalloc *pma)
+pscmem_checkinnerguard(struct psc_memalloc *pma)
 {
-	if (pma->pma_userlen % psc_pagesize)
-		psc_assert(pfl_memchk(pma->pma_guardbase, PFL_MEMGUARD_MAGIC,
-		    psc_pagesize - pma->pma_userlen % psc_pagesize));
+	size_t len;
 
-	/*
-	 * XXX disable access to region; add a mode where we leak on
-	 * purpose with PROT_NONE to prevent use-after-free.
-	 */
-	psc_mprotect(pma->pma_allocbase, psc_pagesize +
-	    PSC_ALIGN(pma->pma_userlen, psc_pagesize),
-	    PROT_READ | PROT_WRITE);
+	if (pma->pma_userlen % psc_pagesize) {
+		len = psc_pagesize - pma->pma_userlen % psc_pagesize;
+		psc_mprotect(MIN(pma->pma_guardbase, pma->pma_userbase),
+		    len, PROT_READ);
+		psc_assert(pfl_memchk(pma->pma_guardbase,
+		    PFL_MEMGUARD_MAGIC, len));
+	}
 }
+
+void _psc_lru_sysfree(struct psc_memalloc *);
 #endif
 
 /**
@@ -112,6 +169,17 @@ _psc_realloc(void *oldp, size_t size, int flags)
 	struct psc_memalloc *pma;
 	size_t specsize, oldlen;
 
+#  if PFL_MEMGUARD_LINGERSZ
+	if (oldp == NULL) {
+		PLL_LOCK(&psc_memallocs_lru);
+		if (pll_nitems(&psc_memallocs_lru) >= PFL_MEMGUARD_LINGER_SZ) {
+			pma = pll_get(&psc_memallocs_lru);
+			_psc_lru_sysfree(pma);
+		}
+		PLL_ULOCK(&psc_memallocs_lru);
+	}
+#  endif
+
 	/*
 	 * Since PAGEALIGN'd allocations must end on a page-boundary,
 	 * there is no guarentee they can begin on one, so we must force
@@ -122,25 +190,26 @@ _psc_realloc(void *oldp, size_t size, int flags)
 		guard_after = 0;
 
 	if (oldp && (flags & PAF_NOGUARD) == 0) {
-		if (flags & PAF_PAGEALIGN)
-			errx(1, "realloc of page-aligned is not implemented");
-
 		pma = _psc_getpma(oldp);
 
 		/* XXX if CANFAIL occurs, restore protections. */
-		_psc_checkpma(pma);
-
+		pscmem_checkinnerguard(pma);
+		psc_mprotect(pma->pma_allocbase, PMA_LEN(pma),
+		    PROT_READ | PROT_WRITE);
 		oldp = pma->pma_allocbase;
 		oldlen = pma->pma_userlen;
 	}
 #endif
+
+	if (oldp && (flags & PAF_PAGEALIGN))
+		errx(1, "realloc of page-aligned is not implemented");
 
 	/*
 	 * Disallow realloc(p, sz, PAF_LOCK) because we can't munlock
 	 * the old region as we don't have the size.
 	 */
 	if ((flags & PAF_LOCK) && oldp)
-		psc_fatalx("unable to lock realloc'd mem");
+		psc_fatalx("unable to realloc mlocked mem");
 
 	/*
 	 * If the new allocation size is 0, treat it like a free and
@@ -148,15 +217,8 @@ _psc_realloc(void *oldp, size_t size, int flags)
 	 * wants with NULL in place of a zero-length pointer as this
 	 * behavior differs on systems.
 	 */
-	if (size == 0) {
-		if (oldp) {
-#if PFL_DEBUG > 1
-			free(pma);
-#endif
-			free(oldp);
-		}
-		return (NULL);
-	}
+	if (oldp)
+		psc_assert(size);
 
 #if PFL_DEBUG > 1
 	if ((flags & PAF_NOGUARD) == 0) {
@@ -175,10 +237,6 @@ _psc_realloc(void *oldp, size_t size, int flags)
 
  retry:
 	if (flags & PAF_PAGEALIGN) {
-#if PFL_DEBUG <= 1
-		if (oldp)
-			errx(1, "realloc of page-aligned is not implemented");
-#endif
 		rc = posix_memalign(&newp, psc_pagesize, size);
 		if (rc) {
 			errno = rc;
@@ -187,12 +245,12 @@ _psc_realloc(void *oldp, size_t size, int flags)
 		} else if (oldp) {
  movepage:
 			/*
-			 * Since realloc() does not work with
-			 * posix_memalign() in that a page-aligned base
-			 * is guarenteed to be returned, fabricate a
-			 * realloc, solely for the PFL memory guard, by
-			 * issuing a new page-aligned alloc, copying,
-			 * and freeing the old chunk.
+			 * To support protected PAGEALIGN'd allocations,
+			 * since realloc() does not work with
+			 * posix_memalign() such that a page-aligned
+			 * base is guarenteed to be returned, fabricate
+			 * a realloc by issuing a new page-aligned
+			 * alloc, copying, and freeing the old chunk.
 			 */
 			if (guard_after)
 				memmove((char *)newp + (psc_pagesize -
@@ -202,8 +260,7 @@ _psc_realloc(void *oldp, size_t size, int flags)
 				memmove((char *)newp +
 				    (pma->pma_userbase - pma->pma_allocbase),
 				    pma->pma_userbase, pma->pma_userlen);
-			if (size != psc_pagesize +
-			    PSC_ALIGN(pma->pma_userlen, psc_pagesize)) {
+			if (size != PMA_LEN(pma)) {
 				free(pma->pma_allocbase);
 				goto setupguard;
 			}
@@ -235,12 +292,13 @@ _psc_realloc(void *oldp, size_t size, int flags)
 		if (oldp == NULL) {
 			/*
 			 * XXX consider using guard region for this to
-			 * save on two malloc overhead.
+			 * save on two malloc overhead or pools.
 			 */
 			pma = calloc(1, sizeof(*pma));
 			psc_assert(pma);
 			psc_hashent_init(&psc_memallocs, pma);
 		}
+
  setupguard:
 		rem = (psc_pagesize - specsize % psc_pagesize) % psc_pagesize;
 
@@ -250,19 +308,12 @@ _psc_realloc(void *oldp, size_t size, int flags)
 		if (guard_after) {
 			pma->pma_userbase = (char *)newp + rem;
 			pma->pma_guardbase = (char *)newp;
-
-			psc_mprotect(newp, PSC_ALIGN(specsize, psc_pagesize),
-			    PROT_READ | PROT_WRITE);
 			psc_mprotect((char *)newp + size - psc_pagesize,
 			    psc_pagesize, PROT_NONE);
 		} else {
 			pma->pma_userbase = (char *)newp + psc_pagesize;
 			pma->pma_guardbase = (char *)pma->pma_userbase +
 			    specsize;
-
-			psc_mprotect(newp + psc_pagesize,
-			    PSC_ALIGN(specsize, psc_pagesize),
-			    PROT_READ | PROT_WRITE);
 			psc_mprotect(newp, psc_pagesize, PROT_NONE);
 		}
 		memset(pma->pma_guardbase, PFL_MEMGUARD_MAGIC, rem);
@@ -337,13 +388,66 @@ psc_strdup(const char *str)
 	return (p);
 }
 
+#if PFL_MEMGUARD_LINGERSZ
+/**
+ * _psc_lru_userfree - The user wants to release a chunk of memory.
+ *	Instead of releasing it back into the system, set PROT_NONE
+ *	on it and keep it around for awhile.
+ * @p: memory base.
+ * @flags: flags the region was allocated with.
+ */
+void
+_psc_lru_userfree(void *p, int flags, ...)
+{
+	struct psc_memalloc *pma;
+
+	if (flags & PAF_LOCK) {
+		size_t len;
+		va_list ap;
+
+		va_start(ap, flags);
+		len = va_arg(ap, size_t);
+		va_end(ap);
+
+		if (munlock(p, len) == -1)
+			psc_fatal("munlock %p", p);
+	}
+
+	pma = _psc_getpma(p);
+	pscmem_checkinnerguard(pma);
+	psc_mprotect(pma->pma_allocbase, PMA_LEN(pma), PROT_NONE);
+	INIT_PSC_LISTENTRY(&pma->pma_lru_lentry);
+	pll_add(&psc_memallocs_lru, pma);
+}
+
+/**
+ * _psc_lru_sysfree - OK, no more games, the system needs the memory
+ *	back so release it for real.
+ * @p: memory base.
+ * @flags: flags the region was allocated with.
+ */
+void
+_psc_lru_sysfree(struct psc_memalloc *pma)
+{
+	void *p;
+
+	p = pma->pma_allocbase;
+	psc_mprotect(pma->pma_allocbase, PMA_LEN(pma),
+	    PROT_READ | PROT_WRITE);
+	free(pma);
+	free(p);
+}
+#else
+/**
+ * _psc_free - counterpart to psc_alloc (unless LINGER is enabled):
+ *	release memory.
+ * @p: memory base.
+ * @flags: flags the region was allocated with.
+ */
 void
 _psc_free(void *p, int flags, ...)
 {
 	va_list ap;
-
-	if (p == NULL)
-		return;
 
 	if (flags & PAF_LOCK) {
 		size_t len;
@@ -362,13 +466,16 @@ _psc_free(void *p, int flags, ...)
 
 		pma = _psc_getpma(p);
 		p = pma->pma_allocbase;
-		_psc_checkpma(pma);
+		pscmem_checkinnerguard(pma);
+		psc_mprotect(pma->pma_allocbase, PMA_LEN(pma),
+		    PROT_READ | PROT_WRITE);
 		free(pma);
 	}
-#endif
+#  endif
 
 	free(p);
 }
+#endif
 
 #define PFL_DEF_NMEMMAPS	(1024 * 1024)
 #define _PATH_MAX_MEMMAPS	"/proc/sys/vm/max_map_count"
