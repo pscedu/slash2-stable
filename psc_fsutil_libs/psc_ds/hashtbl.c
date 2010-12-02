fe7990d617d7e2083b5b7952d58dceb0db7943ba
Jared Yanovich <yanovich@psc.edu>
2010-12-02 22:28:38 +0000

    unnecessary
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@14566 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_ds/hashtbl.c
+++ psc_fsutil_libs/psc_ds/hashtbl.c
@@ -48,7 +48,7 @@ _psc_str_hashify(const char *s, int len)
 	if (s == NULL)
 		return (-1);
 	for (p = s; *p != '\0' && len; p++, len--) {
-		h = (h << 4) + (*p);
+		h = (h << 4) + *p;
 		if ((g = h & 0xf0000000)) {
 			h = h ^ (g >> 24);
 			h = h ^ g;
