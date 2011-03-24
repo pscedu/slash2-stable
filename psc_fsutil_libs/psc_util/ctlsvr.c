fc29b956214d9fef912417d8c82b64270d28a150
Jared Yanovich <yanovich@psc.edu>
2011-03-24 21:05:59 +0000

    wrap long lines
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@15906 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/psc_util/ctlsvr.c
+++ psc_fsutil_libs/psc_util/ctlsvr.c
@@ -1720,7 +1720,8 @@ psc_ctlthr_main(const char *ofn, const struct psc_ctlop *ct, int nops,
 	for (;;) {
 		spinlock(&psc_ctl_clifds_lock);
 		if (psc_dynarray_len(&psc_ctl_clifds) == 0) {
-			psc_waitq_wait(&psc_ctl_clifds_waitq, &psc_ctl_clifds_lock);
+			psc_waitq_wait(&psc_ctl_clifds_waitq,
+			    &psc_ctl_clifds_lock);
 			continue;
 		}
 		rnd = psc_random32u(psc_dynarray_len(&psc_ctl_clifds));
