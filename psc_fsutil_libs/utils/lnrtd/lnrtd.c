99576f4ee01a61ef90aeb623f0bd9ef68d30c17d
Jared Yanovich <yanovich@psc.edu>
2011-06-14 23:34:13 +0000

    fix compile
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@17042 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/utils/lnrtd/lnrtd.c
+++ psc_fsutil_libs/utils/lnrtd/lnrtd.c
@@ -1,13 +1,39 @@
 /* $Id$ */
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
+#include "pfl/str.h"
 #include "psc_rpc/rpc.h"
 
 #include "lnet/lnet.h"
 
 const char *progname;
+
+enum {
+	LRTHRT_LNETAC,
+	LRTHRT_USKLNDPL,
+};
+
+int
+psc_usklndthr_get_type(const char *namefmt)
+{
+	if (strstr(namefmt, "lnacthr"))
+		return (LRTHRT_LNETAC);
+	return (LRTHRT_USKLNDPL);
+}
+
+void
+psc_usklndthr_get_namev(char buf[PSC_THRNAME_MAX], const char *namefmt,
+    va_list ap)
+{
+	size_t n;
+
+	n = strlcpy(buf, "ln", PSC_THRNAME_MAX);
+	if (n < PSC_THRNAME_MAX)
+		vsnprintf(buf + n, PSC_THRNAME_MAX - n, namefmt, ap);
+}
 
 void
 usage(void)
