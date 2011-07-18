f94e8fc49d53ce0d902e0d4c6b6a460dce9cc046
Jared Yanovich <yanovich@psc.edu>
2011-07-18 19:40:35 +0000

    better
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@17292 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/sdp_inet.h
+++ psc_fsutil_libs/include/sdp_inet.h
@@ -4,7 +4,7 @@
 #define _PFL_SDP_INET_H_
 
 #ifndef AF_INET_SDP
-#define AF_INET_SDP 27			/* SDP socket protocol family */
+#define AF_INET_SDP 27			/* sockets-direct addr family */
 #endif
 
 #ifndef AF_INET_STR
