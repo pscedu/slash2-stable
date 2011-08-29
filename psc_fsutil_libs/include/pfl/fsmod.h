2129b17c96f7adf4dcce007e4adb82f9db5d96e9
Jared Yanovich <yanovich@psc.edu>
2011-08-29 15:56:51 +0000

    space
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@17604 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/pfl/fsmod.h
+++ psc_fsutil_libs/include/pfl/fsmod.h
@@ -41,8 +41,8 @@ struct pscfs_req {
 	struct fuse_file_info		*pfr_fuse_fi;
 	struct pscfs_clientctx		 pfr_clientctx;
 	struct psc_listentry		 pfr_lentry;
-	char                            *pfr_buf;
-	void                            *pfr_info;
+	char				*pfr_buf;
+	void				*pfr_info;
 };
 
 #  define PSCFS_ARGS_INIT(n, av)	{ FUSE_ARGS_INIT((n), (av)) }
