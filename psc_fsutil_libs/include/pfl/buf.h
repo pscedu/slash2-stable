d5dd243e3f62bc0e9ad0732ffabba98a9ee12ccc
Jared Yanovich <yanovich@psc.edu>
2012-02-13 23:09:02 +0000

    new dynamic buffer API
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@18739 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ psc_fsutil_libs/include/pfl/buf.h
@@ -0,0 +1,51 @@
+/* $Id$ */
+/*
+ * %PSC_START_COPYRIGHT%
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2005-2010, Pittsburgh Supercomputing Center (PSC).
+ *
+ * Permission to use, copy, and modify this software and its documentation
+ * without fee for personal use or non-commercial use within your organization
+ * is hereby granted, provided that the above copyright notice is preserved in
+ * all copies and that the copyright and this permission notice appear in
+ * supporting documentation.  Permission to redistribute this software to other
+ * organizations or individuals is not permitted without the written permission
+ * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+ * the suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ * -----------------------------------------------------------------------------
+ * %PSC_END_COPYRIGHT%
+ */
+
+/*
+ * Simple routines for manipulating variable-sized
+ * buffers.
+ *
+ * TODO: add the following members:
+ *	- buf_opts with option BUFOPT_FATAL_ALLOC
+ *	- buf_growamt to control growth size
+ */
+
+#ifndef _PFL_BUF_H_
+#define _PFL_BUF_H_
+
+struct buf {
+	int	 buf_pos;
+	int	 buf_max;
+	char	*buf_buf;
+};
+
+#define buf_nul(buf) buf_append((buf), '\0')
+
+void	 buf_append(struct buf *, int);
+void	 buf_appendfv(struct buf *, const char *, ...);
+void	 buf_appendv(struct buf *, const char *);
+void	 buf_chop(struct buf *);
+void	 buf_free(struct buf *);
+char	*buf_get(const struct buf *);
+void	 buf_init(struct buf *);
+int	 buf_len(const struct buf *);
+void	 buf_realloc(struct buf *);
+void	 buf_reset(struct buf *);
+
+#endif /* _PFL_BUF_H_ */
---
+++ psc_fsutil_libs/psc_util/buf.c
@@ -0,0 +1,109 @@
+/* $Id$ */
+/*
+ * %PSC_START_COPYRIGHT%
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2005-2010, Pittsburgh Supercomputing Center (PSC).
+ *
+ * Permission to use, copy, and modify this software and its documentation
+ * without fee for personal use or non-commercial use within your organization
+ * is hereby granted, provided that the above copyright notice is preserved in
+ * all copies and that the copyright and this permission notice appear in
+ * supporting documentation.  Permission to redistribute this software to other
+ * organizations or individuals is not permitted without the written permission
+ * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+ * the suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ * -----------------------------------------------------------------------------
+ * %PSC_END_COPYRIGHT%
+ */
+
+#include <err.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "pfl/buf.h"
+
+#define BUF_GROWAMT 30
+
+__inline void
+buf_init(struct buf *buf)
+{
+	buf->buf_pos = buf->buf_max = -1;
+	buf->buf_buf = NULL;
+}
+
+void
+buf_realloc(struct buf *buf)
+{
+	void *ptr;
+
+	if ((ptr = realloc(buf->buf_buf,
+	     buf->buf_max * sizeof(*buf->buf_buf))) == NULL)
+		err(1, "realloc");
+	buf->buf_buf = ptr;
+}
+
+void
+buf_append(struct buf *buf, int ch)
+{
+	if (++buf->buf_pos >= buf->buf_max) {
+		buf->buf_max += BUF_GROWAMT;
+		buf_realloc(buf);
+	}
+	buf->buf_buf[buf->buf_pos] = ch;
+}
+
+__inline void
+buf_appendv(struct buf *buf, const char *s)
+{
+	while (*s != '\0')
+		buf_append(buf, *s++);
+}
+
+void
+buf_appendfv(struct buf *buf, const char *fmt, ...)
+{
+	char *s, *t;
+	va_list ap;
+
+	va_start(ap, fmt);
+	if (vasprintf(&t, fmt, ap) == -1)
+		err(1, "vasprintf");
+	va_end(ap);
+
+	for (s = t; *s != '\0'; s++)
+		buf_append(buf, *s);
+	free(t);
+}
+
+__inline char *
+buf_get(const struct buf *buf)
+{
+	return (buf->buf_buf);
+}
+
+__inline void
+buf_free(struct buf *buf)
+{
+	free(buf->buf_buf);
+}
+
+__inline void
+buf_reset(struct buf *buf)
+{
+	buf->buf_pos = -1;
+}
+
+__inline void
+buf_chop(struct buf *buf)
+{
+	buf->buf_pos--;
+}
+
+__inline int
+buf_len(const struct buf *buf)
+{
+	return (buf->buf_pos + 1);
+}
