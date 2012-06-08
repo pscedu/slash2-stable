70c0c1aeb084bba76ce069386674e4ffc2aff64a
Jared Yanovich <yanovich@psc.edu>
2012-06-08 23:12:16 +0000

    unneeded
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@19171 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- psc_fsutil_libs/include/psc_util/fmtstr.h
+++ psc_fsutil_libs/include/psc_util/fmtstr.h
@@ -25,8 +25,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "psc_util/alloc.h"
-
 #define FMTSTRCASE(ch, convfmt, ...)					\
 	case ch:							\
 		_tlen = _t - _p + strlen(convfmt) + 1;			\
