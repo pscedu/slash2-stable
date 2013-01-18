60c679c29e82cef73a54a5de6c5f17d0ac2a4ddb
Jared Yanovich <yanovich@psc.edu>
2013-01-18 20:52:26 +0000

    regen
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@20537 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- pfl/utils/typedump/typedump.c
+++ pfl/utils/typedump/typedump.c
@@ -271,7 +271,7 @@ main(int argc, char *argv[])
 	PRTYPE(struct psc_wndmap_block);
 	PRTYPE(struct pscfs);
 	PRTYPE(struct pscfs_clientctx);
-	PRTYPE(struct pscfs_cred);
+	PRTYPE(struct pscfs_creds);
 	PRTYPE(struct pscfs_dirent);
 	PRTYPE(struct psclist_head);
 	PRTYPE(struct psclog_data);
