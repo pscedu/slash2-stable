dcb199449911513bf1beb3d75669f636222bbd44
Jared Yanovich <yanovich@psc.edu>
2013-04-04 21:03:32 +0000

    paths.h is not standard, add compat layer
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@21102 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ pfl/syspaths.h
@@ -0,0 +1,15 @@
+/* $Id$ */
+/* %PSC_COPYRIGHT% */
+
+#ifndef _PFL_SYSPATHS_H_
+#define _PFL_SYSPATHS_H_
+
+#ifdef HAVE_PATHS_H
+#  include "paths.h"
+#endif
+
+#ifndef _PATH_TTY
+#  define _PATH_TTY "/dev/tty"
+#endif
+
+#endif /* _PFL_SYSPATHS_H_ */
