949c170e4bf032f463ae5a3efcf8f2a1116267eb
Jared Yanovich <yanovich@psc.edu>
2013-04-05 22:13:15 +0000

    add test
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@21151 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ compat/blksize_t/blksize_t_compat.c
@@ -0,0 +1,16 @@
+/* $Id$ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+	blksize_t d = 0;
+	(void)argc;
+	(void)argv;
+	(void)d;
+	exit(0);
+}
