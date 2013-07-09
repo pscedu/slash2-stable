366a8394b2e707a6b6bea459ae28f00b9a00f759
Jared Yanovich <yanovich@psc.edu>
2013-07-09 21:21:47 +0000

    portable errno layer
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@21828 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ pfl/err.h
@@ -0,0 +1,14 @@
+/* $Id$ */
+
+#ifndef _PFL_ERR_H_
+#define _PFL_ERR_H_
+
+#define _PFLERR_START			500
+
+#define PFLERR_BADMSG			(_PFLERR_START +  0)
+#define PFLERR_KEYEXPIRED		(_PFLERR_START +  1)
+#define PFLERR_NOTCONN			(_PFLERR_START +  2)
+#define PFLERR_ALREADY			(_PFLERR_START +  3)
+#define PFLERR_NOTSUP			(_PFLERR_START +  4)
+
+#endif /* _PFL_ERR_H_ */
