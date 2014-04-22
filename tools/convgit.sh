1d609d64adb1d5a1dec717d1d5b73ef8560d4bc8
Jared Yanovich <yanovich@psc.edu>
2014-04-22 17:11:57 +0000

    update
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@23637 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- tools/convgit.sh
+++ tools/convgit.sh
@@ -1,9 +1,62 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # $Id$
+
+set -e
+
+echorun()
+{
+	echo "$@"
+	"$@"
+}
+
+clone()
+{
+	local url=$1
+	local dir=$2
+	echorun mv $dir $dir.oldsvn
+	echorun git clone $url $dir
+}
+
+getrepo()
+{
+	local base=$1
+	local name=${2//\//.}
+	local dir=$2
+	[ $# -eq 3 ] && dir=$3
+	cd $dir && git pull || clone $base/$name $dir
+}
+
+pubrepo()
+{
+	(getrepo https://github.com/pscedu "$@")
+}
+
+pubrepo()
+{
+	(getrepo git://source.psc.edu "$@")
+}
 
 base=$(basename $0)
 dir=$(dirname $0)
 cd $dir
-mv $base $base.oldsvn
-git clone https://github.com/pscedu/slash2.git
-git clone https://github.com/pscedu/pfl.git
+pubrepo proj p
+cd p
+pubrepo distrib/crcutil
+pubrepo distrib/fstest
+pubrepo distrib/fuse
+pubrepo distrib/iozone
+pubrepo distrib/libs3
+pubrepo distrib/spiobench
+pubrepo distrib/stumbleupon-tcollector
+pubrepo distrib/zfs-fuse
+pubrepo fio
+pubrepo idxsearch
+pubrepo mkidx
+pubrepo psync
+pubrepo sft
+pubrepo slash2
+pubrepo src-upd
+pubrepo xopctl
+
+prirepo inf
+prirepo zest
