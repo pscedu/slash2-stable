dda1bfd2d58b2c4528f4f777515bcfe91062445a
Jared Yanovich <yanovich@psc.edu>
2014-06-07 05:34:05 +0000

    add portable ESTALE and ECANCELED
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@23935 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- pfl/err.h
+++ pfl/err.h
@@ -11,6 +11,8 @@
 #define PFLERR_ALREADY			(_PFLERR_START +  3)
 #define PFLERR_NOTSUP			(_PFLERR_START +  4)
 #define PFLERR_NOSYS			(_PFLERR_START +  5)
+#define PFLERR_CANCELED			(_PFLERR_START +  6)
+#define PFLERR_STALE			(_PFLERR_START +  7)
 
 const char *pfl_strerror(int);
 
