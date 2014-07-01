0e4a68bfa03b86a806ce7529760c15a9cc0b8465
Jared Yanovich <yanovich@psc.edu>
2014-07-01 07:52:29 +0000

    fix api usage
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@24052 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- pfl/fuse.c
+++ pfl/fuse.c
@@ -675,7 +675,7 @@ pscfs_inum_fuse2pscfs(fuse_ino_t f_inum, int del)
 		else
 			pfic = NULL;
 	}
-	psc_hashbkt_put(b);
+	psc_hashbkt_put(&pscfs_inumcol_hashtbl, b);
 
 	if (del && pfic)
 		psc_pool_return(pscfs_inumcol_pool, pfic);
@@ -733,7 +733,7 @@ pscfs_inum_pscfs2fuse(pscfs_inum_t p_inum, double timeo)
 			    pfic);
 			pfic = NULL;
 		}
-		psc_hashbkt_put(b);
+		psc_hashbkt_put(&pscfs_inumcol_hashtbl, b);
 	} while (t);
 	if (pfic)
 		psc_pool_return(pscfs_inumcol_pool, pfic);
