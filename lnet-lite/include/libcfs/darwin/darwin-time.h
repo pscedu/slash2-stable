491656df8e2f6f85b52ce7f482c6df149922ef5f
Jared Yanovich <yanovich@psc.edu>
2014-10-08 07:46:21 +0000

    somehow...
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@24457 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- lnet-lite/include/libcfs/darwin/darwin-time.h
+++ lnet-lite/include/libcfs/darwin/darwin-time.h
@@ -248,7 +248,7 @@ static inline void cfs_duration_nsec(cfs_duration_t d, struct timespec *s)
 #ifdef __APPLE__
 # define CFS_TIME_T		"%lu"
 #elif defined(__FreeBSD__)
-# include <stdint.h>
+# include <inttypes.h>
 # define CFS_TIME_T		"%"PRId64
 #else
 # define CFS_TIME_T		"%u"
