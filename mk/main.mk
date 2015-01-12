80f7c7fb52aa67247133cc96b62e0dc5013691fc
Jared Yanovich <yanovich@psc.edu>
2015-01-12 18:09:10 +0000

    allow override of OBJDIR
    git-svn-id: svn+ssh://frodo/cluster/svn/projects@25185 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/main.mk
+++ mk/main.mk
@@ -29,7 +29,7 @@ ADD_FILE_CFLAGS=	$(shell if ! [ -f "$(abspath $1)" ]; then echo "ADD_FILE_CFLAGS
 FORCE_INST?=		0
 
 RELCURDIR=		$(call STRIPROOTDIR,${CURDIR}/)
-OBJDIR=			${OBJBASE}/psc.obj/${HOST}${CURDIR}
+OBJDIR?=		${OBJBASE}/psc.obj/${HOST}${CURDIR}
 DEPEND_FILE=		${OBJDIR}/.depend
 
 HOST=			$(word 1,$(subst ., ,$(shell hostname)))
