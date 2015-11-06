bce672aefcfcb0b25df5d6ff233633930cd700b3
Jared Yanovich <jaredy@google.com>
2015-11-06 12:40:16 -0500

    regen copyright

--- mount_wokfs/main.c
+++ mount_wokfs/main.c
@@ -1,5 +1,27 @@
 /* $Id: main.c 25363 2015-02-04 20:10:24Z zhihui $ */
-/* %PSC_COPYRIGHT% */
+/*
+ * %ISC_START_LICENSE%
+ * ---------------------------------------------------------------------
+ * Copyright 2015, Google, Inc.
+ * Copyright 2015, Pittsburgh Supercomputing Center
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+ * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ * --------------------------------------------------------------------
+ * %END_LICENSE%
+ */
 
 #include <dlfcn.h>
 #include <stdlib.h>
