daf90e4ded42df1bd3cab19d3e7050b1e40d7944
zhihui-slash2 <zhihui@psc.edu>
2016-02-11 11:58:57 -0500

    Let journal code returns the slot used for debugging purposes

--- pfl/journal.c
+++ pfl/journal.c
@@ -58,7 +58,7 @@ struct psc_journalthr {
 	struct psc_journal *pjt_pj;
 };
 
-__static void		pjournal_logwrite(struct psc_journal_xidhndl *, int,
+__static uint32_t	pjournal_logwrite(struct psc_journal_xidhndl *, int,
 				struct psc_journal_enthdr *, int);
 
 __static int		pjournal_logwrite_internal(struct psc_journal *,
@@ -401,10 +401,11 @@ pjournal_unreserve_slot(struct psc_journal *pj, int count)
 /**
  * pjournal_add_entry - Add a log entry into the journal.
  */
-void
+uint32_t
 pjournal_add_entry(struct psc_journal *pj, uint64_t txg,
     int type, int distill, void *buf, int size)
 {
+	uint32_t slot;
 	struct psc_journal_xidhndl *xh;
 	struct psc_journal_enthdr *pje;
 
@@ -413,7 +414,8 @@ pjournal_add_entry(struct psc_journal *pj, uint64_t txg,
 	pje = DATA_2_PJE(buf);
 	psc_assert(pje->pje_magic == PJE_MAGIC);
 
-	pjournal_logwrite(xh, type | PJE_NORMAL, pje, size);
+	slot = pjournal_logwrite(xh, type | PJE_NORMAL, pje, size);
+	return (slot);
 }
 
 void *
@@ -512,7 +514,7 @@ pjournal_logwrite_internal(struct psc_journal *pj,
  * @data: the journal entry contents to store.
  * @size: size of the custom data
  */
-__static void
+__static uint32_t 
 pjournal_logwrite(struct psc_journal_xidhndl *xh, int type,
     struct psc_journal_enthdr *pje, int size)
 {
@@ -556,6 +558,7 @@ pjournal_logwrite(struct psc_journal_xidhndl *xh, int type,
 		psc_assert(!xh->pjx_data);
 		freelock(&xh->pjx_lock);
 	}
+	return (xh->pjx_slot);
 }
 
 __static void *
