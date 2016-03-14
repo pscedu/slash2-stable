d9e3641bcbb648e3181c271a15934b9f7a1f8c44
zhihui-slash2 <zhihui@psc.edu>
2016-03-14 15:48:52 -0400

    date -d friendly format

--- pfl/log.c
+++ pfl/log.c
@@ -61,7 +61,7 @@
 #include "pfl/time.h"
 
 #ifndef PSC_LOG_FMT
-#define PSC_LOG_FMT "[%s:%06u %n:%I:%T %B %F %l] "
+#define PSC_LOG_FMT "[@%s.%06u %n:%I:%T %B %F %l] "
 #endif
 
 const char			*psc_logfmt = PSC_LOG_FMT;
