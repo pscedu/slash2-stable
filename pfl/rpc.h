12d6b0887582d286dbf1c1a4bc81092bd60c55b4
Jared Yanovich <jaredy@google.com>
2016-03-16 12:09:25 -0400

    rename

--- pfl/rpc.h
+++ pfl/rpc.h
@@ -605,7 +605,7 @@ int	 pscrpc_target_send_reply_msg(struct pscrpc_request *, int, int);
 void	 pscrpc_fail_import(struct pscrpc_import *, uint32_t);
 
 /* util.c */
-int	pflrpc_portable_rc(int);
+int	pflrpc_portable_errno(int);
 
 void	pscrpc_getlocalprids(struct psc_dynarray *);
 void	pscrpc_getpridforpeer(lnet_process_id_t *,
