e81734ab35d942e2765ee18877e5b0316f27c264
Jared Yanovich <jaredy@google.com>
2016-04-03 05:11:47 -0400

    add API

--- pfl/dynarray.c
+++ pfl/dynarray.c
@@ -103,14 +103,15 @@ psc_dynarray_ensurelen(struct psc_dynarray *pda, int n)
  * @pda: the dynamic array to reverse.
  */
 void
-psc_dynarray_reverse(struct psc_dynarray *pda)
+psc_dynarray_reverse_subsequence(struct psc_dynarray *pda, int start,
+    int len)
 {
 	void *tmp;
 	int i;
 
-	for (i = 0; i < psc_dynarray_len(pda) / 2; i++)
+	for (i = start; i < len / 2; i++)
 		SWAP(pda->pda_items[i], pda->pda_items[
-		    psc_dynarray_len(pda) - 1 - i], tmp);
+		    start + len - 1 - i], tmp);
 }
 
 /*
