82a808ac40b50fee22409d2d823bd3c1a64d5845
zhihui-slash2 <zhihui@psc.edu>
2017-04-27 15:59:16 -0400

    Move fixes of 64-bit IOS limt over

--- slash2/slashd/mds.c
+++ slash2/slashd/mds.c
@@ -58,6 +58,8 @@
 
 struct pfl_odt		*slm_bia_odt;
 
+int			slm_max_ios = SL_MAX_REPLICAS;
+
 int			slm_ptrunc_enabled = 0;
 int			slm_preclaim_enabled = 1;
 
@@ -355,14 +357,15 @@ slm_try_sliodresm(struct sl_resm *resm, int repls)
 
 /*
  * Do proper shuffling to avoid statistical bias when some IOS are
- * offline, which would give unfair advantage to the first IOS that was
- * online if we simply filled this list in sequentially.
+ * offline, which would give unfair advantage to the first IOS that 
+ * was online if we simply filled this list in sequentially.
  */
 void
 slm_res_shuffle(struct psc_dynarray *a, int begin)
 {
 	int i;
 
+	/* We never touch the element before begin */
 	for (i = 1; i < psc_dynarray_len(a) - begin; i++)
 		psc_dynarray_swap(a, begin + i, begin +
 		    psc_random32u(i + 1));
@@ -395,40 +398,61 @@ void
 slm_get_ioslist(struct fidc_membh *f, sl_ios_id_t piosid,
     struct psc_dynarray *a)
 {
+	int i, nr, begin;
 	struct sl_resource *pios, *r;
-	int begin, i;
 
 	pios = libsl_id2res(piosid);
 	if (!pios || (!RES_ISFS(pios) && !RES_ISCLUSTER(pios)))
 		return;
 
-	/* If affinity, prefer the first resm from the reptbl. */
-	if (fcmh_2_inoh(f)->inoh_flags & INOF_IOS_AFFINITY) {
-		r = libsl_id2res(fcmh_getrepl(f, 0).bs_id);
-		if (r)
-			slm_res_fillmembers(r, a, 1);
+	if (slm_max_ios < 1)
+		slm_max_ios = 1;
+	if (slm_max_ios > SL_MAX_REPLICAS)
+		slm_max_ios = SL_MAX_REPLICAS;
+
+	nr = fcmh_2_nrepls(f);
+	if (nr < slm_max_ios) {
+
+		/* If affinity, prefer the first resm from the reptbl. */
+		if (fcmh_2_inoh(f)->inoh_flags & INOF_IOS_AFFINITY) {
+			r = libsl_id2res(fcmh_getrepl(f, 0).bs_id);
+			if (r)
+				slm_res_fillmembers(r, a, 1);
+		}
+		/*
+		 * Add the preferred IOS member(s) next.  Note that PIOS
+		 * may be a CNOS, parallel IOS, or stand-alone.
+		 */
+		slm_res_fillmembers(pios, a, 1);
+		begin = psc_dynarray_len(a);
+
+		/*
+		 * Add everything else.  Archival are not considered and 
+		 * must be specifically set via PREF_IOS to obtain write 
+		 * leases.
+		 */
+		DYNARRAY_FOREACH(r, i, &nodeSite->site_resources) {
+			if (!RES_ISFS(r) || r == pios ||
+			    r->res_type == SLREST_ARCHIVAL_FS)
+				continue;
+
+			slm_res_fillmembers(r, a, 0);
+		}
+
+	} else {
+
+		OPSTAT_INCR("reuse-iolist");
+
+		begin = psc_dynarray_len(a);
+		slm_ptrunc_enabled = 0;
+
+
+		for (i = 0; i < slm_max_ios; i++) {
+			r = libsl_id2res(fcmh_getrepl(f, i).bs_id);
+			if (r)
+				slm_res_fillmembers(r, a, 0);
+		}
 	}
-
-	/*
-	 * Add the preferred IOS member(s) next.  Note that PIOS may be
-	 * a CNOS, parallel IOS, or stand-alone.
-	 */
-	slm_res_fillmembers(pios, a, 1);
-
-	begin = psc_dynarray_len(a);
-
-	/*
-	 * Add everything else.  Archival are not considered and must be
-	 * specifically set via PREF_IOS to obtain write leases.
-	 */
-	DYNARRAY_FOREACH(r, i, &nodeSite->site_resources) {
-		if (!RES_ISFS(r) || r == pios ||
-		    r->res_type == SLREST_ARCHIVAL_FS)
-			continue;
-
-		slm_res_fillmembers(r, a, 0);
-	}
-
 	slm_res_shuffle(a, begin);
 }
 
@@ -559,8 +583,7 @@ mds_bmap_add_repl(struct bmap *b, struct bmap_ios_assign *bia)
 	psc_assert(b->bcm_flags & BMAPF_IOSASSIGNED);
 
 	FCMH_LOCK(f);
-	iosidx = mds_repl_ios_lookup_add(current_vfsid, ih,
-	    bia->bia_ios);
+	iosidx = mds_repl_ios_lookup_add(current_vfsid, ih, bia->bia_ios);
 	FCMH_ULOCK(f);
 
 	if (iosidx < 0) {
