046af077f7ac59fd11318891b7d83bb2499c937b
zhihui-slash2 <zhihui@psc.edu>
2017-05-31 13:55:13 -0400

    move get_callerinfo to avoid compiler complains

--- pfl/pfl.h
+++ pfl/pfl.h
@@ -118,27 +118,7 @@ extern pid_t			  pfl_pid;
 # define __callerinfo __unusedx const struct pfl_callerinfo *pci
 #endif
 
-static __inline const struct pfl_callerinfo *
-_pfl_callerinfo_get(const char *fn, const char *func, int lineno,
-    int subsys)
-{
-	struct pfl_callerinfo *pci;
-
-	/*
-	 * Hit segmentation fault with _psc_realloc() from here
-	 * coming from pscrpcthr_waitevent(). The MDS has been
-	 * running for 40+ days.
-	 *
-	 * XXX we have no way to check if allocation of pci is
-	 * successful or not.  Instead of being smart, just
-	 * allocate them in the per-thread structure.
-	 */
-	pci = pfl_tls_get(PFL_TLSIDX_CALLERINFO, sizeof(*pci));
-	pci->pci_filename = fn;
-	pci->pci_func = func;
-	pci->pci_lineno = lineno;
-	pci->pci_subsys = subsys;
-	return (pci);
-}
+__inline const struct pfl_callerinfo *
+_pfl_callerinfo_get(const char *, const char *, int, int);
 
 #endif /* _PFL_PFL_H_ */
--- pfl/thread.c
+++ pfl/thread.c
@@ -137,6 +137,28 @@ pfl_tls_get(int idx, size_t len)
 	return (tbl[idx]);
 }
 
+__inline const struct pfl_callerinfo *
+_pfl_callerinfo_get(const char *fn, const char *func, int lineno,
+    int subsys)
+{
+	struct pfl_callerinfo *pci;
+
+	/*
+	 * Hit segmentation fault with _psc_realloc() from here
+	 * coming from pscrpcthr_waitevent(). The MDS has been
+	 * running for 40+ days.
+	 *
+	 * XXX we have no way to check if allocation of pci is
+	 * successful or not.  Instead of being smart, just
+	 * allocate them in the per-thread structure.
+	 */
+	pci = pfl_tls_get(PFL_TLSIDX_CALLERINFO, sizeof(*pci));
+	pci->pci_filename = fn;
+	pci->pci_func = func;
+	pci->pci_lineno = lineno;
+	pci->pci_subsys = subsys;
+}
+
 /*
  * Pause thread execution.
  * @sig: signal number.
