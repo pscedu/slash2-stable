8b51c31b9b255dc7cf09ba4f3430f5019fd0a1c5
zhihui-slash2 <zhihui@psc.edu>
2017-07-13 10:08:37 -0400

    doc a crash

--- lnet-lite/ulnds/socklnd/conn.c
+++ lnet-lite/ulnds/socklnd/conn.c
@@ -689,7 +689,13 @@ usocklnd_destroy_conn(usock_conn_t *conn)
         }
 
         if (!list_empty(&conn->uc_tx_list)) {
-		/* 04/04/2017: Hit NULL crash below */
+		/*
+		 * 04/04/2017: Hit NULL assert below.
+		 *
+		 * 07/13/2017: Hit NULL assert again: 
+		 *
+		 * uc_peerid = { 10.4.108.85@sdp5, 54321 }
+		 */
                 LASSERT (conn->uc_peer != NULL);                
                 usocklnd_destroy_txlist(conn->uc_peer->up_ni, &conn->uc_tx_list);
         }
