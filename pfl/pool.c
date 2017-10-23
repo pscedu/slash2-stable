f3a2ef848737cbde56585054e759b10cf3edcecd
zhihui-slash2 <zhihui@psc.edu>
2017-10-23 10:33:43 -0400

    comment

--- pfl/pool.c
+++ pfl/pool.c
@@ -739,7 +739,13 @@ _psc_pool_return(struct psc_poolmgr *m, void *p)
 		/*
 		 * We need this if all the memory of the item will be used 
 		 * including the link entry. Right now, we don't do this.
+		 *
+		 * 10/23/2017: Looks like we should not touch the linkage
+		 * entry.  We can zero the whole item because the linkage
+		 * entry is already zero'ed when an item is retrieved for
+		 * use.
 		 */
+
 		INIT_PSC_LISTENTRY(psclist_entry2(p, m->ppm_explist.pexl_offset));
 #endif
 		POOL_ADD_ITEM(m, p);
