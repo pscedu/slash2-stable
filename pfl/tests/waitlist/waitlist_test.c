07812c2574b4b095f472ceba60e1c1fae02a3a8b
zhihui-slash2 <zhihui@psc.edu>
2018-03-15 11:32:09 -0400

    copyright

--- pfl/tests/waitlist/waitlist_test.c
+++ pfl/tests/waitlist/waitlist_test.c
@@ -3,7 +3,7 @@
  * %ISC_START_LICENSE%
  * ---------------------------------------------------------------------
  * Copyright 2016, Google, Inc.
- * Copyright 2016, Pittsburgh Supercomputing Center
+ * Copyright 2016-2018, Pittsburgh Supercomputing Center
  * All rights reserved.
  *
  * Permission to use, copy, modify, and distribute this software for any
