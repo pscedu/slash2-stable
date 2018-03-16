51dd7513f0e0003e0d563595aece263366853862
zhihui-slash2 <zhihui@psc.edu>
2018-03-16 13:40:05 -0400

    copyright

--- mount_wokfs/ctl.c
+++ mount_wokfs/ctl.c
@@ -2,8 +2,7 @@
 /*
  * %ISC_START_LICENSE%
  * ---------------------------------------------------------------------
- * Copyright 2015, Google, Inc.
- * Copyright 2015-2018, Pittsburgh Supercomputing Center
+ * Copyright 2015, Pittsburgh Supercomputing Center
  * All rights reserved.
  *
  * Permission to use, copy, modify, and distribute this software for any
